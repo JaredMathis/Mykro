f6295a83f353f393efbcb070c29f07335d00c9f9
JaredMathis <jared.mathis@native.consulting>
2022-07-02 16:23:45 -0400

    progress

--- src/arguments_assert.mjs
+++ src/arguments_assert.mjs
@@ -1,3 +1,13 @@
+import { assert } from "./assert.mjs";
+import { equals } from "./equals.mjs";
+
 export function arguments_assert() {
-    
+    let asserts = Array.from(arguments);
+    return function wrapped() {
+        let _arguments = Array.from(arguments);
+        assert(equals)(asserts.length, arguments.length);
+        asserts.forEach((a, i) => {
+            assert(a)(_arguments[i]);
+        })
+    }
 }
