d3be266a825d88f10027ce28e0b2abb100d7484f
JaredMathis <jared.mathis@native.consulting>
2022-07-02 16:36:04 -0400

    missing import

--- src/file_overwrite.mjs
+++ src/file_overwrite.mjs
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { string_is } from './string_is.mjs';
+import { arguments_assert } from './arguments_assert.mjs';
 
 export function file_overwrite(file_path, text) {
     arguments_assert(string_is)(arguments);
