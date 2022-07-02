84af98585c3b73cce5a68e51cfef87ccc38694a1
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:43:19 -0400

    progress

--- src/git_commit.mjs
+++ src/git_commit.mjs
@@ -1,5 +1,5 @@
 import { command_line } from "./command_line.mjs";
 
-export async function git_commit() {
-    return await command_line(`git commit`)
+export async function git_commit(message) {
+    return await command_line(`git commit -m ${message}`)
 }
