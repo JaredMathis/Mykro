1924542f88c99509c8670328603d09bab32efb64
JaredMathis <jared.mathis@native.consulting>
2022-07-02 18:42:42 -0400

    missing-import

--- src/msft_vs_code_open.mjs
+++ src/msft_vs_code_open.mjs
@@ -1,5 +1,6 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { command_line } from "./command_line.mjs";
 import { string_is } from "./string_is.mjs";
 
 export async function msft_vs_code_open(file_path) {
