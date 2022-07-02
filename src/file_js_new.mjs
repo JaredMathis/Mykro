772d6ffb0cc558a58ee2779f5ebd24c892e4510f
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:19:43 -0400

    progress

--- src/file_js_new.mjs
+++ src/file_js_new.mjs
@@ -1,10 +1,10 @@
 import { not } from "./not.mjs";
 import { assert } from "./assert.mjs";
 import { file_exists } from "./file_exists.mjs";
-import { file_new } from "./file_new.mjs";
+import { file_overwrite } from "./file_overwrite.mjs";
 
-export async function file_js_new(file_path) {
+export async function file_js_new(file_path, function_name) {
+    let file_path = ``
     await assert(not(file_exists))(file_path)
-
-    //file_new()
+    file_overwrite(file_path, `export function function_name`)
 }
