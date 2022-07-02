9d8850f19b7982e5c22ca5261cd76755747154a9
JaredMathis <jared.mathis@native.consulting>
2022-07-02 18:15:59 -0400

    progress

--- src/file_prepend.mjs
+++ src/file_prepend.mjs
@@ -2,8 +2,8 @@ import fs from 'fs';
 import { arguments_assert } from './arguments_assert.mjs';
 import { string_is } from './string_is.mjs';
 
-export function file_prepend(file_path, text_prepend) {
-    arguments_assert(string_is, string_is)(arguments);
+export async function file_prepend(file_path, text_prepend) {
+    await arguments_assert(string_is, string_is)(arguments);
     // file_read(file_path)
     // fs.promises.readFile()
     // TODO
--- src/file_read.mjs
+++ src/file_read.mjs
@@ -1,4 +1,6 @@
+import { arguments_assert } from "./arguments_assert.mjs";
 
 export function file_read() {
+    await arguments_assert()(arguments);
     // TODO
 }
