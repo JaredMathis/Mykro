fb08096335933ba8a984a1e941db41df9d5f2faf
JaredMathis <jared.mathis@native.consulting>
2022-07-02 16:51:02 -0400

    missing import

--- src/not.mjs
+++ src/not.mjs
@@ -1,5 +1,6 @@
 import { assert } from "./assert.mjs";
 import { function_call } from "./function_call.mjs";
+import { boolean_is } from "./boolean_is.mjs";
 
 export function not(_function) {
     return function wrapped() {
