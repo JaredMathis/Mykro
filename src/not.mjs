4f9eb6ee985b260a396af703cb295aa8c7000485
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:06:02 -0400

    fixed typo

--- src/not.mjs
+++ src/not.mjs
@@ -2,8 +2,8 @@ import { assert } from "./assert.mjs";
 import { function_call } from "./function_call.mjs";
 import { boolean_is } from "./boolean_is.mjs";
 
-export async function not(_function) {
-    return function wrapped() {
+export function not(_function) {
+    return async function wrapped() {
         let result = await function_call(_function, arguments);
         assert(boolean_is)(result);
         return !result;
