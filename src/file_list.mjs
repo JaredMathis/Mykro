ad4a736508ef9c73c60484606418d54a75be9a3d
JaredMathis <jared.mathis@native.consulting>
2022-07-02 15:14:28 -0400

    converted to import

--- src/file_list.mjs
+++ src/file_list.mjs
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 export function *file_list(dir) {
   const files = fs.readdirSync(dir, { withFileTypes: true });
