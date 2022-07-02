e5767473b7ca3e19303b9c5539e8457220dda177
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:20:05 -0400

    typofix

--- src/file_js_new.mjs
+++ src/file_js_new.mjs
@@ -6,5 +6,5 @@ import { file_overwrite } from "./file_overwrite.mjs";
 export async function file_js_new(file_path, function_name) {
     let file_path = ``
     await assert(not(file_exists))(file_path)
-    file_overwrite(file_path, `export function function_name`)
+    file_overwrite(file_path, `export function ${function_name}`)
 }
