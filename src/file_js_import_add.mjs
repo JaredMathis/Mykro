a8a6cef6b94e24d2bfd20fe6e8abf05eb14aa00b
JaredMathis <jared.mathis@native.consulting>
2022-07-02 18:34:47 -0400

    comment-remove

--- src/file_js_import_add.mjs
+++ src/file_js_import_add.mjs
@@ -7,8 +7,6 @@ import { string_is } from "./string_is.mjs"
 export async function file_js_import_add(function_name, import_name) {
     await arguments_assert(string_is, string_is)(arguments);
     let file_path = await file_js_name_to_path(function_name);
-
-    // TODO
     await file_prepend(
         file_path, 
         `import ${import_name} from '${import_name}';    
