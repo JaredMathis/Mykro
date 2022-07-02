4a3ded09b3a7d956a3a87e4bda4c12abd4f363cc
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:06:41 -0400

    fixed typo

--- src/returns.mjs
+++ src/returns.mjs
@@ -2,8 +2,8 @@ import {equals} from './equals.mjs';
 import {function_call} from './function_call.mjs';
 import {error} from './error.mjs';
 
-export async function returns(_function, expected, data) {
-    return function wrapped() {
+export function returns(_function, expected, data) {
+    return async function wrapped() {
         let result = await function_call(_function, arguments);
         if (!equals(result, expected)) {
             error(JSON.stringify({
