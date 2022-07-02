70339cda4f345c7670369f53bf9f947cce7b59ee
JaredMathis <jared.mathis@native.consulting>
2022-07-02 18:06:46 -0400

    progress

--- src/file_js_import_add.mjs
+++ src/file_js_import_add.mjs
@@ -1,4 +1,11 @@
+import { arguments_assert } from "./arguments_assert.mjs"
+import { string_is } from "./string_is.mjs"
 
-export function file_js_import_add() {
+export function file_js_import_add(import_name) {
+    arguments_assert(string_is)(arguments);
+    
     // TODO
+    file_prepend(`
+import ${import_name} from '${import_name}';    
+`)
 }
