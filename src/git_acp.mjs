2ede61eb2c66441df15150c4bcf25006f8ab3b85
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:48:47 -0400

    progress

--- src/git_acp.mjs
+++ src/git_acp.mjs
@@ -2,10 +2,10 @@ import { git_add } from "./git_add.mjs";
 import { git_commit } from "./git_commit.mjs";
 import { git_push } from "./git_push.mjs";
 
-export async function git_acp() {
+export async function git_acp(message) {
     for (let command in [
         git_add, 
-        git_commit, 
+        () => git_commit(message), 
         git_push
     ]) {
         await command();
