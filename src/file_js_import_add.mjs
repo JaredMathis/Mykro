e8ba7663e3a724746d3e43e078204bc939a5ba4b
JaredMathis <jared.mathis@native.consulting>
2022-07-02 18:34:21 -0400

    progress

--- src/file_js_import_add.mjs
+++ src/file_js_import_add.mjs
@@ -1,6 +1,7 @@
 import { arguments_assert } from "./arguments_assert.mjs"
 import { file_js_name_to_path } from "./file_js_name_to_path.mjs";
 import { file_prepend } from "./file_prepend.mjs";
+import { msft_vs_code_open } from "./msft_vs_code_open.mjs";
 import { string_is } from "./string_is.mjs"
 
 export async function file_js_import_add(function_name, import_name) {
@@ -11,5 +12,6 @@ export async function file_js_import_add(function_name, import_name) {
     await file_prepend(
         file_path, 
         `import ${import_name} from '${import_name}';    
-`)
+`);
+    await msft_vs_code_open(file_path);
 }
