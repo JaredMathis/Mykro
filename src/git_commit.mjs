cb44b77a01e979e62decdf190e93dd6aa476cfc8
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:56:45 -0400

    added string delimeters

--- src/git_commit.mjs
+++ src/git_commit.mjs
@@ -5,5 +5,5 @@ import { string_is } from "./string_is.mjs";
 export async function git_commit(message) {
     await arguments_assert(string_is)(arguments);
     
-    return await command_line(`git commit -m ${message}`)
+    return await command_line(`git commit -m "${message}"`)
 }
