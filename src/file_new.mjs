b0a35241696c311d8fda138f04da42830db65bee
JaredMathis <jared.mathis@native.consulting>
2022-07-02 16:16:48 -0400

    progress

--- src/file_new.mjs
+++ src/file_new.mjs
@@ -1,9 +1,9 @@
 import fs from 'fs';
-import { assert } from './assert.mjs';
+import { arguments_assert } from './arguments_assert.mjs';
 import { string_is } from './string_is.mjs';
 
 export function file_new(file_path) {
-    assert(string_is)(file_path);
+    arguments_assert(string_is)(arguments);
     file_overwrite(file_path)
     fs.promises.writeFile()
 }
