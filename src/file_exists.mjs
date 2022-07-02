95ac0faa85ed96ec88aeea251207fb1cf9375ca9
JaredMathis <jared.mathis@native.consulting>
2022-07-02 16:45:37 -0400

    progress

---
+++ src/file_exists.mjs
@@ -0,0 +1,8 @@
+import fs from 'fs';
+
+// From: https://stackoverflow.com/questions/17699599/node-js-check-if-file-exists
+export default function file_exists(file) {
+    return fs.promises.access(file, fs.constants.F_OK)
+             .then(() => true)
+             .catch(() => false)
+}
