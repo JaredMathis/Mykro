048a4c39eeb900001e517c74c63c8fac21d8720d
JaredMathis <jared.mathis@native.consulting>
2022-07-02 16:37:04 -0400

    fixed bug

--- src/file_overwrite.mjs
+++ src/file_overwrite.mjs
@@ -3,6 +3,6 @@ import { string_is } from './string_is.mjs';
 import { arguments_assert } from './arguments_assert.mjs';
 
 export function file_overwrite(file_path, text) {
-    arguments_assert(string_is)(arguments);
+    arguments_assert(string_is, string_is)(arguments);
     fs.promises.writeFile(file_path, text);
 }
