ca54cae3d82448a7cfb3af526b01bbb12c2c75f1
JaredMathis <jared.mathis@native.consulting>
2022-07-02 15:24:48 -0400

    exported function

--- src/not.js
+++ src/not.js
@@ -1,4 +1,4 @@
-function not(_function) {
+export function not(_function) {
     return function wrapped() {
         let result = function_call(_function, arguments);
         assert(boolean_is)(result);
