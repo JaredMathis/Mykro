f2e78fb4251b9b27d64b722f681c3aac5bce7623
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:48:18 -0400

    whitespace

--- src/git_acp.mjs
+++ src/git_acp.mjs
@@ -3,7 +3,11 @@ import { git_commit } from "./git_commit.mjs";
 import { git_push } from "./git_push.mjs";
 
 export async function git_acp() {
-    for (let command in [git_add, git_commit, git_push]) {
+    for (let command in [
+        git_add, 
+        git_commit, 
+        git_push
+    ]) {
         await command();
     }
 }
