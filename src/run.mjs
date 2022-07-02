571b1576a07272cf36824fd58a9260b84cf920c1
JaredMathis <jared.mathis@native.consulting>
2022-07-02 15:55:09 -0400

    refactor

--- src/run.mjs
+++ src/run.mjs
@@ -1,8 +1,6 @@
-import _ from 'lodash';
+
 import readline from 'readline';
-import {file_list} from './file_list.mjs'
-import { path_file_name } from './path_file_name.mjs';
-import path from 'path';
+import { run_line } from './run_line.mjs';
 
 var rl = readline.createInterface({
     input: process.stdin,
@@ -14,26 +12,3 @@ rl.on('line', run_line)
 
 let directory = './src';
 
-
-async function run_line(line) {
-    let tokens = line.split(' ');
-    let first = tokens[0];
-    let files = Array.from(file_list(directory));
-    let mapped = files.map(f => {
-        return {
-            name: path_file_name(f),
-            file_path: f,
-        } 
-    } );
-    let match;
-    if (match = _.find(mapped, { name: first })) {
-        let import_path = path.resolve(match.file_path)
-        let imported = await import("file://" + import_path);
-        let _function = imported[match.name];
-        let tokens_remaining = tokens.slice(1);
-        let result = _function(...tokens_remaining)
-        console.log(result);
-    } else {
-        console.log('No matching command.')
-    }
-}
---
+++ src/run_line.mjs
@@ -0,0 +1,27 @@
+import _ from 'lodash';e
+import {file_list} from './file_list.mjs'
+import { path_file_name } from './path_file_name.mjs';
+import path from 'path';
+
+export async function run_line(line) {
+    let tokens = line.split(' ');
+    let first = tokens[0];
+    let files = Array.from(file_list(directory));
+    let mapped = files.map(f => {
+        return {
+            name: path_file_name(f),
+            file_path: f,
+        } 
+    } );
+    let match;
+    if (match = _.find(mapped, { name: first })) {
+        let import_path = path.resolve(match.file_path)
+        let imported = await import("file://" + import_path);
+        let _function = imported[match.name];
+        let tokens_remaining = tokens.slice(1);
+        let result = _function(...tokens_remaining)
+        console.log(result);
+    } else {
+        console.log('No matching command.')
+    }
+}s
