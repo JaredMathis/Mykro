5ab8e94df7188e7a8bffcf19fdb32ee8d2da12a1
JaredMathis <jared.mathis@native.consulting>
2022-07-02 19:01:23 -0400

    echo progress

--- src/web_search.mjs
+++ src/web_search.mjs
@@ -2,5 +2,6 @@ import axios from 'axios';
 import { parse } from 'node-html-parser';
 export async function web_search(query) {
     let result = await axios.get('https://duckduckgo.com/?q=' + query);
-    console.log(result.data);
+    let parsed = parse(result.data);
+    return parsed;
 }
