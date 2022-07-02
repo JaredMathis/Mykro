80bea6bf25f307736a6bdfd3e7a7c19bb2c9ea3f
JaredMathis <jared.mathis@native.consulting>
2022-07-02 16:12:21 -0400

    missing import

--- src/file_new.mjs
+++ src/file_new.mjs
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { assert } from './assert.mjs';
+import { string_is } from './string_is.mjs';
 
 export function file_new(file_path) {
     assert(string_is)(file_path);
