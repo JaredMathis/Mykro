20d93151774139d4828c8c67137a5c6c6ca1a887
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:21:08 -0400

    typo bug fix

--- src/file_js_new.mjs
+++ src/file_js_new.mjs
@@ -4,7 +4,7 @@ import { file_exists } from "./file_exists.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 import { directory_get } from "./directory_get.mjs";
 
-export async function file_js_new(file_path, function_name) {
+export async function file_js_new(function_name) {
     let file_path = path.join(directory_get(), `${function_name}.mjs`);
     await assert(not(file_exists))(file_path)
     file_overwrite(file_path, `export function ${function_name}`)
