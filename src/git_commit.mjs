64ea9906661cce5c5c2f0a16b40d67f48402b57e
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:51:50 -0400

    progress

--- src/git_commit.mjs
+++ src/git_commit.mjs
@@ -3,7 +3,7 @@ import { arguments_assert } from "./arguments_assert.mjs";
 import { string_is } from "./string_is.mjs";
 
 export async function git_commit(message) {
-    arguments_assert(string_is)(arguments);
+    await arguments_assert(string_is)(arguments);
     
     return await command_line(`git commit -m ${message}`)
 }
