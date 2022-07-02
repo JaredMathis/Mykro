5cbc5cf2510fa288f54c189f0cd0e786a23ca947
JaredMathis <jared.mathis@native.consulting>
2022-07-02 16:24:51 -0400

    argument

--- src/returns.mjs
+++ src/returns.mjs
@@ -2,7 +2,7 @@ import {equals} from './equals.mjs';
 import {function_call} from './function_call.mjs';
 import {error} from './error.mjs';
 
-export function returns(_function, expected) {
+export function returns(_function, expected, data) {
     return function wrapped() {
         let result = function_call(_function, arguments);
         if (!equals(result, expected)) {
