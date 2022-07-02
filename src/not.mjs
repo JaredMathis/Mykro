11c4527c43b16a21071144d642d2af618ae408d3
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:02:22 -0400

    made function async

--- src/not.mjs
+++ src/not.mjs
@@ -2,9 +2,9 @@ import { assert } from "./assert.mjs";
 import { function_call } from "./function_call.mjs";
 import { boolean_is } from "./boolean_is.mjs";
 
-export function not(_function) {
+export async function not(_function) {
     return function wrapped() {
-        let result = function_call(_function, arguments);
+        let result = await function_call(_function, arguments);
         assert(boolean_is)(result);
         return !result;
     }
