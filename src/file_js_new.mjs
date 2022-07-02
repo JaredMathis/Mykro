b8246b0beb3712a2f6a54bddce914494b2524d4f
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:21:28 -0400

    added missing import

--- src/file_js_new.mjs
+++ src/file_js_new.mjs
@@ -3,6 +3,7 @@ import { assert } from "./assert.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 import { directory_get } from "./directory_get.mjs";
+import path from 'path'
 
 export async function file_js_new(function_name) {
     let file_path = path.join(directory_get(), `${function_name}.mjs`);
