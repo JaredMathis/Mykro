9d3bee28e3c52d85dd66be4bd5737be723dc7f44
JaredMathis <jared.mathis@native.consulting>
2022-07-02 18:48:29 -0400

    call-git_acp

--- src/npm_install.mjs
+++ src/npm_install.mjs
@@ -2,9 +2,11 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { command_line } from "./command_line.mjs";
 import { string_is } from "./string_is.mjs";
+import { git_acp } from "./git_acp.mjs";
 
 export async function npm_install(package_name) {
     await arguments_assert(string_is)(arguments);
 
     await command_line(`npm i ${package_name}`);
+    await git_acp(`${npm_install.name} ${package_name}`);
 }
