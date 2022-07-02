d5d9a179e3ea8e4cbbf542ed8529b45b7750034e
JaredMathis <jared.mathis@native.consulting>
2022-07-02 16:24:41 -0400

    added argument

--- src/assert.mjs
+++ src/assert.mjs
@@ -1,5 +1,5 @@
 import { returns } from "./returns.mjs";
 
-export function assert(_function) {
-    return returns(_function, true);
+export function assert(_function, data) {
+    return returns(_function, true, data);
 }
