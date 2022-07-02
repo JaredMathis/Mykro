726e0f56e047fffc24837a937e159e61e90956c1
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:05:08 -0400

    made function async

--- src/file_overwrite.mjs
+++ src/file_overwrite.mjs
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { string_is } from './string_is.mjs';
 import { arguments_assert } from './arguments_assert.mjs';
 
-export function file_overwrite(file_path, text) {
-    arguments_assert(string_is, string_is)(arguments);
-    fs.promises.writeFile(file_path, text);
+export async function file_overwrite(file_path, text) {
+    await arguments_assert(string_is, string_is)(arguments);
+    await fs.promises.writeFile(file_path, text);
 }
