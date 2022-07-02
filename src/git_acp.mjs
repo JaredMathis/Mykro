81db38eb47a9420b2ada39af007e60587c6b62b3
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:49:09 -0400

    progress

--- src/git_acp.mjs
+++ src/git_acp.mjs
@@ -1,8 +1,11 @@
+import { arguments_assert } from "./arguments_assert.mjs";
 import { git_add } from "./git_add.mjs";
 import { git_commit } from "./git_commit.mjs";
 import { git_push } from "./git_push.mjs";
+import { string_is } from "./string_is.mjs";
 
 export async function git_acp(message) {
+    arguments_assert(string_is)(message)
     for (let command in [
         git_add, 
         () => git_commit(message), 
