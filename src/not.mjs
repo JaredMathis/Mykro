2985ea1d7f7637b0af274cb17416eee836d6ad93
JaredMathis <jared.mathis@native.consulting>
2022-07-02 16:56:23 -0400

    whitespace

--- src/not.mjs
+++ src/not.mjs
@@ -5,7 +5,9 @@ import { boolean_is } from "./boolean_is.mjs";
 export function not(_function) {
     return function wrapped() {
         let result = function_call(_function, arguments);
-        assert(boolean_is, {_function_name:_function.name})(result);
+        assert(boolean_is, {
+            _function_name:_function.name
+        })(result);
         return !result;
     }
 }
