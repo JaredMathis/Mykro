ebbd2e18dac1dca566031ae1c27b88c535baa2a7
JaredMathis <jared.mathis@native.consulting>
2022-07-02 17:00:09 -0400

    progress

--- src/not.mjs
+++ src/not.mjs
@@ -5,10 +5,7 @@ import { boolean_is } from "./boolean_is.mjs";
 export function not(_function) {
     return function wrapped() {
         let result = function_call(_function, arguments);
-        assert(boolean_is, {
-            _function_name:_function.name,
-            arguments: Array.from(arguments)
-        })(result);
+        assert(boolean_is)(result);
         return !result;
     }
 }
