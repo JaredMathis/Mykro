6944983dd57199dcb995e3f2e80ea047349645f1
JaredMathis <jared.mathis@native.consulting>
2022-07-03 00:07:02 -0400

    lyrics

--- src/lyrics_database_get.mjs
+++ src/lyrics_database_get.mjs
@@ -3,6 +3,7 @@ import { arguments_assert } from "./arguments_assert.mjs";
 import { assert } from "./assert.mjs";
 import { equals } from "./equals.mjs";
 import { list_map } from "./list_map.mjs";
+import { string_split } from "./string_split.mjs";
 
 export async function lyrics_database_get() {
     await arguments_assert()(arguments);
@@ -47,8 +48,8 @@ export async function lyrics_database_get() {
         'grace saved me',
     ]
 
-    let propositions = list_map(proposition_sentences, s => {
-        let split = string_split(proposition_sentences, ' ');
+    let propositions = list_map(proposition_sentences, async s => {
+        let split = await string_split(s, ' ');
         assert(equals)(split.length, 3);
 
         return [{
