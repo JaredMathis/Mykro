fbcf32b5736ab8820bfd8373cf4b16b7e1a536a2
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:37:31 -0400

    file_js_new probability_is

--- src/random_with_probability.mjs
+++ src/random_with_probability.mjs
@@ -1,7 +1,8 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
-export async function random_with_probability() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function random_with_probability(probability) {
+    await arguments_assert(probability_is)(arguments);
+    
+    return Math.random() <= probability;
 }
