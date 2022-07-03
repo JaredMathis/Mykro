8373edd01785aec51d2feaa27f023656dcf99ea9
JaredMathis <jared.mathis@native.consulting>
2022-07-03 02:31:23 -0400

    echo progress

--- src/lyrics.mjs
+++ src/lyrics.mjs
@@ -1,13 +1,7 @@
 
 import _ from "lodash";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { list_map } from "./list_map.mjs";
-import { list_item_random } from "./list_item_random.mjs";
 import { lyrics_database_get } from "./lyrics_database_get.mjs";
-import { lyrics_sentence } from "./lyrics_sentence.mjs";
-import { list_where } from "./list_where.mjs";
-import { lyrics_group_is_about } from "./lyrics_group_is_about.mjs";
-import { random_number } from "./random_number.mjs";
 
 export async function lyrics() {
     await arguments_assert()(arguments);
--- src/lyrics_paragraph.mjs
+++ src/lyrics_paragraph.mjs
@@ -2,6 +2,12 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { defined_is } from "./defined_is.mjs";
 import { string_is } from "./string_is.mjs";
+import { list_map } from "./list_map.mjs";
+import { list_item_random } from "./list_item_random.mjs";
+import { lyrics_sentence } from "./lyrics_sentence.mjs";
+import { list_where } from "./list_where.mjs";
+import { lyrics_group_is_about } from "./lyrics_group_is_about.mjs";
+import { random_number } from "./random_number.mjs";
 
 export async function lyrics_paragraph(database, subject) {
     await arguments_assert(defined_is, string_is)(arguments);
