1c1436acfb98df34bdb96a2e6468d7d819fe9b75
JaredMathis <jared.mathis@native.consulting>
2022-07-02 20:19:04 -0400

    echo progress

--- src/web_search.mjs
+++ src/web_search.mjs
@@ -1,10 +1,15 @@
 import axios from 'axios';
 import { parse } from 'node-html-parser';
 export async function web_search(query) {
-    let result = await axios.get('https://duckduckgo.com/?q=' + query);
-    let parsed = parse(result.data);
+    let get = await axios.get('https://duckduckgo.com/?q=' + query);
+    let parsed = parse(get.data);
 
-    
+    let result = [];
+    tree_traverse(parsed, node => node.childNodes, node => {
+        if (node.rawTagName === 'a') {
+            result.push(node.toString());
+        }
+    })
 
-    return parsed.toString();
+    return result;
 }
