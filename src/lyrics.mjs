bde89f60e95dbef5a605d14874be4b52249f1e41
JaredMathis <jared.mathis@native.consulting>
2022-07-03 02:30:02 -0400

    file_js_new lyrics_paragraph

--- src/lyrics.mjs
+++ src/lyrics.mjs
@@ -18,20 +18,3 @@ export async function lyrics() {
     subject = 'me'
     return await lyrics_paragraph(database, subject);
 }
-
-async function lyrics_paragraph(database, subject) {
-    let groups_about_subject = await list_where(
-        database.groups,
-        async (g) => g.parent === subject || (await lyrics_group_is_about(subject))(g));
-
-    let group = await list_item_random(groups_about_subject);
-
-    let sentences_count = await random_number(1, 3);
-
-    let result = await list_map(_.range(0, sentences_count), async (i) => {
-        let item = await lyrics_sentence(group, database);
-        return item;
-    });
-
-    return result;
-}
---
+++ src/lyrics_paragraph.mjs
@@ -0,0 +1,7 @@
+
+import { arguments_assert } from "./arguments_assert.mjs";
+
+export async function lyrics_paragraph() {
+    await arguments_assert()(arguments);
+    // TODO
+}
