d3911cc1d1d42a68b197d0fcb80f14ccbc5f9e29
JaredMathis <jared.mathis@native.consulting>
2022-07-03 19:38:57 -0400

    echo progress

--- src/string_identifier_is.mjs
+++ src/string_identifier_is.mjs
@@ -2,10 +2,16 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { constant_alphabet } from "./constant_alphabet.mjs";
 import { defined_is } from "./defined_is.mjs";
+import { for_each } from "./for_each.mjs";
 
 export async function string_identifier_is(value) {
     await arguments_assert(defined_is)(arguments);
 
     let identifer_characters = await constant_alphabet() + `_`;
-    return identifer_characters
+
+    let result = true;
+    await for_each(range(string_size(value)), i => {
+    })
+
+    return result
 }
