443c338514ae8f9598d339f92d59358772d8072e
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:43:22 -0400

    echo progress

--- src/probability_is.mjs
+++ src/probability_is.mjs
@@ -6,5 +6,11 @@ import { defined_is } from "./defined_is.mjs";
 export async function probability_is(p) {
     await arguments_assert(defined_is)(arguments);
     
-    return _.isNumber(p) && number_negative_not_is(p) && number_at_most(p, 1);
+    let tests = [
+        _.isNumber(p),
+        number_negative_not_is(p),
+        number_at_most(p, 1),
+    ];
+
+    return _.some(list_where(tests, false_is));
 }
