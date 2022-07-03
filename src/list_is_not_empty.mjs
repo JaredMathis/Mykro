0061a9269dc2f2f7aab8d73725e45f2690a7fbbe
JaredMathis <jared.mathis@native.consulting>
2022-07-03 01:24:22 -0400

    file_js_new list_size

--- src/list_is_not_empty.mjs
+++ src/list_is_not_empty.mjs
@@ -1,9 +1,9 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
 import { list_is } from "./list_is.mjs";
+import { number_at_least } from "./number_at_least.mjs";
 
 export async function list_is_not_empty(list) {
     await arguments_assert(list_is)(arguments);
-
-
+    return number_at_least(list_size(list), 1);
 }
