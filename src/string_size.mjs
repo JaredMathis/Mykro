7bfe491d66b1f41cd3f32f4529c9400bdc594a90
JaredMathis <jared.mathis@native.consulting>
2022-07-03 19:43:11 -0400

    echo progress

--- src/string_size.mjs
+++ src/string_size.mjs
@@ -1,7 +1,9 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { string_is } from "./string_is.mjs";
 
-export async function string_size() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function string_size(s) {
+    await arguments_assert(string_is)(arguments);
+
+    return s.length;
 }
