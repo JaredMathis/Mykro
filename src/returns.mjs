56963dcd8bad65f5d1e76b18f9878661fbafc690
JaredMathis <jared.mathis@native.consulting>
2022-07-03 02:41:11 -0400

    echo unused import

--- src/returns.mjs
+++ src/returns.mjs
@@ -2,7 +2,6 @@ import {equals} from './equals.mjs';
 import {function_call} from './function_call.mjs';
 import {error} from './error.mjs';
 import _ from 'lodash';
-import { defined_is } from './defined_is.mjs';
 
 export function returns(_function, expected, data) {
     return async function wrapped() {
