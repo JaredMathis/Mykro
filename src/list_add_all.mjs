5f279f72faca83968945e1a290dba9320f3a9a5e
JaredMathis <jared.mathis@native.consulting>
2022-07-03 02:40:43 -0400

    echo progress

--- src/list_add_all.mjs
+++ src/list_add_all.mjs
@@ -1,7 +1,12 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { for_each } from "./for_each.mjs";
+import { list_is } from "./list_is.mjs";
 
-export async function list_add_all() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function list_add_all(list, other) {
+    await arguments_assert(list_is, list_is)(arguments);
+    
+    await for_each(other, o => {
+        await list_add(list, o);
+    })
 }
