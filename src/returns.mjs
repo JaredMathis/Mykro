98f17a52f0e13597c7c67f2729419af0aa5599bc
JaredMathis <jared.mathis@native.consulting>
2022-07-03 01:35:05 -0400

    echo progress

--- src/returns.mjs
+++ src/returns.mjs
@@ -1,11 +1,16 @@
 import {equals} from './equals.mjs';
 import {function_call} from './function_call.mjs';
 import {error} from './error.mjs';
+import _ from 'lodash';
+import { defined_is } from './defined_is.mjs';
 
 export function returns(_function, expected, data) {
     return async function wrapped() {
         let actual = await function_call(_function, arguments);
         if (!equals(actual, expected)) {
+            if (data?.value?.then) {
+                console.log(`data?.value?.then is a promise, maybe there is a missing await?`.yellow);
+            }
             error(JSON.stringify({
                 _function_data:_function.data || _function.name,
                 actual, 
