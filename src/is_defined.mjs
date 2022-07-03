15ef5b4ec034a1d8a22972573feb309735ce8c42
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:38:42 -0400

    echo progress

--- src/is_defined.mjs
+++ src/is_defined.mjs
@@ -1,7 +1,9 @@
 
+import _ from "lodash";
 import { arguments_assert } from "./arguments_assert.mjs";
+import { tautology } from "./tautology.mjs";
 
-export async function is_defined() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function is_defined(value) {
+    await arguments_assert(tautology)(arguments);
+    return !_.isUndefined(value);
 }
