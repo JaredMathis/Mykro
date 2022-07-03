556f8cb342216c061a40a248c2994707ca1198e1
JaredMathis <jared.mathis@native.consulting>
2022-07-02 20:53:08 -0400

    lyrics

--- src/list_item_random.mjs
+++ src/list_item_random.mjs
@@ -6,5 +6,6 @@ export async function list_item_random(list) {
     await arguments_assert(_.isArray)(arguments);
 
     let index = _.random(list.length)
+    console.log(index);
     return list[index];
 }
