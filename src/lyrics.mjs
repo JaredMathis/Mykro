4e16a6c9ccb58d0d9b4a9563081e734026d15eb0
JaredMathis <jared.mathis@native.consulting>
2022-07-03 01:01:18 -0400

    echo progress

--- src/lyrics.mjs
+++ src/lyrics.mjs
@@ -5,6 +5,7 @@ import { list_map } from "./list_map.mjs";
 import { list_item_random } from "./list_item_random.mjs";
 import { lyrics_database_get } from "./lyrics_database_get.mjs";
 import { lyrics_sentence } from "./lyrics_sentence.mjs";
+import { list_where } from "./list_where.mjs";
 
 export async function lyrics() {
     await arguments_assert()(arguments);
@@ -12,7 +13,7 @@ export async function lyrics() {
     let database = await lyrics_database_get();
 
     let subject = 'god';
-    let groups_about_god = _.filter(database.groups, { parent: subject });
+    let groups_about_god = await list_where(database.groups, g => g.parent === subject );
     let group = await list_item_random(groups_about_god);
 
     let sentences_count = 12;
