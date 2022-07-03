79f4723123b6f5636b813bde78ab75e70f6c3d62
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:38:15 -0400

    file_js_new is_defined

---
+++ src/is_defined.mjs
@@ -0,0 +1,7 @@
+
+import { arguments_assert } from "./arguments_assert.mjs";
+
+export async function is_defined() {
+    await arguments_assert()(arguments);
+    // TODO
+}
--- src/probability_is.mjs
+++ src/probability_is.mjs
@@ -1,7 +1,8 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { not } from "./not.mjs";
 
-export async function probability_is() {
-    await arguments_assert()(arguments);
+export async function probability_is(p) {
+    await arguments_assert(not(_.isUndefined))(arguments);
     // TODO
 }
