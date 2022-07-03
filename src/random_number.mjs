ece32c6f8fe7a5901153dd529c8e69fd1dbb70c8
JaredMathis <jared.mathis@native.consulting>
2022-07-03 02:27:11 -0400

    echo progress

--- src/random_number.mjs
+++ src/random_number.mjs
@@ -1,7 +1,7 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
-export async function random_number() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function random_number(min, max) {
+    await arguments_assert(number_is, number_is)(arguments);
+    return _.random(min, max);
 }
