8e468db6f6b777b1d8bbf2081dad8a564de7d602
JaredMathis <jared.mathis@native.consulting>
2022-07-03 01:55:37 -0400

    echo progress

--- src/equals_json.mjs
+++ src/equals_json.mjs
@@ -1,7 +1,8 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { defined_is } from "./defined_is.mjs";
 
-export async function equals_json() {
-    await arguments_assert()(arguments);
+export async function equals_json(a, b) {
+    await arguments_assert(defined_is, defined_is)(arguments);
     // TODO
 }
