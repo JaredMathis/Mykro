603abbb2ac0e084d0eea5319eaa8508af439b53c
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:53:03 -0400

    echo progress

--- src/false_is.mjs
+++ src/false_is.mjs
@@ -1,7 +1,9 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { defined_is } from "./defined_is.mjs";
+import { equals } from "./equals.mjs";
 
-export async function false_is() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function false_is(value) {
+    await arguments_assert(defined_is)(arguments);
+    return equals(value, false);
 }
