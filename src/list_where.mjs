8a9150691ff963d4c961a236027b2ef34bc0f407
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:50:21 -0400

    echo progress

--- src/list_where.mjs
+++ src/list_where.mjs
@@ -1,7 +1,8 @@
 
+import _ from "lodash";
 import { arguments_assert } from "./arguments_assert.mjs";
 
-export async function list_where() {
-    await arguments_assert()(arguments);
+export async function list_where(list, predicate) {
+    await arguments_assert(_.isArray, _.isFunction)(arguments);
     // TODO
 }
