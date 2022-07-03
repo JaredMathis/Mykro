c2264d4bcd23b78c21b19617fd5d5857ad78a866
JaredMathis <jared.mathis@native.consulting>
2022-07-03 01:23:34 -0400

    echo progress

--- src/list_is.mjs
+++ src/list_is.mjs
@@ -1,7 +1,9 @@
 
+import _ from "lodash";
 import { arguments_assert } from "./arguments_assert.mjs";
+import { defined_is } from "./defined_is.mjs";
 
-export async function list_is() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function list_is(value) {
+    await arguments_assert(defined_is)(arguments);
+    return _.isArray(value);
 }
--- src/list_is_not_empty.mjs
+++ src/list_is_not_empty.mjs
@@ -1,7 +1,8 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
-export async function list_is_not_empty() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function list_is_not_empty(list) {
+    await arguments_assert(list_is)(arguments);
+
+
 }
