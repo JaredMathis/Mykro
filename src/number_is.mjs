eb35a32454053c6943beb025e61f6fb2963f9041
JaredMathis <jared.mathis@native.consulting>
2022-07-03 02:27:46 -0400

    echo progress

--- src/number_is.mjs
+++ src/number_is.mjs
@@ -1,7 +1,9 @@
 
+import _ from "lodash";
 import { arguments_assert } from "./arguments_assert.mjs";
+import { defined_is } from "./defined_is.mjs";
 
-export async function number_is() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function number_is(value) {
+    await arguments_assert(defined_is)(arguments);
+    return _.isNumber(value);
 }
