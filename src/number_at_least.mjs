00b59987a92283f9154dd6a48ed86c8d968c89a9
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:47:17 -0400

    echo progress

--- src/number_at_least.mjs
+++ src/number_at_least.mjs
@@ -1,7 +1,8 @@
 
+import _ from "lodash";
 import { arguments_assert } from "./arguments_assert.mjs";
 
-export async function number_at_least() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function number_at_least(value, minimum) {
+    await arguments_assert(_.isNumber, _.isNumber)(arguments);
+    return value >= minimum
 }
