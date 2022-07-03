54354276f59de3a4ca42ea5725c9e3b9709ab73d
JaredMathis <jared.mathis@native.consulting>
2022-07-02 20:32:52 -0400

    echo progress

--- src/run.mjs
+++ src/run.mjs
@@ -1,6 +1,7 @@
 
 import readline from 'readline';
 import { command_line } from './command_line.mjs';
+import 'colors';
 
 var rl = readline.createInterface({
     input: process.stdin,
@@ -9,7 +10,11 @@ var rl = readline.createInterface({
 });
 
 rl.on('line', async (line) => {
-    let result = await command_line(`node src/run_function.mjs ` + line);
-    console.log(result.stdout);
+    try {
+        let result = await command_line(`node src/run_function.mjs ` + line);
+        console.log(result.stdout);
+    } catch (e) {
+        console.log(e.stack.red);
+    }
 })
 
