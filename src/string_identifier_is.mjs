59221523066bee64c13fbfb8fbe512e796c1774d
JaredMathis <jared.mathis@native.consulting>
2022-07-03 19:35:21 -0400

    file_js_new string_identifier_is

---
+++ src/string_identifier_is.mjs
@@ -0,0 +1,7 @@
+
+import { arguments_assert } from "./arguments_assert.mjs";
+
+export async function string_identifier_is() {
+    await arguments_assert()(arguments);
+    // TODO
+}
--- src/string_search_matches.mjs
+++ src/string_search_matches.mjs
@@ -1,7 +1,11 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { string_is } from "./string_is.mjs";
+import { string_split } from "./string_split.mjs";
 
-export async function string_search_matches() {
-    await arguments_assert()(arguments);
+export async function string_search_matches(candidate, query) {
+    await arguments_assert(string_is, string_is)(arguments);
+
+    let split = string_split(query, '');
     // TODO
 }
