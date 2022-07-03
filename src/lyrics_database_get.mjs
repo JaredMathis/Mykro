5f6ea2d955b2f3647573a8ce93bf6a7fcbc28207
JaredMathis <jared.mathis@native.consulting>
2022-07-03 00:04:22 -0400

    echo progress

--- src/lyrics_database_get.mjs
+++ src/lyrics_database_get.mjs
@@ -1,5 +1,8 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { assert } from "./assert.mjs";
+import { equals } from "./equals.mjs";
+import { list_map } from "./list_map.mjs";
 
 export async function lyrics_database_get() {
     await arguments_assert()(arguments);
@@ -40,11 +43,20 @@ export async function lyrics_database_get() {
         ]
     };
 
-    let propositions = [{
-        nouns: ['grace'],
-        verbs: ['saved'],
-        objects: ['me'],
-    }];
+    let proposition_sentences = [
+        'grace saved me',
+    ]
+
+    let propositions = list_map(proposition_sentences, s => {
+        let split = string_split(proposition_sentences, ' ');
+        assert(equals)(split.length, 3);
+
+        return [{
+            nouns: [split[0]],
+            verbs: [split[1]],
+            objects: [split[2]],
+        }]
+    });
 
     let groups = [
         group_god_attributes,
