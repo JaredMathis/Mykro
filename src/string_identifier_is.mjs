a3d1fb78be71e79b07101c1974fff43d6576f65d
JaredMathis <jared.mathis@native.consulting>
2022-07-03 19:37:28 -0400

    string_identifier_is a

--- src/string_identifier_is.mjs
+++ src/string_identifier_is.mjs
@@ -1,9 +1,11 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { constant_alphabet } from "./constant_alphabet.mjs";
 import { defined_is } from "./defined_is.mjs";
 
 export async function string_identifier_is(value) {
     await arguments_assert(defined_is)(arguments);
 
-    let identifer_characters = `qwertyuiopasdfghjklzxcvbnm_`;
+    let identifer_characters = await constant_alphabet() + `_`;
+    return identifer_characters
 }
