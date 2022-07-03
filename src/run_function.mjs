a3e3adfb81f9c8d96dca482c6715c11813efc6bd
JaredMathis <jared.mathis@native.consulting>
2022-07-02 20:02:42 -0400

    echo console-log

--- src/run_function.mjs
+++ src/run_function.mjs
@@ -3,5 +3,7 @@ import { arguments_assert } from "./arguments_assert.mjs";
 
 export async function run_function() {
     await arguments_assert()(arguments);
+
+    console.log(process.argv);
     // TODO
 }
