31c9b80f61dfe4ec9999de5b4caaa27a55180744
JaredMathis <jared.mathis@native.consulting>
2022-07-03 02:34:31 -0400

    lyrics

--- src/lyrics_paragraph.mjs
+++ src/lyrics_paragraph.mjs
@@ -8,6 +8,7 @@ import { lyrics_sentence } from "./lyrics_sentence.mjs";
 import { list_where } from "./list_where.mjs";
 import { lyrics_group_is_about } from "./lyrics_group_is_about.mjs";
 import { random_number } from "./random_number.mjs";
+import _ from "lodash";
 
 export async function lyrics_paragraph(database, subject) {
     await arguments_assert(defined_is, string_is)(arguments);
@@ -20,7 +21,7 @@ export async function lyrics_paragraph(database, subject) {
 
     let group = await list_item_random(groups_about_subject);
 
-    let sentences_count = 12;
+    let sentences_count = await random_number(1, 3);
 
     let result = await list_map(_.range(0, sentences_count), async (i) => {
         let item = await lyrics_sentence(group, database);
