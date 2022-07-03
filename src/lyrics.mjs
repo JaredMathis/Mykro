b3b507f03da938a8c13783eb42d4fdbfb7fc84a1
JaredMathis <jared.mathis@native.consulting>
2022-07-03 02:45:30 -0400

    echo missing async

--- src/lyrics.mjs
+++ src/lyrics.mjs
@@ -6,6 +6,7 @@ import { list_size } from "./list_size.mjs";
 import { lyrics_database_get } from "./lyrics_database_get.mjs";
 import { lyrics_paragraph } from "./lyrics_paragraph.mjs";
 import { list_add_all } from "./list_add_all.mjs";
+import { list_where } from "./list_where.mjs";
 
 export async function lyrics() {
     await arguments_assert()(arguments);
@@ -19,6 +20,11 @@ export async function lyrics() {
         let subject = await list_item_random(subjects);
         let paragraph = await lyrics_paragraph(database, subject);
         await list_add_all(result, paragraph);
+        
+        // Remove duplicate sentences
+        result = _.uniq(result);
+        
+        result = await list_where(result, item => item !== 'i');
     }
 
     return result;
