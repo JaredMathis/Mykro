9d11682470305d785244bbefa802297bbfb78f38
JaredMathis <jared.mathis@native.consulting>
2022-07-02 20:05:25 -0400

    echo progress

--- src/run_function.mjs
+++ src/run_function.mjs
@@ -1,9 +1,1 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function run_function() {
-    await arguments_assert()(arguments);
-
     console.log(process.argv);
-    // TODO
-}
