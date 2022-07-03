42f85ac0ca0e99ccfd71d07cc3c728d43cfa6b9c
JaredMathis <jared.mathis@native.consulting>
2022-07-03 03:05:47 -0400

    lyrics

--- src/lyrics.mjs
+++ src/lyrics.mjs
@@ -30,7 +30,7 @@ export async function lyrics() {
         // Remove duplicate sentences
         // result = _.uniq(result);
         
-        result = await list_where(result, tautology);
+        result = await list_where(result, async i => tautology(i));
     }) ;
 
     return result;
