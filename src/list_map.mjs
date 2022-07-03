62632138dd03a8370106316fc121a6fc2ae29fdd
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:29:21 -0400

    echo progress

--- src/list_map.mjs
+++ src/list_map.mjs
@@ -1,7 +1,15 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { for_each } from "./for_each.mjs";
 
-export async function list_map() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function list_map(list, mapper) {
+    await arguments_assert(_.isArray, _.isFunction)(arguments);
+
+    let result = [];
+    for_each(list, async item => {
+        let mapped = await mapper(item);
+        result.push(mapped);
+    })
+
+    return result;
 }
