192d5675615accde7754d8812ca77bbc25099b91
JaredMathis <jared.mathis@native.consulting>
2022-07-02 20:23:58 -0400

    echo progress

--- src/tree_traverse.mjs
+++ src/tree_traverse.mjs
@@ -5,5 +5,5 @@ import { not } from "./not.mjs";
 export async function tree_traverse(root, children_get) {
     await arguments_assert(not(_.isUndefined), _.isFunction)(arguments);
 
-
+    let children = await children_get(root);
 }
