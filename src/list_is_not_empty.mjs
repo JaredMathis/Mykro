cf16f7b3e9d9ad1bf278a872b71fd90d7742ba93
JaredMathis <jared.mathis@native.consulting>
2022-07-03 01:25:07 -0400

    echo progress

--- src/list_is_not_empty.mjs
+++ src/list_is_not_empty.mjs
@@ -2,6 +2,7 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { list_is } from "./list_is.mjs";
 import { number_at_least } from "./number_at_least.mjs";
+import { list_size } from "./list_size.mjs";
 
 export async function list_is_not_empty(list) {
     await arguments_assert(list_is)(arguments);
