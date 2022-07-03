22ac9d97250c31278de33c3423dfe296e987a4e1
JaredMathis <jared.mathis@native.consulting>
2022-07-03 02:39:54 -0400

    file_js_new list_add_all

--- src/lyrics.mjs
+++ src/lyrics.mjs
@@ -2,6 +2,7 @@
 import _ from "lodash";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { list_item_random } from "./list_item_random.mjs";
+import { list_size } from "./list_size.mjs";
 import { lyrics_database_get } from "./lyrics_database_get.mjs";
 import { lyrics_paragraph } from "./lyrics_paragraph.mjs";
 
@@ -10,7 +11,12 @@ export async function lyrics() {
 
     let database = await lyrics_database_get();
 
-    let subjects = ['god', 'me'];
-    let subject = await list_item_random(subjects);
-    return await lyrics_paragraph(database, subject);
+    let result = [];
+
+    while (await list_size(result) < 12) {
+        let subjects = ['god', 'me'];
+        let subject = await list_item_random(subjects);
+        let paragraph = await lyrics_paragraph(database, subject);
+        list_add_all(result, paragraph);
+    }
 }
