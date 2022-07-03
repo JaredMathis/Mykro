a221594897e2dc3f11731de684afa8ce51369f1e
JaredMathis <jared.mathis@native.consulting>
2022-07-02 21:04:30 -0400

    echo refactor

--- src/lyrics.mjs
+++ src/lyrics.mjs
@@ -5,21 +5,25 @@ import { list_item_random } from "./list_item_random.mjs";
 export async function lyrics() {
     await arguments_assert()(arguments);
 
-    let adjectives = [
-        "amazing",
-        "unending"
-    ];
-    let nouns = [
-        "grace", 
-        "love"
-    ]
+    let group = {
+        adjectives: [
+            "amazing",
+            "unending"
+        ],
+        nouns: [
+            "grace", 
+            "love"
+        ],
+    }
 
     let probability_sentence = .9;
+
+    let {adjectives} = group;
+    let {nouns} = group;
 
     let adjective = await list_item_random(adjectives);
     let noun = await list_item_random(nouns);
     let result = `${adjective} ${noun}!`;
-
     
     return result
 }
