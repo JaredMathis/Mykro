61d3f34b3ba05aed350b950ff9f714198d4b54a8
JaredMathis <jared.mathis@native.consulting>
2022-07-02 20:50:26 -0400

    echo progress

--- src/lyrics.mjs
+++ src/lyrics.mjs
@@ -4,5 +4,9 @@ import { arguments_assert } from "./arguments_assert.mjs";
 export async function lyrics() {
     await arguments_assert()(arguments);
 
+    let adjectives = ["Amazing"];
+
+    
+
     return "Amazing grace!";
 }
