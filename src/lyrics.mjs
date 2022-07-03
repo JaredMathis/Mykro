bdf49cff5601f18545e130185ca5a34124b8d70a
JaredMathis <jared.mathis@native.consulting>
2022-07-03 02:28:46 -0400

    echo progress

--- src/lyrics.mjs
+++ src/lyrics.mjs
@@ -7,6 +7,7 @@ import { lyrics_database_get } from "./lyrics_database_get.mjs";
 import { lyrics_sentence } from "./lyrics_sentence.mjs";
 import { list_where } from "./list_where.mjs";
 import { lyrics_group_is_about } from "./lyrics_group_is_about.mjs";
+import { random_number } from "./random_number.mjs";
 
 export async function lyrics() {
     await arguments_assert()(arguments);
@@ -21,7 +22,7 @@ export async function lyrics() {
 
     let group = await list_item_random(groups_about_subject);
 
-    let sentences_count = ;
+    let sentences_count = await random_number(1, 3);
 
     let result = await list_map(_.range(0, sentences_count), async i => {
         let item = await lyrics_sentence(group, database);
