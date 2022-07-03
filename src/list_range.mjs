dd298e148bdb267a705ab021b4a4be6a71814d9e
JaredMathis <jared.mathis@native.consulting>
2022-07-03 19:41:21 -0400

    echo progress

--- src/list_range.mjs
+++ src/list_range.mjs
@@ -1,7 +1,14 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { for_each } from "./for_each.mjs";
+import { number_negative_not_is } from "./number_negative_not_is.mjs";
 
-export async function list_range() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function list_range(count) {
+    await arguments_assert(number_negative_not_is)(arguments);
+
+    let result = [];
+    for (let i = 0; i < count; i++) {
+
+    }
+    return result;
 }
