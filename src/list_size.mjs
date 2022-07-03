b1d0aed3219e1beb24cc5e40cf7b2fd29d973e05
JaredMathis <jared.mathis@native.consulting>
2022-07-03 01:24:41 -0400

    echo progress

--- src/list_size.mjs
+++ src/list_size.mjs
@@ -1,7 +1,8 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { list_is } from "./list_is.mjs";
 
-export async function list_size() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function list_size(list) {
+    await arguments_assert(list_is)(arguments);
+    return list.length;
 }
