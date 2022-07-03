4d9ea884aace2f6f78e670466121d8312f5c011e
JaredMathis <jared.mathis@native.consulting>
2022-07-02 20:56:12 -0400

    echo progress

--- src/lyrics.mjs
+++ src/lyrics.mjs
@@ -5,7 +5,8 @@ import { list_item_random } from "./list_item_random.mjs";
 export async function lyrics() {
     await arguments_assert()(arguments);
 
-    let adjectives = ["Amazing", "Unending"];
+    let adjectives = ["amazing", "unending"];
+    let nouns = ["grace"]
 
     let choice = await list_item_random(adjectives);
 
