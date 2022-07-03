0994b116335b957d391e5f4c4d9a6a5db36f9cc6
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:56:16 -0400

    echo rename

--- src/probability_is.mjs
+++ src/probability_is.mjs
@@ -11,11 +11,11 @@ import { true_is } from "./true_is.mjs";
 export async function probability_is(p) {
     await arguments_assert(defined_is)(arguments);
     
-    let tests = [
+    let requirements = [
         _.isNumber(p),
         await number_negative_not_is(p),
         await number_at_most(p, 1),
     ];
 
-    return _.every(list_where(tests, true_is));
+    return _.every(list_where(requirements, true_is));
 }
