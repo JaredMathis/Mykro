4e0194bc9b686d05df23f5e0cbec70927d770c0d
JaredMathis <jared.mathis@native.consulting>
2022-07-03 01:40:57 -0400

    echo whitespace

--- src/lyrics_sentence.mjs
+++ src/lyrics_sentence.mjs
@@ -13,10 +13,12 @@ export async function lyrics_sentence(group, database) {
     let noun = await list_item_random(group.nouns);
     let adjective = ``;
     
+
     if (await random_with_probability(0.5)) {
         adjective = await list_item_random(group.adjectives);
         adjective += ' ';
     }
+    
     let noun_clause = `${adjective}${noun}`;
 
     let result = noun_clause;
