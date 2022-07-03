e073713372cdd1113487cb29760c0f2859d7ce4c
JaredMathis <jared.mathis@native.consulting>
2022-07-02 21:31:14 -0400

    file_js_new lyrics_sentence_exclamation_sub_how

---
+++ src/lyrics_sentence_exclamation_sub_how.mjs
@@ -0,0 +1,7 @@
+
+import { arguments_assert } from "./arguments_assert.mjs";
+
+export async function lyrics_sentence_exclamation_sub_how() {
+    await arguments_assert()(arguments);
+    // TODO
+}
