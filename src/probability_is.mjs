eaf0bc69bd9b1804df161bd2862bac9350de4691
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:41:11 -0400

    echo progress

--- src/probability_is.mjs
+++ src/probability_is.mjs
@@ -1,8 +1,10 @@
 
+import _ from "lodash";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { defined_is } from "./defined_is.mjs";
 
 export async function probability_is(p) {
     await arguments_assert(defined_is)(arguments);
-    // TODO
+    
+    return _.isNumber(p) && number_negative_not_is(p) && number_at_most(p, 1);
 }
