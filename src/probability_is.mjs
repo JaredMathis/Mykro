455c0fe4eb356f3168041490e390b45896a3dfd5
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:53:39 -0400

    echo missing import

--- src/probability_is.mjs
+++ src/probability_is.mjs
@@ -5,6 +5,7 @@ import { defined_is } from "./defined_is.mjs";
 import { number_at_most } from "./number_at_most.mjs";
 import { number_negative_not_is } from "./number_negative_not_is.mjs";
 import { list_where } from "./list_where.mjs";
+import { false_is } from "./false_is.mjs";
 
 export async function probability_is(p) {
     await arguments_assert(defined_is)(arguments);
