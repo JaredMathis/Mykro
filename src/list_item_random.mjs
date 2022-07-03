9e9cbc1cac9e7c43f58434094cbcc06a3c358032
JaredMathis <jared.mathis@native.consulting>
2022-07-03 01:25:52 -0400

    echo progress

--- src/list_item_random.mjs
+++ src/list_item_random.mjs
@@ -1,9 +1,10 @@
 
 import _ from "lodash";
 import { arguments_assert } from "./arguments_assert.mjs";
+import { list_is_not_empty } from "./list_is_not_empty.mjs";
 
 export async function list_item_random(list) {
-    await arguments_assert(_.isArray)(arguments);
+    await arguments_assert(list_is_not_empty)(arguments);
     let index = _.random(0, list.length - 1)
     return list[index];
 }
