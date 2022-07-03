ce5e8a720b705e0f16b78c0a1f54b21d938517bb
JaredMathis <jared.mathis@native.consulting>
2022-07-02 22:45:14 -0400

    echo progress

--- src/number_negative_not_is.mjs
+++ src/number_negative_not_is.mjs
@@ -1,7 +1,8 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
-export async function number_negative_not_is() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function number_negative_not_is(value) {
+    await arguments_assert(_.isNumber)(arguments);
+    
+    return number_at_least(value, 0);
 }
