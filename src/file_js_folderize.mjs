1356fd9a2832abc989a8a44c0bedc57297918234
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:23:37 -0400

    fjargad file_js_folderize file_name string_identifier_is

--- src/file_js_folderize.mjs
+++ src/file_js_folderize.mjs
@@ -1,7 +1,4 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function file_js_folderize() {
-    await arguments_assert()(arguments);
-    // TODO
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function file_js_folderize(file_name) {
+  await arguments_assert(string_identifier_is)(arguments);
 }
