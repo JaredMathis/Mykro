202510ce14b31eba23589fb25facacd16c433954
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:59:51 -0400

    fjg test a a

--- src/file_js_argument_add.mjs
+++ src/file_js_argument_add.mjs
@@ -2,6 +2,7 @@ import {arguments_assert} from './arguments_assert.mjs';
 import {string_is} from './string_is.mjs';
 import {file_js_parse} from './file_js_parse.mjs';
 import { es_function_exported } from './es_function_exported.mjs';
+import { property_get } from './property_get.mjs';
 
 export async function file_js_argument_add(function_name, argument_name, argument_type) {
     await arguments_assert(string_is, string_is, string_is)(arguments)
@@ -9,5 +10,7 @@ export async function file_js_argument_add(function_name, argument_name, argumen
     let parsed = await file_js_parse(function_name);
     let function_exported = await es_function_exported(parsed);
 
-    return function_exported;
+    let declaration = await property_get(function_exported, 'declaration');
+
+    return declaration;
 }
