448ee6f90039bb7c7d67a42b06fff6825e0fbcc0
JaredMathis <jared.mathis@native.consulting>
2022-07-04 16:23:25 -0400

    noop refactor move code

--- src/file_js_all_for_each.mjs
+++ src/file_js_all_for_each.mjs
@@ -7,10 +7,10 @@ import { for_each } from './for_each.mjs';
 export async function file_js_all_for_each(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
 
-  await file_js_run(function_name, [], error, error, error, error);
-
   let files = await file_js_all();
   await for_each(files, async file => {
+
+    await file_js_run(function_name, [], error, error, error, error);
 
   });
 
