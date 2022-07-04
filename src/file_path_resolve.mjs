cf7ac3155b6492d3b24f6f2a032ad1d0cbd7fbd9
JaredMathis <jared.mathis@native.consulting>
2022-07-04 16:10:50 -0400

    fjargad file_path_resolve file_path string_is

--- src/file_path_resolve.mjs
+++ src/file_path_resolve.mjs
@@ -1,7 +1,5 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function file_path_resolve() {
-    await arguments_assert()(arguments);
-    // TODO
+import path from "path";
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function file_path_resolve(file_path) {
+  await arguments_assert(string_is)(arguments);
 }
