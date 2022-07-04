1a6a72133a3335a71146d078786b28fa23a887c0
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:57:57 -0400

    echo progress

--- src/file_js_imports_get.mjs
+++ src/file_js_imports_get.mjs
@@ -6,6 +6,8 @@ export async function file_js_imports_get(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
   let ast = await file_js_parse(function_name);
   let body = await property_get(ast, 'body');
-  let result = await list_where(body, async b => equals(await property_get(b, 'type'), 'ImportDeclaration'))
+  let result = await list_where(
+    body, 
+    async b => equals(await property_get(b, 'type'), 'ImportDeclaration'))
   return result;
 }
