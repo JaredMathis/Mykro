5624f657b247039c2718e4f93b145ccaaad75b54
JaredMathis <jared.mathis@native.consulting>
2022-07-04 18:03:55 -0400

    fjsargad js_log_error e defined_is

--- src/js_log_error.mjs
+++ src/js_log_error.mjs
@@ -1,7 +1,5 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function js_log_error() {
-    await arguments_assert()(arguments);
-    console.log(e.stack.red);
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function js_log_error(e) {
+  await arguments_assert(defined_is)(arguments);
+  console.log(e.stack.red);
 }
