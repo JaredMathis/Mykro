ecfda043347b1bcfbfeaca521772815d8d32074d
JaredMathis <jared.mathis@native.consulting>
2022-07-03 21:11:59 -0400

    echo progress

--- src/file_js_parse.mjs
+++ src/file_js_parse.mjs
@@ -1,7 +1,8 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { string_is } from "./string_is.mjs";
 
-export async function file_js_parse() {
-    await arguments_assert()(arguments);
+export async function file_js_parse(file_path) {
+    await arguments_assert(string_is)(arguments);
     // TODO
 }
