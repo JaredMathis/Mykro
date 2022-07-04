08d36e189dd669f8eab956db8ed0a8b1bb887a72
JaredMathis <jared.mathis@native.consulting>
2022-07-04 03:13:22 -0400

    fjg test b defined_is

--- src/file_js_argument_add.mjs
+++ src/file_js_argument_add.mjs
@@ -5,6 +5,11 @@ import { es_function_exported } from './es_function_exported.mjs';
 import { property_get } from './property_get.mjs';
 import { string_identifier_is } from './string_identifier_is.mjs';
 import { list_add } from './list_add.mjs';
+import { list_where } from './list_where.mjs';
+import { number_at_least } from './number_at_least.mjs';
+import { list_size } from './list_size.mjs';
+import { assert } from './assert.mjs';
+import { equals } from './equals.mjs';
 
 export async function file_js_argument_add(function_name, argument_name, argument_type) {
     await arguments_assert(string_is, string_identifier_is, string_is)(arguments)
@@ -22,6 +27,10 @@ export async function file_js_argument_add(function_name, argument_name, argumen
 
 async function es_function_declaration_param_add(declaration, argument_name) {
     let params = await property_get(declaration, 'params');
+
+    let existing = await list_where(params, p => p.type === 'Identifier' && p.name === argument_name);
+
+    await assert(equals)(await list_size(existing), 0)
 
     await list_add(params, { type: 'Identifier', name: argument_name });
 }
