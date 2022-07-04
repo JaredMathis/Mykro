9d5ced1b119487462ef1d9e23409b3d6042e50c9
JaredMathis <jared.mathis@native.consulting>
2022-07-04 06:28:04 -0400

    fjargad list_contains item defined_is

--- src/list_contains.mjs
+++ src/list_contains.mjs
@@ -1,4 +1,4 @@
 import {arguments_assert} from "./arguments_assert.mjs";
-export async function list_contains(list) {
-  await arguments_assert(list_is)(arguments);
+export async function list_contains(list, item) {
+  await arguments_assert(list_is, defined_is)(arguments);
 }
