e51d5578cba35057f70ee16b4b52ad7f190e3218
JaredMathis <jared.mathis@native.consulting>
2022-07-04 03:49:44 -0400

    fjg test b defined_is

--- src/file_js_argument_add.mjs
+++ src/file_js_argument_add.mjs
@@ -32,9 +32,9 @@ export async function file_js_argument_add(function_name, argument_name, argumen
     let awaited_first_arguments = await property_get(awaited_first, 'arguments');
     await assert(equals)(await json_to(awaited_first_arguments), "[{\"type\":\"Identifier\",\"name\":\"arguments\"}]")
     let awaited_first_callee = await property_get(awaited_first, 'callee');
+    let expected_function_name = arguments_assert.name;
     await assert(equals)(await property_get(awaited_first_callee, 'type'), 'CallExpression');
     let awaited_first_callee_callee = await property_get(awaited_first_callee, 'callee');
-    let expected_function_name = arguments_assert.name;
     await assert(equals)(
         await json_to(awaited_first_callee_callee), 
         `{\"type\":\"Identifier\",\"name\":\"${expected_function_name}\"}`);
