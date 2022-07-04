3bf2c985a819a2036dc482e24b3091c55da1cb4d
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:20:38 -0400

    echo progress

--- src/file_js_argument_add.mjs
+++ src/file_js_argument_add.mjs
@@ -2,10 +2,10 @@ import arguments_assert from './arguments_assert.mjs';
 import string_is from './string_is.mjs';
 import file_js_parse from './file_js_parse.mjs';
 
-export function file_js_argument_add(file_path, argument_name) {
+export function file_js_argument_add(function_name, argument_name) {
     arguments_assert(string_is, string_is)(arguments)
 
-    let parsed = await file_js_parse(file_path);
+    let parsed = await file_js_parse(function_name);
 
-    
+
 }
