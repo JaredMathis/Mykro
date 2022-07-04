f885c41c63e7b3b3a390048cf3e74c9f8c52c9d2
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:17:07 -0400

    echo missing import

--- src/list_remove_at.mjs
+++ src/list_remove_at.mjs
@@ -1,5 +1,6 @@
 import {arguments_assert} from "./arguments_assert.mjs";
 import { list_is } from "./list_is.mjs";
+import { number_is } from "./number_is.mjs";
 export async function list_remove_at(list, index) {
   await arguments_assert(list_is, number_is)(arguments);
   list.splice(index, 1);
