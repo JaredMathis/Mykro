07456d80485d4be765af3f6b41a945cb94ffe294
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:45:13 -0400

    echo added missing await

--- src/not.mjs
+++ src/not.mjs
@@ -5,7 +5,7 @@ import { boolean_is } from "./boolean_is.mjs";
 export function not(_function) {
     return async function wrapped() {
         let result = await function_call(_function, arguments);
-        assert(boolean_is)(result);
+        await assert(boolean_is)(result);
         return !result;
     }
 }
