dc8d31b813e5356112188ecf3df3d8327957e27c
JaredMathis <jared.mathis@native.consulting>
2022-07-03 20:12:51 -0400

    echo progress

--- src/list_index_of_starting_at.mjs
+++ src/list_index_of_starting_at.mjs
@@ -1,7 +1,8 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
-export async function list_index_of_starting_at() {
+export async function list_index_of_starting_at(list, item, starting_at) {
     await arguments_assert()(arguments);
-    // TODO
+
+
 }
