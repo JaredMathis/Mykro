3d64e6280db7093c4425ae19c702da225280be13
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:15:17 -0400

    echo progress

--- src/file_js_parse.mjs
+++ src/file_js_parse.mjs
@@ -1,9 +1,11 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
 import { string_is } from "./string_is.mjs";
-import {Parser} from "acorn"
+import { file_read } from "./file_read.mjs";
 
 export async function file_js_parse(file_path) {
     await arguments_assert(string_is)(arguments);
-    // TODO
+    
+    let unparsed = await file_read(file_path);
+    let parsed = await es_parse(unparsed);
 }
