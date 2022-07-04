8c64ee929a9678b42158db7a6e9a438aefd76a59
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:17:13 -0400

    fjgrv test a

--- src/test.mjs
+++ src/test.mjs
@@ -1,5 +1,5 @@
 import {arguments_assert} from "./arguments_assert.mjs";
 import {defined_is} from "./defined_is.mjs";
-export async function test(a, b) {
-  await arguments_assert(defined_is, defined_is)(arguments);
+export async function test(b) {
+  await arguments_assert(defined_is)(arguments);
 }
