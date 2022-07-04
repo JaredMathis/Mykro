efb0aa8ee3ce1465e39a1afd29a40a671428b232
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:03:52 -0400

    fjagd list_remove item defined_is

--- src/list_remove.mjs
+++ src/list_remove.mjs
@@ -1,8 +1,7 @@
 import {arguments_assert} from "./arguments_assert.mjs";
-import { list_index_of } from "./list_index_of.mjs";
-import { list_is } from "./list_is.mjs";
-export async function list_remove(list) {
-  await arguments_assert(list_is)(arguments);
-
+import {list_index_of} from "./list_index_of.mjs";
+import {list_is} from "./list_is.mjs";
+export async function list_remove(list, item) {
+  await arguments_assert(list_is, defined_is)(arguments);
   await list_index_of(list, item);
 }
