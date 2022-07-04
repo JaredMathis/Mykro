2f1a41b30feffa6a8a853332f6f7681be19698ea
JaredMathis <jared.mathis@native.consulting>
2022-07-04 04:55:04 -0400

    echo rename

--- src/file_js_imports_missing_add.mjs
+++ src/file_js_imports_missing_add.mjs
@@ -1,4 +1,4 @@
 import {arguments_assert} from "./arguments_assert.mjs";
-export async function file_js_imports_add() {
+export async function file_js_imports_missing_add() {
   await arguments_assert()(arguments);
 }
