fb82e6ccb387b5bcceaafaf11aa59d455f1ac574
JaredMathis <jared.mathis@native.consulting>
2022-07-04 16:19:08 -0400

    noop refactor add function call

--- src/file_js_all_for_each.mjs
+++ src/file_js_all_for_each.mjs
@@ -1,10 +1,12 @@
 import {file_js_all} from './file_js_all.mjs';
 import {string_identifier_is} from './string_identifier_is.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
+import { for_each } from './for_each.mjs';
 export async function file_js_all_for_each(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
 
   let files = await file_js_all();
+  for_each();
 
   return files;
 }
