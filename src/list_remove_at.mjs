fc98fdb01346fd065ac0d86561414e85635dd5d0
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:12:19 -0400

    fjagad list_remove_at index number_is

--- src/list_remove_at.mjs
+++ src/list_remove_at.mjs
@@ -1,4 +1,4 @@
 import {arguments_assert} from "./arguments_assert.mjs";
-export async function list_remove_at(list) {
-  await arguments_assert(list_is)(arguments);
+export async function list_remove_at(list, index) {
+  await arguments_assert(list_is, number_is)(arguments);
 }
