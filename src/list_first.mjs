061cd8b0b0486164ed007d14acc58371035a1067
JaredMathis <jared.mathis@native.consulting>
2022-07-04 03:21:19 -0400

    echo argument

--- src/list_first.mjs
+++ src/list_first.mjs
@@ -1,7 +1,8 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { list_is } from "./list_is.mjs";
 
-export async function list_first() {
-    await arguments_assert()(arguments);
+export async function list_first(list) {
+    await arguments_assert(list_is)(arguments);
     // TODO
 }
