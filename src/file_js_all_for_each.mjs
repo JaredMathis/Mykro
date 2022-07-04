9b1b0a5d0902cd306deafeabe53b733823f29e4a
JaredMathis <jared.mathis@native.consulting>
2022-07-04 19:58:41 -0400

    noop refactor

--- src/file_js_all_for_each.mjs
+++ src/file_js_all_for_each.mjs
@@ -5,10 +5,11 @@ import {string_identifier_is} from "./string_identifier_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 import {for_each} from "./for_each.mjs";
 import {noop} from "./noop.mjs";
+import { js_log_error } from "./js_log_error.mjs";
 export async function file_js_all_for_each(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
   let files = await file_js_all();
   await for_each(files, async file => {
-    await file_js_run(function_name, [file.name], error, noop, e => console.log(e.stack.red), error);
+    await file_js_run(function_name, [file.name], error, noop, js_log_error, error);
   });
 }
