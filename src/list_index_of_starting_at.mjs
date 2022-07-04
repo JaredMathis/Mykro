62fb66d0f61d70b680586d1a85b83f80f1ff005f
JaredMathis <jared.mathis@native.consulting>
2022-07-03 20:13:12 -0400

    echo progress

--- src/list_index_of_starting_at.mjs
+++ src/list_index_of_starting_at.mjs
@@ -1,8 +1,11 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { defined_is } from "./defined_is.mjs";
+import { list_is } from "./list_is.mjs";
+import { number_negative_not_is } from "./number_negative_not_is.mjs";
 
 export async function list_index_of_starting_at(list, item, starting_at) {
-    await arguments_assert()(arguments);
+    await arguments_assert(list_is, defined_is, number_negative_not_is)(arguments);
 
 
 }
