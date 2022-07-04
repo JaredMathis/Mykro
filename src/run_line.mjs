5c2ace9e8f0be1bc03982faf88e925c561cfa0ba
JaredMathis <jared.mathis@native.consulting>
2022-07-04 15:55:21 -0400

    noop refactor into function

--- src/run_line.mjs
+++ src/run_line.mjs
@@ -14,7 +14,7 @@ export async function run_line(line) {
     let token_first = tokens[0];
 
     let on_no_matches = async () => console.log(`No matching command: ${token_first}`.red)
-    
+
     let matches = await run_line_search(token_first);
     let matches_count = await list_size(matches);
 
@@ -32,13 +32,16 @@ export async function run_line(line) {
         try {
             let result = await _function(...tokens_remaining);
             console.log(result);
-            let git_result = await git_acp(`${line}`);
-            if (_.isUndefined(git_result)) {
-                console.log(`${git_acp.name} ran successfully`.magenta);
-            } else {
-                console.log(`${git_acp.name} errored. Maybe there was nothing to commit?`.magenta);
-                console.log(git_result.stack.red);
+            let on_success = async () => {
+                let git_result = await git_acp(`${line}`);
+                if (_.isUndefined(git_result)) {
+                    console.log(`${git_acp.name} ran successfully`.magenta);
+                } else {
+                    console.log(`${git_acp.name} errored. Maybe there was nothing to commit?`.magenta);
+                    console.log(git_result.stack.red);
+                }
             }
+            await on_success();
         } catch (e) {
             console.log(e.stack.red);
         }
