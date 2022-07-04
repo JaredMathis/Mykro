10dc3695095ea3bf8aa2dfe82265734bd1cd8d8d
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:08:02 -0400

    install meriyah

--- src/file_js_parse.mjs
+++ src/file_js_parse.mjs
@@ -1,6 +1,7 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
 import { string_is } from "./string_is.mjs";
+import {Parser} from "acorn"
 
 export async function file_js_parse(file_path) {
     await arguments_assert(string_is)(arguments);
