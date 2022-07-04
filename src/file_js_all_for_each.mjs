88e78e02ab554ddea5dc3a128135f0ba3b725728
JaredMathis <jared.mathis@native.consulting>
2022-07-04 15:49:06 -0400

    fjargadd file_js_all_for_each function_name string_identifier_is

--- src/file_js_all_for_each.mjs
+++ src/file_js_all_for_each.mjs
@@ -1,7 +1,4 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function file_js_all_for_each() {
-    await arguments_assert()(arguments);
-    // TODO
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function file_js_all_for_each(function_name) {
+  await arguments_assert(string_identifier_is)(arguments);
 }
