56271b8e3332a7c84f62edf3b9671dab3cd4f529
JaredMathis <jared.mathis@native.consulting>
2022-07-03 20:24:24 -0400

    echo missing import

--- src/list_index_of_starting_at.mjs
+++ src/list_index_of_starting_at.mjs
@@ -4,6 +4,7 @@ import { defined_is } from "./defined_is.mjs";
 import { list_index_of } from "./list_index_of.mjs";
 import { list_is } from "./list_is.mjs";
 import { number_negative_not_is } from "./number_negative_not_is.mjs";
+import { list_starting_at } from "./list_starting_at.mjs";
 
 export async function list_index_of_starting_at(list, item, starting_at) {
     await arguments_assert(list_is, defined_is, number_negative_not_is)(arguments);
