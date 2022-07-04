fee4adac8290f4f3fc111af33d958b666ff12039
JaredMathis <jared.mathis@native.consulting>
2022-07-04 06:36:54 -0400

    file_js_identifiers_get test

--- src/file_js_identifiers_get.mjs
+++ src/file_js_identifiers_get.mjs
@@ -8,20 +8,26 @@ import _ from "lodash";
 import { string_identifier_is } from "./string_identifier_is.mjs";
 import { js_properties } from "./js_properties.mjs";
 import { js_property_has } from "./js_property_has.mjs";
+import { list_add } from "./list_add.mjs";
 
 export async function file_js_identifiers_get(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
   let ast = await file_js_parse(function_name);
+  
+  let result = [];
   await es_traverse(ast, async node => {
-    console.log(node)
+    if (node.type === 'Identifier') {
+        await list_add(result, node.name);
+    }
   });
+  return result;
 }
 async function es_traverse(ast, ast_node_for_each) {
     await tree_traverse(ast, async node => {
         let values = await js_values(node);
         return await list_where(
             values, 
-            async v => await js_property_has(v, 'type') || _.isArray(v));
+            async v => v !== null && (await js_property_has(v, 'type') || _.isArray(v)));
     }, ast_node_for_each);
 }
 
