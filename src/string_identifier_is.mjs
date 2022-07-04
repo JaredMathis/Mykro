1eaedd4e6c9e4c05379de894c26be58193340c0a
JaredMathis <jared.mathis@native.consulting>
2022-07-04 03:07:30 -0400

    echo progress

--- src/string_identifier_is.mjs
+++ src/string_identifier_is.mjs
@@ -3,18 +3,23 @@ import { arguments_assert } from "./arguments_assert.mjs";
 import { constant_alphabet } from "./constant_alphabet.mjs";
 import { defined_is } from "./defined_is.mjs";
 import { for_each } from "./for_each.mjs";
-import { list_range } from "./list_range.mjs";
+import { list_index_of } from "./list_index_of.mjs";
 import { string_to_list } from "./string_to_list.mjs";
 
 export async function string_identifier_is(value) {
     await arguments_assert(defined_is)(arguments);
 
     let identifer_characters = await constant_alphabet() + `_`;
-    let choices = await string_to_list(identifer_characters);
+    let identifer_characters_as_list = await string_to_list(identifer_characters);
+    let value_as_list = await string_to_list(value);
 
     let result = true;
-    await for_each(list_range(string_size(value)), i => {
-        // TODO
+    await for_each(value_as_list, v => {
+        let index = await list_index_of(identifer_characters_as_list, v)
+        if (!index.success) {
+            result = false;
+            return true;
+        }
     })
 
     return result
