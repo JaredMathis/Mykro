008b82775d3d290af2adca7d734bd26c45f5583f
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:38:17 -0400

    echo missing import

--- src/es_function_exported.mjs
+++ src/es_function_exported.mjs
@@ -1,11 +1,12 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
 import { defined_is } from "./defined_is.mjs";
+import { es_exports } from "./es_exports.mjs";
 
 export async function es_function_exported(parsed) {
     await arguments_assert(defined_is)(arguments);
 
-    let exports = es_exports(parsed);
+    let exports = await es_exports(parsed);
 
 
 }
