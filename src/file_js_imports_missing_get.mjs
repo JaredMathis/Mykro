4f7359bb49da700f121826bd01bb4f54be658ac8
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:37:35 -0400

    fjargad file_js_imports_missing_get import_name string_identifier_is

--- src/file_js_imports_missing_get.mjs
+++ src/file_js_imports_missing_get.mjs
@@ -1,4 +1,4 @@
 import {arguments_assert} from "./arguments_assert.mjs";
-export async function file_js_imports_missing_get(function_name) {
-  await arguments_assert(string_identifier_is)(arguments);
+export async function file_js_imports_missing_get(function_name, import_name) {
+  await arguments_assert(string_identifier_is, string_identifier_is)(arguments);
 }
