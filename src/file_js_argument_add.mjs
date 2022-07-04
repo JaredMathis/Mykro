884ed27aafcf220cb93e55795b8777c7a89b9004
JaredMathis <jared.mathis@native.consulting>
2022-07-04 03:02:23 -0400

    echo progress

--- src/file_js_argument_add.mjs
+++ src/file_js_argument_add.mjs
@@ -3,15 +3,19 @@ import {string_is} from './string_is.mjs';
 import {file_js_parse} from './file_js_parse.mjs';
 import { es_function_exported } from './es_function_exported.mjs';
 import { property_get } from './property_get.mjs';
+import { string_identifier_is } from './string_identifier_is.mjs';
 
 export async function file_js_argument_add(function_name, argument_name, argument_type) {
-    await arguments_assert(string_is, string_is, string_is)(arguments)
+    await arguments_assert(string_is, string_identifier_is, string_is)(arguments)
 
     let parsed = await file_js_parse(function_name);
     let function_exported = await es_function_exported(parsed);
 
     let declaration = await property_get(function_exported, 'declaration');
     let params = await property_get(declaration, 'params');
+
+
+
     let body = await property_get(declaration, 'body');
 
     return declaration;
