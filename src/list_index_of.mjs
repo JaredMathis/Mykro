d424a630a1e642e2d1147d1da886fcc493522067
JaredMathis <jared.mathis@native.consulting>
2022-07-03 20:09:51 -0400

    echo progress

--- src/list_index_of.mjs
+++ src/list_index_of.mjs
@@ -1,7 +1,12 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { defined_is } from "./defined_is.mjs";
+import { list_is } from "./list_is.mjs";
 
-export async function list_index_of() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function list_index_of(list, item) {
+    await arguments_assert(list_is, defined_is)(arguments);
+    
+    let result = list.indexOf(item);
+
+    return result;
 }
