dd943e3c33c9cf2485a99857cc2cbbf1c51cd8f8
JaredMathis <jared.mathis@native.consulting>
2022-07-04 06:28:38 -0400

    echo progress

--- src/list_contains.mjs
+++ src/list_contains.mjs
@@ -1,4 +1,9 @@
 import {arguments_assert} from "./arguments_assert.mjs";
+import { list_index_of } from "./list_index_of.mjs";
 export async function list_contains(list, item) {
   await arguments_assert(list_is, defined_is)(arguments);
+
+  let index_of = await list_index_of(list, item)
+
+  return index_of;
 }
