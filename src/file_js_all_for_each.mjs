a38a69caf378af5c750c0c9d90286c8d4099ca94
JaredMathis <jared.mathis@native.consulting>
2022-07-04 16:18:10 -0400

    noop refactor store result

--- src/file_js_all_for_each.mjs
+++ src/file_js_all_for_each.mjs
@@ -4,5 +4,5 @@ import {arguments_assert} from "./arguments_assert.mjs";
 export async function file_js_all_for_each(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
 
-  await file_js_all();
+  let files = await file_js_all();
 }
