40907e936ff7c38bc3fd6d519c159f2a918aee31
JaredMathis <jared.mathis@native.consulting>
2022-07-04 16:39:39 -0400

    file_js_arg_add file_js_whitespace function_name string_identifier_is

--- src/file_js_whitespace.mjs
+++ src/file_js_whitespace.mjs
@@ -1,10 +1,7 @@
-
-import { noop } from "lodash";
-import { arguments_assert } from "./arguments_assert.mjs";
-import { file_js_transform } from "./file_js_transform.mjs";
-
-export async function file_js_whitespace() {
-    await arguments_assert()(arguments);
-
-    await file_js_transform(function_name, noop);
+import {noop} from "lodash";
+import {arguments_assert} from "./arguments_assert.mjs";
+import {file_js_transform} from "./file_js_transform.mjs";
+export async function file_js_whitespace(function_name) {
+  await arguments_assert(string_identifier_is)(arguments);
+  await file_js_transform(function_name, noop);
 }
