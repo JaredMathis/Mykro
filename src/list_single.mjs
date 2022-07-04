44c3814f7142e0c0e71c7f078e761731570c7055
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:42:08 -0400

    echo progress

--- src/list_single.mjs
+++ src/list_single.mjs
@@ -1,7 +1,13 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { assert } from "./assert.mjs";
+import { equals } from "./equals.mjs";
+import { list_is } from "./list_is.mjs";
+import { list_size } from "./list_size.mjs";
 
-export async function list_single() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function list_single(list) {
+    await arguments_assert(list_is)(arguments);
+
+    assert(equals)(list_size(list), 1);
+    return list[0];
 }
