6d0b15c28d4bc8601b6a88ff70c626cc65605d4c
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:43:17 -0400

    echo progress

--- src/es_exports.mjs
+++ src/es_exports.mjs
@@ -1,9 +1,10 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { defined_is } from "./defined_is.mjs";
 import { property_get } from "./property_get.mjs";
 
 export async function es_exports() {
-    await arguments_assert()(arguments);
+    await arguments_assert(defined_is)(arguments);
     
     let body = await property_get(parsed, 'body');
     return body;
