927669aea1255014ce9bcc44be8b6e9f797f7106
JaredMathis <jared.mathis@native.consulting>
2022-07-04 04:50:45 -0400

    echo rename

--- src/file_js_argument_remove.mjs
+++ src/file_js_argument_remove.mjs
@@ -1,5 +1,5 @@
 import {arguments_assert} from "./arguments_assert.mjs";
 import {string_identifier_is} from "./string_identifier_is.mjs";
-export async function file_js_argument_remove(file_name, _argument) {
+export async function file_js_argument_remove(file_name, argument_name) {
   await arguments_assert(string_identifier_is, string_identifier_is)(arguments);
 }
