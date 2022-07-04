c2a26173ebbdbcfb66d5a860941b0838690bd952
JaredMathis <jared.mathis@native.consulting>
2022-07-04 16:39:13 -0400

    noop refactor added function call

--- src/file_js_whitespace.mjs
+++ src/file_js_whitespace.mjs
@@ -1,7 +1,10 @@
 
+import { noop } from "lodash";
 import { arguments_assert } from "./arguments_assert.mjs";
+import { file_js_transform } from "./file_js_transform.mjs";
 
 export async function file_js_whitespace() {
     await arguments_assert()(arguments);
-    // TODO
+
+    file_js_transform(function_name, noop);
 }
