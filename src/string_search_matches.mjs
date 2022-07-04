2f3c0531e7917409b2970c846506fc5da325f63e
JaredMathis <jared.mathis@native.consulting>
2022-07-03 20:10:40 -0400

    echo progress

--- src/string_search_matches.mjs
+++ src/string_search_matches.mjs
@@ -3,6 +3,7 @@ import { arguments_assert } from "./arguments_assert.mjs";
 import { for_each } from "./for_each.mjs";
 import { string_is } from "./string_is.mjs";
 import { string_to_list } from "./string_to_list.mjs";
+import { list_index_of } from "./list_index_of.mjs";
 
 export async function string_search_matches(candidate, query) {
     await arguments_assert(string_is, string_is)(arguments);
