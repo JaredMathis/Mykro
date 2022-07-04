421af18eaa69676bf5408d06bdbfd0e9d69ec7d4
JaredMathis <jared.mathis@native.consulting>
2022-07-04 06:16:58 -0400

    echo progress

--- src/file_js_identifiers_get.mjs
+++ src/file_js_identifiers_get.mjs
@@ -3,11 +3,14 @@ import { file_js_parse } from "./file_js_parse.mjs";
 import { tree_traverse } from "./tree_traverse.mjs";
 import { list_map } from "./list_map.mjs";
 import { property_get } from "./property_get.mjs";
+import { list_where } from "./list_where.mjs";
+import _ from "lodash";
 export async function file_js_identifiers_get(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
   let ast = await file_js_parse(function_name);
   await tree_traverse(ast, node => {
-    return await js_values(node);
+    let values = await js_values(node);
+    await list_where(values, v => await has_property(v, 'type') || _.isArray(v))
   })
 }
 async function js_values(node) {
