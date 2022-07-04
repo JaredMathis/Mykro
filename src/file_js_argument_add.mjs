32ea9955aa82ef23306612b38c53cb8a6f9a140b
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:51:45 -0400

    echo added missing await

--- src/file_js_argument_add.mjs
+++ src/file_js_argument_add.mjs
@@ -4,7 +4,7 @@ import {file_js_parse} from './file_js_parse.mjs';
 import { es_function_exported } from './es_function_exported.mjs';
 
 export async function file_js_argument_add(function_name, argument_name) {
-    arguments_assert(string_is, string_is)(arguments)
+    await arguments_assert(string_is, string_is)(arguments)
 
     let parsed = await file_js_parse(function_name);
     let function_exported = await es_function_exported(parsed);
