85206e66036ea237d0e81a496484b683c2e71f60
JaredMathis <jared.mathis@native.consulting>
2022-07-04 16:14:27 -0400

    fjimportmisadd file_js_run

--- src/file_js_run.mjs
+++ src/file_js_run.mjs
@@ -7,7 +7,7 @@ import {list_size} from './list_size.mjs';
 import { arguments_assert } from "./arguments_assert.mjs";
 import { list_is } from "./list_is.mjs";
 import { string_identifier_is } from "./string_identifier_is.mjs";
-import path from 'path';
+import { file_path_resolve } from "./file_path_resolve.mjs";
 
 export async function file_js_run(function_name, _arguments, on_no_matches, on_success, on_error, on_multiple_matches) {
     await arguments_assert(string_identifier_is, list_is, js_function_is, js_function_is, js_function_is, js_function_is)(arguments);
@@ -20,7 +20,7 @@ export async function file_js_run(function_name, _arguments, on_no_matches, on_s
     } else if (equals(matches_count, 1)) {
         let match = matches[0];
 
-        let import_path = path.resolve(match.file_path);
+        let import_path = await file_path_resolve(match.file_path);
         console.log(import_path.blue);
         let imported = await import("file://" + import_path);
         let _function = imported[match.name];
