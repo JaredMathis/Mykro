cf727bca1a6332e59b8679dbbde31109f88b7711
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:24:14 -0400

    echo added missing async

--- src/file_js_argument_add.mjs
+++ src/file_js_argument_add.mjs
@@ -2,10 +2,10 @@ import arguments_assert from './arguments_assert.mjs';
 import string_is from './string_is.mjs';
 import file_js_parse from './file_js_parse.mjs';
 
-export function file_js_argument_add(function_name, argument_name) {
+export async function file_js_argument_add(function_name, argument_name) {
     arguments_assert(string_is, string_is)(arguments)
 
     let parsed = await file_js_parse(function_name);
 
-
+    return parsed;
 }
--- src/file_js_parse.mjs
+++ src/file_js_parse.mjs
@@ -2,10 +2,12 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { string_is } from "./string_is.mjs";
 import { file_read } from "./file_read.mjs";
+import { file_js_name_to_path } from "./file_js_name_to_path.mjs";
 
-export async function file_js_parse(file_path) {
+export async function file_js_parse(function_name) {
     await arguments_assert(string_is)(arguments);
     
+    let file_path = await file_js_name_to_path(function_name);
     let unparsed = await file_read(file_path);
     let parsed = await es_parse(unparsed);
     return parsed;
