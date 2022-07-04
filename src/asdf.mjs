7af994f605b62b55f2116287b88750cc2b4772d9
JaredMathis <jared.mathis@native.consulting>
2022-07-04 13:17:26 -0400

    fjimmia asdf

--- src/asdf.mjs
+++ src/asdf.mjs
@@ -1,3 +1,4 @@
+import {number_is} from './number_is.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function asdf(x) {
   await arguments_assert(number_is)(arguments);
