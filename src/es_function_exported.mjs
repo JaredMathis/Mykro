9890afed3366181a99f1431f93800ab459f01f59
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:34:30 -0400

    echo missing import

--- src/es_function_exported.mjs
+++ src/es_function_exported.mjs
@@ -1,9 +1,11 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
 import { defined_is } from "./defined_is.mjs";
+import { property_get } from "./property_get.mjs";
 
 export async function es_function_exported(parsed) {
     await arguments_assert(defined_is)(arguments);
     
-    let body = await property_get(parsed, 'body')
+    let body = await property_get(parsed, 'body');
+
 }
