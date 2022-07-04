91fbffd6f6335972e48357a363e2fdff8061d412
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:26:58 -0400

    echo progress

--- src/file_js_imports_missing_add.mjs
+++ src/file_js_imports_missing_add.mjs
@@ -1,4 +1,7 @@
 import {arguments_assert} from "./arguments_assert.mjs";
+import { file_js_import_add } from "./file_js_import_add.mjs";
 export async function file_js_imports_missing_add() {
   await arguments_assert()(arguments);
+  let {import_names} = await file_js_imports_missing_get(function_name);
+  await file_js_import_add(function_name, import_name);
 }
