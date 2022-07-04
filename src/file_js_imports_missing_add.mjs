a1f64d98e29e92170a76bed264917ec7433d3b90
JaredMathis <jared.mathis@native.consulting>
2022-07-04 11:38:37 -0400

    noop rename

--- src/file_js_imports_missing_add.mjs
+++ src/file_js_imports_missing_add.mjs
@@ -5,7 +5,7 @@ import {file_js_imports_missing_get} from "./file_js_imports_missing_get.mjs";
 export async function file_js_imports_missing_add(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
   let {imports_missing} = await file_js_imports_missing_get(function_name);
-  await for_each(imports_missing, async import_name => {
-    await file_js_import_add(function_name, import_name);
+  await for_each(imports_missing, async import_missing => {
+    await file_js_import_add(function_name, import_missing);
   });
 }
