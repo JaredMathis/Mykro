c04323c2acea6b1d0d98361b9ef4be4520a96eff
JaredMathis <jared.mathis@native.consulting>
2022-07-04 16:17:19 -0400

    noop call function

--- src/file_js_all_for_each.mjs
+++ src/file_js_all_for_each.mjs
@@ -2,5 +2,6 @@ import {string_identifier_is} from './string_identifier_is.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function file_js_all_for_each(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
-  
+
+  file_js_all();
 }
