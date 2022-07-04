e1eefde1edb7234e3bf00a86be684108ede1acc6
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:24:36 -0400

    echo updated imports

--- src/file_js_argument_add.mjs
+++ src/file_js_argument_add.mjs
@@ -1,6 +1,6 @@
-import arguments_assert from './arguments_assert.mjs';
-import string_is from './string_is.mjs';
-import file_js_parse from './file_js_parse.mjs';
+import {arguments_assert} from './arguments_assert.mjs';
+import {string_is} from './string_is.mjs';
+import {file_js_parse} from './file_js_parse.mjs';
 
 export async function file_js_argument_add(function_name, argument_name) {
     arguments_assert(string_is, string_is)(arguments)
