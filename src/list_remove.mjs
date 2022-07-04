5b1075843708a26503e41cefdcbb1e4d45c1a78f
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:14:54 -0400

    echo progress

--- src/list_remove.mjs
+++ src/list_remove.mjs
@@ -3,6 +3,7 @@ import { assert } from "./assert.mjs";
 import { defined_is } from "./defined_is.mjs";
 import {list_index_of} from "./list_index_of.mjs";
 import {list_is} from "./list_is.mjs";
+import { list_remove_at } from "./list_remove_at.mjs";
 import { property_get } from "./property_get.mjs";
 import { true_is } from "./true_is.mjs";
 export async function list_remove(list, item) {
@@ -12,6 +13,6 @@ export async function list_remove(list, item) {
   
   let index = await property_get(index_of, 'index')
 
-  list.splice(index, 1);
+  await list_remove_at(list, index);
   return {index}
 }
