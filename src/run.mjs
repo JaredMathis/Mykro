806360d70b2100e9fd7268f678d1634d81e58226
JaredMathis <jared.mathis@native.consulting>
2022-07-04 19:57:43 -0400

    noop refactor change code to function

--- src/run.mjs
+++ src/run.mjs
@@ -1,6 +1,7 @@
 import readline from "readline";
 import {command_line} from "./command_line.mjs";
 import "colors";
+import { js_log_error } from "./js_log_error.mjs";
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -11,6 +12,6 @@ rl.on("line", async line => {
     let result = await command_line(`node src/run_function.mjs ` + line);
     console.log(result.stdout);
   } catch (e) {
-    console.log(e.stack.red);
+    await js_log_error(e);
   }
 });
