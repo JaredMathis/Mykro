b8f15aee78dc5b8dda0b33083db33e944c6bbdf1
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:44:48 -0400

    echo made function async

--- src/boolean_is.mjs
+++ src/boolean_is.mjs
@@ -1,7 +1,7 @@
 import { assert } from "./assert.mjs";
 import { equals } from "./equals.mjs";
 
-export function boolean_is(b) {
-    assert(equals)(arguments.length, 1);
+export async function boolean_is(b) {
+    await assert(equals)(arguments.length, 1);
     return [true, false].includes(b);
 }
