e027823060b1dd3618f5db4798ee0c399b9c0136
JaredMathis <jared.mathis@native.consulting>
2022-07-03 20:28:06 -0400

    echo progress

--- src/for_each.mjs
+++ src/for_each.mjs
@@ -1,14 +1,17 @@
 
 import _ from "lodash";
 import { arguments_assert } from "./arguments_assert.mjs";
+import { defined_is } from "./defined_is.mjs";
 import { true_is } from "./true_is.mjs";
 
 export async function for_each(list, for_each_item) {
     await arguments_assert(_.isArray, _.isFunction)(arguments);
     for (let item of list) {
         let result = await for_each_item(item);
-        if (await true_is(result)) {
-            break;
+        if (defined_is(result)) {
+            if (await true_is(result)) {
+                break;
+            }
         }
     }
 }
