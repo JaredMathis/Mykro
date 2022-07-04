220e80f95b7b0248fba5e63e07dab0b2f9e0ca36
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:03:39 -0400

    echo progress

--- src/list_remove.mjs
+++ src/list_remove.mjs
@@ -1,5 +1,8 @@
 import {arguments_assert} from "./arguments_assert.mjs";
+import { list_index_of } from "./list_index_of.mjs";
 import { list_is } from "./list_is.mjs";
 export async function list_remove(list) {
   await arguments_assert(list_is)(arguments);
+
+  await list_index_of(list, item);
 }
