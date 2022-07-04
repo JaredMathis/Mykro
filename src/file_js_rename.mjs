fc8f1705050f30b51fbf7f9e53c16b500538c4f1
JaredMathis <jared.mathis@native.consulting>
2022-07-04 04:29:59 -0400

    fjg file_js_rename file_path defined_is

--- src/file_js_rename.mjs
+++ src/file_js_rename.mjs
@@ -1,7 +1,4 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function file_js_rename() {
-    await arguments_assert()(arguments);
-    // TODO
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function file_js_rename(file_path) {
+  await arguments_assert(defined_is)(arguments);
 }
