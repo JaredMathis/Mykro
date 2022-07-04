6dd54de1629087a8913fddd90ce0e4ed573c7375
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:47:53 -0400

    fjargad file_js_imports_get function_name string_identifier_is

--- src/file_js_imports_get.mjs
+++ src/file_js_imports_get.mjs
@@ -1,7 +1,4 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function file_js_imports_get() {
-    await arguments_assert()(arguments);
-    // TODO
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function file_js_imports_get(function_name) {
+  await arguments_assert(string_identifier_is)(arguments);
 }
