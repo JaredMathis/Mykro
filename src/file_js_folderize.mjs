79ccc88185700fe6aee9f28d05551371df3f0dae
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:26:04 -0400

    echo progress

--- src/file_js_folderize.mjs
+++ src/file_js_folderize.mjs
@@ -1,4 +1,4 @@
 import {arguments_assert} from "./arguments_assert.mjs";
-export async function file_js_folderize(file_name) {
+export async function file_js_folderize(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
 }
