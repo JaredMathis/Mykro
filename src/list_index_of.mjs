e65c63762ae8bed222b1dd9ec5714479fbc1149a
JaredMathis <jared.mathis@native.consulting>
2022-07-03 20:10:17 -0400

    echo progress

--- src/list_index_of.mjs
+++ src/list_index_of.mjs
@@ -1,12 +1,16 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { assert } from "./assert.mjs";
 import { defined_is } from "./defined_is.mjs";
 import { list_is } from "./list_is.mjs";
+import { number_negative_not_is } from "./number_negative_not_is.mjs";
 
 export async function list_index_of(list, item) {
     await arguments_assert(list_is, defined_is)(arguments);
     
     let result = list.indexOf(item);
+
+    assert(number_negative_not_is)(result);
 
     return result;
 }
