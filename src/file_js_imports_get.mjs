82c00fe9d65111f2a965936279ffcd1865e0a126
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:48:53 -0400

    echo progress

--- src/file_js_imports_get.mjs
+++ src/file_js_imports_get.mjs
@@ -1,4 +1,8 @@
 import {arguments_assert} from "./arguments_assert.mjs";
+import { string_identifier_is } from "./string_identifier_is.mjs";
 export async function file_js_imports_get(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
+
+  let ast = await file_js_parse(function_name);
+  
 }
