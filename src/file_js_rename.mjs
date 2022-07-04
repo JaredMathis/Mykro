b9c8f4fbc6c3b7875bea656ef84d74f5cd93fc51
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:42:53 -0400

    echo progress

--- src/file_js_rename.mjs
+++ src/file_js_rename.mjs
@@ -1,4 +1,4 @@
 import {arguments_assert} from "./arguments_assert.mjs";
-export async function file_js_rename(file_path) {
+export async function file_js_rename(function_name) {
   await arguments_assert(defined_is)(arguments);
 }
