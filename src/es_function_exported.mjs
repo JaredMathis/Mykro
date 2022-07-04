ed022d9c18866d7230a374e65a086fa42a7d1ea0
JaredMathis <jared.mathis@native.consulting>
2022-07-04 02:30:26 -0400

    echo progress

--- src/es_function_exported.mjs
+++ src/es_function_exported.mjs
@@ -2,7 +2,7 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { defined_is } from "./defined_is.mjs";
 
-export async function es_function_exported() {
+export async function es_function_exported(parsed) {
     await arguments_assert(defined_is)(arguments);
-    
+
 }
