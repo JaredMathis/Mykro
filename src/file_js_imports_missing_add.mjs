f23cb1c216d7c67131fd87bc567fd31795d3b6ab
JaredMathis <jared.mathis@native.consulting>
2022-07-04 05:29:28 -0400

    fjargad file_js_imports_missing_add function_name string_identifier_is

--- src/file_js_imports_missing_add.mjs
+++ src/file_js_imports_missing_add.mjs
@@ -1,9 +1,9 @@
 import {arguments_assert} from "./arguments_assert.mjs";
-import { file_js_import_add } from "./file_js_import_add.mjs";
-export async function file_js_imports_missing_add() {
-  await arguments_assert()(arguments);
+import {file_js_import_add} from "./file_js_import_add.mjs";
+export async function file_js_imports_missing_add(function_name) {
+  await arguments_assert(string_identifier_is)(arguments);
   let {import_names} = await file_js_imports_missing_get(function_name);
   await for_each(import_names, async import_name => {
     await file_js_import_add(function_name, import_name);
-  })
+  });
 }
