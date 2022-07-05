152a782ae251caf970e910d4d853176bd1b624f9
JaredMathis <jared.mathis@native.consulting>
2022-07-05 11:54:14 -0400

    noop coded git_push_origin_head

--- src/git_push_origin_head.mjs
+++ src/git_push_origin_head.mjs
@@ -1,7 +1,9 @@
+import {command_line} from './command_line.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
 export async function git_push_origin_head() {
     await arguments_assert()(arguments);
-    // TODO
+    return await command_line(`git push -u origin HEAD`);
 }
+
