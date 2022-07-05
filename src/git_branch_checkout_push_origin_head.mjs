16ad063afb6f18eedd420b1c1b7dca3fac5e3cab
JaredMathis <jared.mathis@native.consulting>
2022-07-05 15:22:19 -0400

    noop added function call

--- src/git_branch_checkout_push_origin_head.mjs
+++ src/git_branch_checkout_push_origin_head.mjs
@@ -1,8 +1,8 @@
+import {git_branch_checkout} from './git_branch_checkout.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
 export async function git_branch_checkout_push_origin_head() {
     await arguments_assert()(arguments);
-    git_branch_checkout(branch_name)
-    // TODO
+    await git_branch_checkout(branch_name)
 }
