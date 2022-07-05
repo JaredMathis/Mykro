242e1424175e673c535767fa8546930a8b0535f9
JaredMathis <jared.mathis@native.consulting>
2022-07-04 21:58:06 -0400

    fjargad list_remove_try item defined_is

--- src/list_remove_try.mjs
+++ src/list_remove_try.mjs
@@ -1,5 +1,6 @@
-import {list_is} from './list_is.mjs';
+import {defined_is} from './defined_is.mjs';
+import {list_is} from "./list_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
-export async function list_remove_try(list) {
-  await arguments_assert(list_is)(arguments);
+export async function list_remove_try(list, item) {
+  await arguments_assert(list_is, defined_is)(arguments);
 }
