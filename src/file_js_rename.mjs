db38c5b3bcf4e04f3655b292ed70dd233ea2420f
JaredMathis <jared.mathis@native.consulting>
2022-07-04 22:16:13 -0400

    noop added assert

--- src/file_js_rename.mjs
+++ src/file_js_rename.mjs
@@ -1,6 +1,9 @@
-import {defined_is} from "./defined_is.mjs";
+import {not} from './not.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
+import { assert } from "./assert.mjs";
+import { equals } from "./equals.mjs";
 import {string_identifier_is} from "./string_identifier_is.mjs";
 export async function file_js_rename(function_name, function_name_new) {
   await arguments_assert(string_identifier_is, string_identifier_is)(arguments);
+  await assert(not(equals))(function_name, function_name_new);
 }
