d586242130e5aa94b4a8f5861cd330a22fb3fd33
JaredMathis <jared.mathis@native.consulting>
2022-07-05 11:46:31 -0400

    fjsargadd git_branch branch_name string_is

--- src/git_branch.mjs
+++ src/git_branch.mjs
@@ -1,7 +1,5 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function git_branch() {
-    await arguments_assert()(arguments);
-    // TODO
+import {string_is} from './string_is.mjs';
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function git_branch(branch_name) {
+  await arguments_assert(string_is)(arguments);
 }
