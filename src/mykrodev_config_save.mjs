4130c4abd601f9462caf0e9fe043f42f80d81ce6
JaredMathis <jared.mathis@native.consulting>
2022-07-05 19:20:28 -0400

    main noop refactor added statement

--- src/mykrodev_config_save.mjs
+++ src/mykrodev_config_save.mjs
@@ -1,7 +1,11 @@
 import {defined_is} from './defined_is.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
 import { json_to } from './json_to.mjs';
+import { file_overwrite } from './file_overwrite.mjs';
+import { mykrodev_config_path } from './mykrodev_config_path.mjs';
 export async function mykrodev_config_save(config) {
   await arguments_assert(defined_is)(arguments);
+  let file_path = await mykrodev_config_path();
   let json = await json_to(config)
+  file_overwrite()
 }
