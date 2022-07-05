d511abb8f01c855ee09ae193d7c72f686d17ef85
JaredMathis <jared.mathis@native.consulting>
2022-07-05 13:58:24 -0400

    noop rename

--- src/git_checkout_pull.mjs
+++ src/git_checkout_pull.mjs
@@ -4,8 +4,8 @@ import {git_checkout} from './git_checkout.mjs';
 import { arguments_assert } from "./arguments_assert.mjs";
 import { string_is } from "./string_is.mjs";
 
-export async function git_checkout_pull(merge_from) {
+export async function git_checkout_pull(branch_name) {
     await arguments_assert(string_is)(arguments);
-    await git_checkout(merge_from)
-    await git_pull(merge_from)
+    await git_checkout(branch_name)
+    await git_pull(branch_name)
 }
