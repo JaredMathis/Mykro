bee6b9b773dd33594e57cc98391288250c70ff07
JaredMathis <jared.mathis@native.consulting>
2022-07-04 22:10:43 -0400

    noop function call

--- src/file_js_folderize.mjs
+++ src/file_js_folderize.mjs
@@ -1,5 +1,8 @@
+import {file_js_all} from './file_js_all.mjs';
 import {string_identifier_is} from "./string_identifier_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function file_js_folderize(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
+
+  file_js_all();
 }
