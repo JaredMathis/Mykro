85d670c7bb524c5ce8b14416178d7d075af6cb97
JaredMathis <jared.mathis@native.consulting>
2022-07-04 22:18:45 -0400

    noop function call

--- src/file_js_all_identifiers_get.mjs
+++ src/file_js_all_identifiers_get.mjs
@@ -1,7 +1,9 @@
+import {file_js_all_for_each} from './file_js_all_for_each.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
 export async function file_js_all_identifiers_get() {
     await arguments_assert()(arguments);
-    // TODO
+    
+    await file_js_all_for_each()
 }
