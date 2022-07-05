882de15b76da1c0dd03b05ba7d7417f64cce6c6c
JaredMathis <jared.mathis@native.consulting>
2022-07-04 23:17:37 -0400

    greet jared

--- src/greet.mjs
+++ src/greet.mjs
@@ -3,5 +3,5 @@ import {number_is} from "./number_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function greet(name) {
   await arguments_assert(string_is)(arguments);
-  return "hello ";
+  return "hello " + name;
 }
