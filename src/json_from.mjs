4b8b050c70cf8f3864e5321bcb5ef46ccceff987
JaredMathis <jared.mathis@native.consulting>
2022-07-05 12:09:50 -0400

    fjargadd json_from unparsed string_is

--- src/json_from.mjs
+++ src/json_from.mjs
@@ -1,7 +1,5 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function json_from() {
-    await arguments_assert()(arguments);
-    // TODO
+import {string_is} from './string_is.mjs';
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function json_from(unparsed) {
+  await arguments_assert(string_is)(arguments);
 }
