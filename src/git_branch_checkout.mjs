86873987b1cb3daf9bd801a177b8e4bf00b09c09
JaredMathis <jared.mathis@native.consulting>
2022-07-05 15:16:39 -0400

    noop refactor function call

--- src/git_branch_checkout.mjs
+++ src/git_branch_checkout.mjs
@@ -1,5 +1,8 @@
+import {git_branch} from './git_branch.mjs';
 import {string_is} from './string_is.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function git_branch_checkout(branch_name) {
   await arguments_assert(string_is)(arguments);
+  await git_branch(branch_name);
+  await git_branch(branch_name);
 }
