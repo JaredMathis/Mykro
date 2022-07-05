00fc9c087fbe4b9362e07ba341d5361465967653
JaredMathis <jared.mathis@native.consulting>
2022-07-04 22:14:19 -0400

    noop refactor change argument type

--- src/file_js_rename.mjs
+++ src/file_js_rename.mjs
@@ -1,5 +1,6 @@
 import {defined_is} from "./defined_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
+import { string_identifier_is } from "./string_identifier_is.mjs";
 export async function file_js_rename(function_name) {
-  await arguments_assert(defined_is)(arguments);
+  await arguments_assert(string_identifier_is)(arguments);
 }
