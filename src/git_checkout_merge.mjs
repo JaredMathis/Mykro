631d05f9968962626be68b3d4ddb3b7f5f0a3dd6
JaredMathis <jared.mathis@native.consulting>
2022-07-05 14:03:03 -0400

    noop make sure both branches are up to date

--- src/git_checkout_merge.mjs
+++ src/git_checkout_merge.mjs
@@ -4,6 +4,7 @@ import {arguments_assert} from "./arguments_assert.mjs";
 import { git_merge } from './git_merge.mjs';
 export async function git_checkout_merge(merge_from, merge_into) {
   await arguments_assert(string_is, string_is)(arguments);
+  await git_checkout_pull(merge_into)
   await git_checkout_pull(merge_from)
   await git_merge(merge_into)
 }
