c58a28a8f2c7ec94f89e019747f5404264a5bfd4
JaredMathis <jared.mathis@native.consulting>
2022-07-05 19:17:41 -0400

    main noop progress added statement

--- src/mykrodev_config_save.mjs
+++ src/mykrodev_config_save.mjs
@@ -1,5 +1,7 @@
 import {defined_is} from './defined_is.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
+import { json_to } from './json_to.mjs';
 export async function mykrodev_config_save(config) {
   await arguments_assert(defined_is)(arguments);
+  let json = await json_to(config)
 }
