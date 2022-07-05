cb2345fd9df6499125e35d0130716f42936096bf
JaredMathis <jared.mathis@native.consulting>
2022-07-04 22:14:42 -0400

    file_js_argument_add file_js_rename function_name_new string_identifier_is

--- src/file_js_rename.mjs
+++ src/file_js_rename.mjs
@@ -1,6 +1,6 @@
 import {defined_is} from "./defined_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
-import { string_identifier_is } from "./string_identifier_is.mjs";
-export async function file_js_rename(function_name) {
-  await arguments_assert(string_identifier_is)(arguments);
+import {string_identifier_is} from "./string_identifier_is.mjs";
+export async function file_js_rename(function_name, function_name_new) {
+  await arguments_assert(string_identifier_is, string_identifier_is)(arguments);
 }
