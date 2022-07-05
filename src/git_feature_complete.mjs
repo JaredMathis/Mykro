ddfd36ee53fbf4954e8410901a2d77779446e793
JaredMathis <jared.mathis@native.consulting>
2022-07-05 19:32:14 -0400

    main noop refactor function call

--- src/git_feature_complete.mjs
+++ src/git_feature_complete.mjs
@@ -1,3 +1,4 @@
+import {git_checkout_merge} from './git_checkout_merge.mjs';
 import {mykrodev_config_branch_main_get} from './mykrodev_config_branch_main_get.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
@@ -7,4 +8,6 @@ export async function git_feature_complete() {
     await arguments_assert()(arguments);
     let current_branch = await git_branch_show_current();
     let main_branch = await mykrodev_config_branch_main_get();
+
+    git_checkout_merge()
 }
