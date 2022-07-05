642190daa6b2cde2e201460eace5885d798ae630
JaredMathis <jared.mathis@native.consulting>
2022-07-04 22:30:43 -0400

    noop comment

--- src/file_js_transform.mjs
+++ src/file_js_transform.mjs
@@ -6,12 +6,14 @@ import {arguments_assert} from "./arguments_assert.mjs";
 import {js_function_is} from "./js_function_is.mjs";
 import {string_identifier_is} from "./string_identifier_is.mjs";
 import { equals } from "./equals.mjs";
+import { js_comment } from "./js_comment.mjs";
 export async function file_js_transform(function_name, transformer) {
   await arguments_assert(string_identifier_is, js_function_is)(arguments);
   let {ast, unparsed} = await file_js_parse(function_name);
   await transformer(ast);
   let text = await es_unparse(ast);
   if (equals(text, unparsed)) {
+    await js_comment(`There were no changes to the AST, so we're returning rather than saving`);
     return;
   }
   let file_path = await file_js_name_to_path(function_name);
