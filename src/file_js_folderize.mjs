52730d79b46d4b90d1d2b1144f5c69351f3ea9ef
JaredMathis <jared.mathis@native.consulting>
2022-07-04 22:13:17 -0400

    file_js_all_for_each file_js_whitespace

--- src/file_js_folderize.mjs
+++ src/file_js_folderize.mjs
@@ -1,9 +1,8 @@
-import {file_js_all} from './file_js_all.mjs';
+import {file_js_all} from "./file_js_all.mjs";
 import {string_identifier_is} from "./string_identifier_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function file_js_folderize(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
-
   let files = await file_js_all();
   return files;
 }
