51fb602d1626b689c07d8ddad9453b50f62c3bcb
JaredMathis <jared.mathis@native.consulting>
2022-07-05 14:04:08 -0400

    noop refactor

--- src/git_checkout_merge.mjs
+++ src/git_checkout_merge.mjs
@@ -2,9 +2,13 @@ import {git_checkout_pull} from './git_checkout_pull.mjs';
 import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 import { git_merge } from './git_merge.mjs';
+import { for_each } from './for_each.mjs';
+import { js_comment } from './js_comment.mjs';
 export async function git_checkout_merge(merge_from, merge_into) {
   await arguments_assert(string_is, string_is)(arguments);
-  await git_checkout_pull(merge_into)
-  await git_checkout_pull(merge_from)
+  await js_comment(`make sure both branches are up to date`)
+  await for_each([merge_into, merge_from], async branch_name => {
+    await git_checkout_pull(branch_name)
+  })
   await git_merge(merge_into)
 }
