23ed3ae255ab83f95ff7e17dc9f1b68455973fbc
JaredMathis <jared.mathis@native.consulting>
2022-07-05 19:14:31 -0400

    main file_js_new fjew mykrodev_config_branch_main_get

--- src/git_feature_complete.mjs
+++ src/git_feature_complete.mjs
@@ -5,4 +5,5 @@ import { git_branch_show_current } from "./git_branch_show_current.mjs";
 export async function git_feature_complete() {
     await arguments_assert()(arguments);
     let current_branch = await git_branch_show_current();
+    
 }
---
+++ src/mykrodev_config_branch_main_get.mjs
@@ -0,0 +1,7 @@
+
+import { arguments_assert } from "./arguments_assert.mjs";
+
+export async function mykrodev_config_branch_main_get() {
+    await arguments_assert()(arguments);
+    // TODO
+}
