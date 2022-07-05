3f95a68f1de2833c58291849103d113d46c7e3b0
JaredMathis <jared.mathis@native.consulting>
2022-07-05 11:49:56 -0400

    noop copy and pasted from other function, made change

--- src/git_checkout.mjs
+++ src/git_checkout.mjs
@@ -1,7 +1,9 @@
+import {command_line} from './command_line.mjs';
+import {string_is} from './string_is.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
-export async function git_checkout() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function git_checkout(branch_name) {
+    await arguments_assert(string_is)(arguments);
+    return await command_line(`git checkout ${branch_name}`);
 }
