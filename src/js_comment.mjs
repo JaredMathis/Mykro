f22301369957f842174aa10ccb0afb9ad630713a
JaredMathis <jared.mathis@native.consulting>
2022-07-04 22:29:08 -0400

    file_js_arg_add js_comment comment string_is

--- src/js_comment.mjs
+++ src/js_comment.mjs
@@ -1,7 +1,5 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function js_comment() {
-    await arguments_assert()(arguments);
-    // TODO
+import {string_is} from './string_is.mjs';
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function js_comment(comment) {
+  await arguments_assert(string_is)(arguments);
 }
