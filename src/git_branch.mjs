f23ad91397c5f4280616124df17edf886b0bf919
JaredMathis <jared.mathis@native.consulting>
2022-07-05 11:47:49 -0400

    noop copy and pasted from other function, made change

--- src/git_branch.mjs
+++ src/git_branch.mjs
@@ -1,5 +1,7 @@
+import {command_line} from './command_line.mjs';
 import {string_is} from './string_is.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function git_branch(branch_name) {
   await arguments_assert(string_is)(arguments);
+  return await command_line(`git branch ${branch_name}`);
 }
