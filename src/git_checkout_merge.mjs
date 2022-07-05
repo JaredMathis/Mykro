ef1546a930e42f82d1f3943e0aa31ec4ce7fff49
JaredMathis <jared.mathis@native.consulting>
2022-07-05 13:55:39 -0400

    noop added statement

--- src/git_checkout_merge.mjs
+++ src/git_checkout_merge.mjs
@@ -1,7 +1,9 @@
 import {git_checkout} from './git_checkout.mjs';
 import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
+import { git_pull } from './git_pull.mjs';
 export async function git_checkout_merge(merge_from, merge_into) {
   await arguments_assert(string_is, string_is)(arguments);
   await git_checkout(merge_from)
+  await git_pull(merge_from)
 }
