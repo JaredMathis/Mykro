ac91a566ed677ff8b9c284707f6a1bb1f5f4bcfc
JaredMathis <jared.mathis@native.consulting>
2022-07-05 19:13:14 -0400

    main noop function call

--- src/git_feature_complete.mjs
+++ src/git_feature_complete.mjs
@@ -1,7 +1,8 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { git_branch_show_current } from "./git_branch_show_current.mjs";
 
 export async function git_feature_complete() {
     await arguments_assert()(arguments);
-    // TODO
+    let current_branch = await git_branch_show_current();
 }
