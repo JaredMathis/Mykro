d8bf45b287ff6739ef91225c404ee75d2724e0ef
JaredMathis <jared.mathis@native.consulting>
2022-07-05 19:16:05 -0400

    main noop progress

--- src/mykrodev_config_branch_main_get.mjs
+++ src/mykrodev_config_branch_main_get.mjs
@@ -1,7 +1,14 @@
+import {mykrodev_config_get} from './mykrodev_config_get.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { js_property_has } from "./js_property_has.mjs";
 
 export async function mykrodev_config_branch_main_get() {
     await arguments_assert()(arguments);
-    // TODO
+    let config = await mykrodev_config_get();
+    let branch = 'branch';
+    if (!await js_property_has(config, branch)) {
+        config[branch] = {};
+        await mykrodev_config_save(config);
+    }
 }
