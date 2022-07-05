e97e9a3e115043b21d4cedbc2d24ab48f95b60ee
JaredMathis <jared.mathis@native.consulting>
2022-07-05 13:43:21 -0400

    fjargadd git_merge merge_from string_is

--- src/git_merge.mjs
+++ src/git_merge.mjs
@@ -1,8 +1,7 @@
-import {command_line} from './command_line.mjs';
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function git_merge() {
-    await arguments_assert()(arguments);
-    return await command_line(`git merge "${merge_from}"`);
+import {string_is} from './string_is.mjs';
+import {command_line} from "./command_line.mjs";
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function git_merge(merge_from) {
+  await arguments_assert(string_is)(arguments);
+  return await command_line(`git merge "${merge_from}"`);
 }
