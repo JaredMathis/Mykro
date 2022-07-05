186af9b038e30e4eee6eba1d1cead2b818c17ca6
JaredMathis <jared.mathis@native.consulting>
2022-07-04 23:14:13 -0400

    noop added return

--- src/greet.mjs
+++ src/greet.mjs
@@ -2,4 +2,5 @@ import {number_is} from "./number_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function greet() {
   await arguments_assert()(arguments);
+  return "hello ";
 }
