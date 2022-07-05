5f0afe51ddfed3cc243d9d3e1024b7ae1f1c81b9
JaredMathis <jared.mathis@native.consulting>
2022-07-04 23:23:38 -0400

    noop this is a no op command but this message will appear in git

--- src/greet.mjs
+++ src/greet.mjs
@@ -1,7 +1,13 @@
+import {list_where} from './list_where.mjs';
+import {list_add} from './list_add.mjs';
+import {number_is} from './number_is.mjs';
 import {string_is} from './string_is.mjs';
-import {number_is} from "./number_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function greet(name) {
   await arguments_assert(string_is)(arguments);
-  return "hello " + name;
+
+number_is
+list_add
+list_where
+
 }
