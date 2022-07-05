09cb938d7473199894f82555a853f935e181000b
JaredMathis <jared.mathis@native.consulting>
2022-07-04 22:20:16 -0400

    noop progress

--- src/file_js_all_identifiers_get.mjs
+++ src/file_js_all_identifiers_get.mjs
@@ -1,9 +1,14 @@
+import {file_js_all} from './file_js_all.mjs';
 import {file_js_all_for_each} from './file_js_all_for_each.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { for_each } from './for_each.mjs';
 
 export async function file_js_all_identifiers_get() {
     await arguments_assert()(arguments);
     
-    await file_js_all_for_each()
+    let files = await file_js_all();
+    await for_each(files, async file => {
+        console.log(file);
+    })
 }
