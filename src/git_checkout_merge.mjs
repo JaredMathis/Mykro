6da49e219280c8e92d0ae47791ac08ff9c31e9b2
JaredMathis <jared.mathis@native.consulting>
2022-07-05 15:42:04 -0400

    noop added git_push to git_checkout_merge

--- src/git_checkout_merge.mjs
+++ src/git_checkout_merge.mjs
@@ -1,3 +1,4 @@
+import {git_push} from './git_push.mjs';
 import {git_checkout_pull} from './git_checkout_pull.mjs';
 import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
@@ -11,4 +12,5 @@ export async function git_checkout_merge(merge_from, merge_into) {
     await git_checkout_pull(branch_name)
   })
   await git_merge(merge_from)
+  await git_push();
 }
