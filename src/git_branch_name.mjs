8a8688c9010394388f10d58d4ecec685eb8c116f
JaredMathis <jared.mathis@native.consulting>
2022-07-05 16:02:01 -0400

    undefined noop progress

--- src/git_branch_name.mjs
+++ src/git_branch_name.mjs
@@ -1,9 +1,23 @@
+import {string_from_list} from './string_from_list.mjs';
+import {string_size} from './string_size.mjs';
 import {command_line} from './command_line.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { js_comment } from './js_comment.mjs';
+import { string_starts_with } from './string_starts_with.mjs';
+import { list_starting_at } from './list_starting_at.mjs';
+import { string_to_list } from './string_to_list.mjs';
 
 export async function git_branch_name() {
     await arguments_assert()(arguments);
     let result = await command_line(`git branch`);
-    return result.stdout
+    let stdout = result.stdout
+    const prefix = '*';
+    if (string_starts_with(stdout, prefix)) {
+        let stdout_as_list = await string_to_list(stdout)
+        let sublist = await list_starting_at(stdout_as_list, await string_size(prefix));
+        stdout = await string_from_list(sublist);
+    }
+    js_comment(`remove leading space and trailing new line`);  
+    stdout = stdout.trim();
 }
---
+++ src/string_from_list.mjs
@@ -0,0 +1,6 @@
+import {list_is} from './list_is.mjs';
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function string_from_list(list) {
+  await arguments_assert(list_is)(arguments);
+  return list.join("");
+}
