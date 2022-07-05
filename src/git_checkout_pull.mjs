39fd9439782d0e6928647abcee00a9e886462c29
JaredMathis <jared.mathis@native.consulting>
2022-07-05 14:32:43 -0400

    noop remove unused function

--- src/git_checkout_pull.mjs
+++ src/git_checkout_pull.mjs
@@ -7,5 +7,5 @@ import { string_is } from "./string_is.mjs";
 export async function git_checkout_pull(branch_name) {
     await arguments_assert(string_is)(arguments);
     await git_checkout(branch_name)
-    await git_pull(branch_name)
+    await git_pull()
 }
