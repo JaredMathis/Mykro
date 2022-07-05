364562deaae386d738c21dfe4ac5e398554a3153
JaredMathis <jared.mathis@native.consulting>
2022-07-05 19:30:25 -0400

    main noop refactor function to have a default_value argument

--- src/mykrodev_config_branch_main_get.mjs
+++ src/mykrodev_config_branch_main_get.mjs
@@ -7,13 +7,16 @@ import { js_property_has } from "./js_property_has.mjs";
 export async function mykrodev_config_branch_main_get() {
     await arguments_assert()(arguments);
     let config = await mykrodev_config_get();
-    await mykrodev_config_property_exists_ensure(config, config, 'branch');
-    await mykrodev_config_property_exists_ensure(config, config.branch, 'main');
+    await mykrodev_config_property_exists_ensure(
+        config, config, 'branch', {});
+    await mykrodev_config_property_exists_ensure(
+        config, config.branch, 'main', 'main');
 }
 
-async function mykrodev_config_property_exists_ensure(config, object, property_name) {
+async function mykrodev_config_property_exists_ensure(
+    config, object, property_name, default_value) {
     if (!await js_property_has(object, property_name)) {
-        object[property_name] = {};
+        object[property_name] = default_value;
         await mykrodev_config_save(config);
     }
 }
