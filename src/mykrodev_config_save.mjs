d484551e850de14eeab29417175496bf473e0243
JaredMathis <jared.mathis@native.consulting>
2022-07-05 19:21:53 -0400

    main noop refactored code to its own function

--- src/mykrodev_config_save.mjs
+++ src/mykrodev_config_save.mjs
@@ -6,6 +6,10 @@ import { mykrodev_config_path } from './mykrodev_config_path.mjs';
 export async function mykrodev_config_save(config) {
   await arguments_assert(defined_is)(arguments);
   let file_path = await mykrodev_config_path();
-  let json = await json_to(config)
-  await file_overwrite(file_path, json);
+  await file_json_save(config, file_path);
 }
+async function file_json_save(config, file_path) {
+    let json = await json_to(config);
+    await file_overwrite(file_path, json);
+}
+
