e394395c845d25023558a89ec1d00504602a16cd
JaredMathis <jared.mathis@native.consulting>
2022-07-05 15:22:01 -0400

    file_js_new fjew git_branch_checkout_push_origin_head

---
+++ src/git_branch_checkout_push_origin_head.mjs
@@ -0,0 +1,8 @@
+
+import { arguments_assert } from "./arguments_assert.mjs";
+
+export async function git_branch_checkout_push_origin_head() {
+    await arguments_assert()(arguments);
+    git_branch_checkout(branch_name)
+    // TODO
+}
