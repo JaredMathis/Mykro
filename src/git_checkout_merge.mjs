9364ecd825f057a493b94fedd348217a036eaf3a
JaredMathis <jared.mathis@native.consulting>
2022-07-05 13:46:26 -0400

    fjargadd git_checkout_merge merge_from string_is

--- src/git_checkout_merge.mjs
+++ src/git_checkout_merge.mjs
@@ -1,7 +1,5 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function git_checkout_merge() {
-    await arguments_assert()(arguments);
-    // TODO
+import {string_is} from './string_is.mjs';
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function git_checkout_merge(merge_from) {
+  await arguments_assert(string_is)(arguments);
 }
