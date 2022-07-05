88d1ff82158e5cc4f5bfba50dfb978235ad68558
JaredMathis <jared.mathis@native.consulting>
2022-07-05 13:54:11 -0400

    noop added code

--- src/git_pull.mjs
+++ src/git_pull.mjs
@@ -1,7 +1,8 @@
+import {command_line} from './command_line.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
 export async function git_pull() {
     await arguments_assert()(arguments);
-    // TODO
+    return await command_line(`git pull`);
 }
