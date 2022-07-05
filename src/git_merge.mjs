10026fe431069c6637c0e5c406be9f1d8bf6cd61
JaredMathis <jared.mathis@native.consulting>
2022-07-05 13:42:58 -0400

    noop refactor added code

--- src/git_merge.mjs
+++ src/git_merge.mjs
@@ -1,7 +1,8 @@
+import {command_line} from './command_line.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
 export async function git_merge() {
     await arguments_assert()(arguments);
-    // TODO
+    return await command_line(`git merge "${merge_from}"`);
 }
