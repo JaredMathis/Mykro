85d68e48a5f98cea8eb92dd80abfdf6d0b1298b2
JaredMathis <jared.mathis@native.consulting>
2022-07-05 13:10:22 -0400

    noop refactor command_line to join together its arguments so it can be used from mykrodev command line

--- src/command_line.mjs
+++ src/command_line.mjs
@@ -1,6 +1,8 @@
 import util from "util";
 import child_process from "child_process";
 let exec = util.promisify(child_process.exec);
-export async function command_line(command) {
+export async function command_line() {
+  let _arguments = Array.from(arguments);
+  let command = _arguments.join(' ');
   return await exec(command);
 }
