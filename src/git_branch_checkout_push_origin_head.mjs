2e1a961479f1f574e78c53de273b3454bc82b31c
JaredMathis <jared.mathis@native.consulting>
2022-07-05 15:22:46 -0400

    file_js_argument_add fjargadd git_branch_checkout_push_origin_head branch_name string_is

--- src/git_branch_checkout_push_origin_head.mjs
+++ src/git_branch_checkout_push_origin_head.mjs
@@ -1,8 +1,7 @@
-import {git_branch_checkout} from './git_branch_checkout.mjs';
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function git_branch_checkout_push_origin_head() {
-    await arguments_assert()(arguments);
-    await git_branch_checkout(branch_name)
+import {string_is} from './string_is.mjs';
+import {git_branch_checkout} from "./git_branch_checkout.mjs";
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function git_branch_checkout_push_origin_head(branch_name) {
+  await arguments_assert(string_is)(arguments);
+  await git_branch_checkout(branch_name);
 }
