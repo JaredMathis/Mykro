35cf34ec49677c85c64c3f34270881d0ca5371fd
JaredMathis <jared.mathis@native.consulting>
2022-07-05 12:06:18 -0400

    fjargadd file_json_read file_path string_is

--- src/file_json_read.mjs
+++ src/file_json_read.mjs
@@ -1,7 +1,5 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function file_json_read() {
-    await arguments_assert()(arguments);
-    // TODO
+import {string_is} from './string_is.mjs';
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function file_json_read(file_path) {
+  await arguments_assert(string_is)(arguments);
 }
