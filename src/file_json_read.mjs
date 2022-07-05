b4c3440ac8eee7c99005ee1c88bb39eeec32acbe
JaredMathis <jared.mathis@native.consulting>
2022-07-05 12:06:57 -0400

    noop added argument to function call

--- src/file_json_read.mjs
+++ src/file_json_read.mjs
@@ -3,5 +3,5 @@ import {string_is} from './string_is.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function file_json_read(file_path) {
   await arguments_assert(string_is)(arguments);
-  file_read(file_path)
+  await file_read(file_path)
 }
