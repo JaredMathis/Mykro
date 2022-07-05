328d1e8a8cf8c99818e520ab6b0c7a4e988a0ff8
JaredMathis <jared.mathis@native.consulting>
2022-07-05 19:23:21 -0400

    main file_js_argument_add fjargadd file_json_save file_path string_is

--- src/file_json_save.mjs
+++ src/file_json_save.mjs
@@ -1,5 +1,8 @@
-import {defined_is} from './defined_is.mjs';
+import {string_is} from './string_is.mjs';
+import {defined_is} from "./defined_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
-export async function file_json_save(value) {
-  await arguments_assert(defined_is)(arguments);
+export async function file_json_save(value, file_path) {
+  await arguments_assert(defined_is, string_is)(arguments);
+  let json = await json_to(config);
+  await file_overwrite(file_path, json);
 }
