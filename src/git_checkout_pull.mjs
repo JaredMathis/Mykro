c8c19f917222ff0298c4aee8e41a68d68528ff4d
JaredMathis <jared.mathis@native.consulting>
2022-07-05 13:58:12 -0400

    noop refactor progress

--- src/git_checkout_pull.mjs
+++ src/git_checkout_pull.mjs
@@ -1,7 +1,11 @@
+import {git_pull} from './git_pull.mjs';
+import {git_checkout} from './git_checkout.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { string_is } from "./string_is.mjs";
 
-export async function git_checkout_pull() {
-    await arguments_assert()(arguments);
-    // TODO
+export async function git_checkout_pull(merge_from) {
+    await arguments_assert(string_is)(arguments);
+    await git_checkout(merge_from)
+    await git_pull(merge_from)
 }
