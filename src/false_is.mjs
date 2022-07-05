6f559ad5b6456be3b0f2203c8de47af30b6e39e8
JaredMathis <jared.mathis@native.consulting>
2022-07-04 21:48:07 -0400

    file_js_imports_mis_ad false_is

--- src/false_is.mjs
+++ src/false_is.mjs
@@ -1,9 +1,6 @@
 import {equals} from './equals.mjs';
 import {defined_is} from './defined_is.mjs';
 import {arguments_assert} from './arguments_assert.mjs';
-import {arguments_assert} from './arguments_assert.mjs';
-import {defined_is} from './defined_is.mjs';
-import {equals} from './equals.mjs';
 export async function false_is(value) {
   await arguments_assert(defined_is)(arguments);
   return equals(value, false);
