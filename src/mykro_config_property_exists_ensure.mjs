4b4821c702f0df62a144683934f10ebab26766dd
JaredMathis <jared.mathis@native.consulting>
2022-07-06 16:15:10 -0400

    main file_js_all_for_each file_js_all_each fjwhite

--- src/mykro_config_property_exists_ensure.mjs
+++ src/mykro_config_property_exists_ensure.mjs
@@ -5,6 +5,12 @@ import {defined_is} from "./defined_is.mjs";
 import {string_is} from "./string_is.mjs";
 export async function mykro_config_property_exists_ensure(config, object, property_name, default_value) {
   await arguments_assert(defined_is, defined_is, string_is, defined_is)(arguments);
+  console.log({
+    config,
+    object,
+    property_name,
+    default_value
+  });
   if (!await js_property_has(object, property_name)) {
     object[property_name] = default_value;
     await mykro_config_save(config);
