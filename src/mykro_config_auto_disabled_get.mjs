39d03ed8ebacdfcc8bf730e20a96900c833ac536
JaredMathis <jared.mathis@native.consulting>
2022-07-06 16:05:02 -0400

    main noop mykro_config_auto_disabled_get implementation

--- src/mykro_config_auto_disabled_get.mjs
+++ src/mykro_config_auto_disabled_get.mjs
@@ -1,7 +1,8 @@
+import {js_property_path_initialize_get} from './js_property_path_initialize_get.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
 export async function mykro_config_auto_disabled_get() {
     await arguments_assert()(arguments);
-    // TODO
+    return await js_property_path_initialize_get(config, ['auto', 'disabled'], false);
 }
