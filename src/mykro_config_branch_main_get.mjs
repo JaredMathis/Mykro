13a34d6c9a0b81bc32a620f060bc6a31520abd5d
JaredMathis <jared.mathis@native.consulting>
2022-07-06 16:01:32 -0400

    main noop refactor

--- src/mykro_config_branch_main_get.mjs
+++ src/mykro_config_branch_main_get.mjs
@@ -11,6 +11,10 @@ export async function mykro_config_branch_main_get() {
   let config = await mykro_config_get();
   let path = ['branch', 'main'];
   let default_value = "main";
+  await js_property_path_initialize(config, path, default_value);
+  return await js_property_path_get(config, path);
+}
+async function js_property_path_initialize(config, path, default_value) {
   let index_last = await list_index_last(path);
   let current = await js_property_path_get_generic(config, path, for_each_lambda);
   async function for_each_lambda(path_part, index) {
@@ -22,8 +26,8 @@ export async function mykro_config_branch_main_get() {
     }
     await mykro_config_property_exists_ensure(config, current, path_part, value);
   }
-  return await js_property_path_get(config, path);
 }
+
 async function js_property_path_get(object, path) {
   let result = await js_property_path_get_generic(object, path, noop);
   return result;
