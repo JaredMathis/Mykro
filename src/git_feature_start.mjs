6a3e4379cef2b2818253f7451a661d977a263cbd
JaredMathis <jared.mathis@native.consulting>
2022-07-05 20:01:07 -0400

    main noop add statement

--- src/git_feature_start.mjs
+++ src/git_feature_start.mjs
@@ -1,7 +1,8 @@
+import {git_branch_checkout_push_origin_head} from './git_branch_checkout_push_origin_head.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
 export async function git_feature_start() {
     await arguments_assert()(arguments);
-    // TODO
+    await git_branch_checkout_push_origin_head(branch_name);
 }
