d869e2fd8b67bdd6c9119d8c2547f2d133a87884
JaredMathis <jared.mathis@native.consulting>
2022-07-05 23:53:52 -0400

    main noop file_js_all_identifiers_get make result unique

--- src/file_js_all_identifiers_get.mjs
+++ src/file_js_all_identifiers_get.mjs
@@ -3,6 +3,7 @@ import {file_js_all} from "./file_js_all.mjs";
 import {file_js_identifiers_get} from "./file_js_identifiers_get.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 import {for_each} from "./for_each.mjs";
+import { list_unique } from "./list_unique.mjs";
 export async function file_js_all_identifiers_get() {
   await arguments_assert()(arguments);
   let result = [];
@@ -11,5 +12,6 @@ export async function file_js_all_identifiers_get() {
     let identifiers = await file_js_identifiers_get(file.name);
     await list_add_all(result, identifiers);
   });
+  result = await list_unique(result);
   return result;
 }
