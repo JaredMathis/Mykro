6e92ddf3f875c0ca665ebdde84f2a718a81b91ba
JaredMathis <jared.mathis@native.consulting>
2022-07-06 00:18:46 -0400

    main file_js_move file_js_move ./file/js/move.mjs

--- src/file_js_move.mjs
+++ src/file_js_move.mjs
@@ -1,7 +1,11 @@
 import {arguments_assert} from "./arguments_assert.mjs";
+import { mykrodev_config_src_path_get } from "./mykrodev_config_src_path_get.mjs";
+import { string_identifier_is } from "./string_identifier_is.mjs";
 import { string_is } from "./string_is.mjs";
-export async function file_js_move(file_path_new) {
-  await arguments_assert(string_is)(arguments);
+export async function file_js_move(function_name, file_path_new) {
+  await arguments_assert(string_identifier_is, string_is)(arguments);
 
-  
+  let directory = await mykrodev_config_src_path_get();
+
+  return directory;
 }
