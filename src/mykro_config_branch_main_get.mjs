499117e8bcdc3e993ff92a81bd04f48f824dd90f
JaredMathis <jared.mathis@native.consulting>
2022-07-06 15:49:14 -0400

    main file_js_new fjew list_index_last

--- src/mykro_config_branch_main_get.mjs
+++ src/mykro_config_branch_main_get.mjs
@@ -1,3 +1,4 @@
+import {list_index_last} from './list_index_last.mjs';
 import {mykro_config_property_exists_ensure} from './mykro_config_property_exists_ensure.mjs';
 import {mykro_config_get} from "./mykro_config_get.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
@@ -7,7 +8,9 @@ export async function mykro_config_branch_main_get() {
   let config = await mykro_config_get();
   let path = ['branch', 'main'];
   let default_value = "main";
+  let index_last = await list_index_last(path);
   await for_each(path, async (path_part, index) => {
+
   })
   await mykro_config_property_exists_ensure(config, config, "branch", {});
   await mykro_config_property_exists_ensure(config, config.branch, "main", default_value);
