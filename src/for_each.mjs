c2f9ca423a28a72264f18c243538805c137d6a55
JaredMathis <jared.mathis@native.consulting>
2022-07-06 15:45:33 -0400

    main noop refactor added index to for_each

--- src/for_each.mjs
+++ src/for_each.mjs
@@ -4,12 +4,14 @@ import {defined_is} from "./defined_is.mjs";
 import {true_is} from "./true_is.mjs";
 export async function for_each(list, for_each_item) {
   await arguments_assert(_.isArray, _.isFunction)(arguments);
+  let index = 0;
   for (let item of list) {
-    let result = await for_each_item(item);
+    let result = await for_each_item(item, index);
     if (await defined_is(result)) {
       if (await true_is(result)) {
         break;
       }
     }
+    index++;
   }
 }
--- src/mykro_config_branch_main_get.mjs
+++ src/mykro_config_branch_main_get.mjs
@@ -1,10 +1,14 @@
 import {mykro_config_property_exists_ensure} from './mykro_config_property_exists_ensure.mjs';
 import {mykro_config_get} from "./mykro_config_get.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
+import { for_each } from './for_each.mjs';
 export async function mykro_config_branch_main_get() {
   await arguments_assert()(arguments);
   let config = await mykro_config_get();
   let path = ['branch', 'main'];
+  await for_each(path, async path_part => {
+
+  })
   await mykro_config_property_exists_ensure(config, config, "branch", {});
   await mykro_config_property_exists_ensure(config, config.branch, "main", "main");
   return config.branch.main;
