2ff9422e94f876f3f1626fe90daa15c0b307fc47
JaredMathis <jared.mathis@native.consulting>
2022-07-06 15:41:19 -0400

    main noop added arguments

--- src/mykro_config_property_exists_ensure.mjs
+++ src/mykro_config_property_exists_ensure.mjs
@@ -2,9 +2,11 @@ import {mykro_config_save} from './mykro_config_save.mjs';
 import {js_property_has} from './js_property_has.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { defined_is } from './defined_is.mjs';
+import { string_is } from './string_is.mjs';
 
-export async function mykro_config_property_exists_ensure() {
-    await arguments_assert()(arguments);
+export async function mykro_config_property_exists_ensure(config, object, property_name, default_value) {
+    await arguments_assert(defined_is, defined_is, string_is, defined_is)(arguments);
     // TODO
 }
 
