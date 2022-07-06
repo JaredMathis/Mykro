cf3a296aa4698fcd4c1decea8fe4b35239905ea1
JaredMathis <jared.mathis@native.consulting>
2022-07-06 16:12:02 -0400

    main noop added indentation to JSON generation

--- src/json_to.mjs
+++ src/json_to.mjs
@@ -2,5 +2,5 @@ import {arguments_assert} from "./arguments_assert.mjs";
 import {defined_is} from "./defined_is.mjs";
 export async function json_to(value) {
   await arguments_assert(defined_is)(arguments);
-  return JSON.stringify(value);
+  return JSON.stringify(value, null, 2);
 }
