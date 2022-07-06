6bdd4f65f39dd0ac3fc56f1c22596391637798a5
JaredMathis <jared.mathis@native.consulting>
2022-07-05 23:46:15 -0400

    main noop added lambda

--- src/file_js_identifier_rename.mjs
+++ src/file_js_identifier_rename.mjs
@@ -12,6 +12,8 @@ export async function file_js_identifier_rename(function_name, name_from, name_t
   let identifiers = await file_js_all_identifiers_get();
   await assert(not(list_contains))(identifiers, name_to);
   await file_js_transform(function_name, ast => {
-    es_traverse();
+    es_traverse(ast, node => {
+      
+    });
   });
 }
