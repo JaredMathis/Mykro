1d99e6276303b1022a2a0146a1b26d87348e6914
JaredMathis <jared.mathis@native.consulting>
2022-07-06 16:04:01 -0400

    main noop moved code

--- src/mykro_config_branch_main_get.mjs
+++ src/mykro_config_branch_main_get.mjs
@@ -12,36 +12,3 @@ export async function mykro_config_branch_main_get() {
   let config = await mykro_config_get();
   return await js_property_path_initialize_get(config, ['branch', 'main'], "main");
 }
-async function js_property_path_initialize_get(config, path, default_value) {
-  await js_property_path_initialize(config, path, default_value);
-  return await js_property_path_get(config, path);
-}
-
-async function js_property_path_initialize(config, path, default_value) {
-  let index_last = await list_index_last(path);
-  let current = await js_property_path_get_generic(config, path, for_each_lambda);
-  async function for_each_lambda(path_part, index) {
-    let value;
-    if (equals(index, index_last)) {
-      value = default_value;
-    } else {
-      value = {};
-    }
-    await mykro_config_property_exists_ensure(config, current, path_part, value);
-  }
-}
-
-async function js_property_path_get(object, path) {
-  let result = await js_property_path_get_generic(object, path, noop);
-  return result;
-}
-
-async function js_property_path_get_generic(object, path, for_each_lambda) {
-  let current = object;
-  await for_each(path, async (path_part, index) => {
-    await for_each_lambda(path_part, index);
-    current = property_get(current, path_part);
-  });
-  return current;
-}
-
