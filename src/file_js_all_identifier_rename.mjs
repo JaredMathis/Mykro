a05f0e8c6bfca2b201991ace3eb16b059266889e
JaredMathis <jared.mathis@native.consulting>
2022-07-05 23:33:01 -0400

    main file_js_argument_add fjargadd file_js_all_identifier_rename name_to string_is

--- src/file_js_all_identifier_rename.mjs
+++ src/file_js_all_identifier_rename.mjs
@@ -1,5 +1,5 @@
-import {string_is} from './string_is.mjs';
+import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
-export async function file_js_all_identifier_rename(name_from) {
-  await arguments_assert(string_is)(arguments);
+export async function file_js_all_identifier_rename(name_from, name_to) {
+  await arguments_assert(string_is, string_is)(arguments);
 }
