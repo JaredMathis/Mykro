6e82f62eed5c20078a534a6bc985bd037b0d4bf6
JaredMathis <jared.mathis@native.consulting>
2022-07-05 23:50:24 -0400

    main noop fixed typo or something

--- src/file_js_all_identifiers_get.mjs
+++ src/file_js_all_identifiers_get.mjs
@@ -1,6 +1,6 @@
 import {list_add_all} from "./list_add_all.mjs";
 import {file_js_all} from "./file_js_all.mjs";
-import {file_js_all_for_each} from "./file_js_all_for_each.mjs";
+import {file_js_identifiers_get} from "./file_js_identifiers_get.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 import {for_each} from "./for_each.mjs";
 export async function file_js_all_identifiers_get() {
@@ -8,7 +8,7 @@ export async function file_js_all_identifiers_get() {
   let result = [];
   let files = await file_js_all();
   await for_each(files, async file => {
-    let identifiers = await file_js_all_identifiers_get(file.name);
+    let identifiers = await file_js_identifiers_get(file.name);
     await list_add_all(result, identifiers);
   });
   return result;
