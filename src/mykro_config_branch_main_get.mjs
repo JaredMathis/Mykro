29e81b2dc1ddb11e0cb92d12689c468b9d463ae5
JaredMathis <jared.mathis@native.consulting>
2022-07-06 15:44:06 -0400

    main noop refactor

--- src/mykro_config_branch_main_get.mjs
+++ src/mykro_config_branch_main_get.mjs
@@ -1,11 +1,10 @@
 import {mykro_config_property_exists_ensure} from './mykro_config_property_exists_ensure.mjs';
-import {mykro_config_save} from "./mykro_config_save.mjs";
 import {mykro_config_get} from "./mykro_config_get.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
-import {js_property_has} from "./js_property_has.mjs";
 export async function mykro_config_branch_main_get() {
   await arguments_assert()(arguments);
   let config = await mykro_config_get();
+  let path = ['branch', 'main'];
   await mykro_config_property_exists_ensure(config, config, "branch", {});
   await mykro_config_property_exists_ensure(config, config.branch, "main", "main");
   return config.branch.main;
