27d4c48005554e0969d8be31bb52a0f8d62a755c
JaredMathis <jared.mathis@native.consulting>
2022-07-05 23:37:11 -0400

    main noop make sure the identifier doesn't already exist

--- src/file_js_all_identifier_rename.mjs
+++ src/file_js_all_identifier_rename.mjs
@@ -1,7 +1,11 @@
+import {not} from './not.mjs';
 import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 import { file_js_all_identifiers_get } from "./file_js_all_identifiers_get.mjs";
+import { assert } from "./assert.mjs";
+import { list_contains } from "./list_contains.mjs";
 export async function file_js_all_identifier_rename(name_from, name_to) {
   await arguments_assert(string_is, string_is)(arguments);
   let identifiers = await file_js_all_identifiers_get()
+  await assert(not(list_contains))(identifiers, name_to);
 }
