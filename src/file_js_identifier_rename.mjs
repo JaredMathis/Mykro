33d5d027dcbb5b0f4225cb2e603412dd569400ba
JaredMathis <jared.mathis@native.consulting>
2022-07-05 23:39:54 -0400

    main noop progress

--- src/file_js_identifier_rename.mjs
+++ src/file_js_identifier_rename.mjs
@@ -4,8 +4,13 @@ import {arguments_assert} from "./arguments_assert.mjs";
 import { file_js_all_identifiers_get } from "./file_js_all_identifiers_get.mjs";
 import { assert } from "./assert.mjs";
 import { list_contains } from "./list_contains.mjs";
-export async function file_js_identifier_rename(name_from, name_to) {
-  await arguments_assert(string_is, string_is)(arguments);
+import { file_js_transform } from './file_js_transform.mjs';
+import { string_identifier_is } from './string_identifier_is.mjs';
+export async function file_js_identifier_rename(function_name, name_from, name_to) {
+  await arguments_assert(string_identifier_is, string_is, string_is)(arguments);
   let identifiers = await file_js_all_identifiers_get()
   await assert(not(list_contains))(identifiers, name_to);
+  await file_js_transform(function_name, ast => {
+
+  });
 }
