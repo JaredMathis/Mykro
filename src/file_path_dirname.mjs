88a8bc0fb265b310d74a187f0c41a67688a4764c
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:37:48 -0400

    main noop implmentation

--- src/file_path_dirname.mjs
+++ src/file_path_dirname.mjs
@@ -3,4 +3,5 @@ import {arguments_assert} from "./arguments_assert.mjs";
 import path from 'path'
 export async function file_path_dirname(file_path) {
   await arguments_assert(string_is)(arguments);
+  return path.dirname(file_path)
 }
