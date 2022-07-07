9ff632d0918f7a21a8334c1d342fba47cdea7199
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:18:39 -0400

    main noop fixed typo

--- src/string_join.mjs
+++ src/string_join.mjs
@@ -1,6 +1,7 @@
 import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
+import { list_is } from "./list_is.mjs";
 export async function string_join(input, join_by) {
-  await arguments_assert(string_is, string_is)(arguments);
+  await arguments_assert(list_is, string_is)(arguments);
   return input.join(join_by)
 }
