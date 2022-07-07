662a5a15ad7227d1fb9afdac955448bf7c15ea72
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:17:41 -0400

    main noop progress

--- src/string_join.mjs
+++ src/string_join.mjs
@@ -2,4 +2,5 @@ import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function string_join(input, join_by) {
   await arguments_assert(string_is, string_is)(arguments);
+  return input.join(join_by)
 }
