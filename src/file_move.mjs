4731321b6d2cb564222fa4f00db63f41a5353cc0
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:38:21 -0400

    main noop progress

--- src/file_move.mjs
+++ src/file_move.mjs
@@ -1,7 +1,9 @@
+import {file_path_dirname} from './file_path_dirname.mjs';
 import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 import fs from 'fs';
 export async function file_move(file_path_from, file_path_to) {
   await arguments_assert(string_is, string_is)(arguments);
+  let directory_path = await file_path_dirname(file_path_to)
   await fs.promises.rename(file_path_from, file_path_to);
 }
