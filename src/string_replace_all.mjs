61504fff46dab1c0e1883851b4b37adcfe358875
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:14:50 -0400

    main file_js_argument_add fjsargadd string_replace_all search string_is

--- src/string_replace_all.mjs
+++ src/string_replace_all.mjs
@@ -1,5 +1,5 @@
-import {string_is} from './string_is.mjs';
+import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
-export async function string_replace_all(input) {
-  await arguments_assert(string_is)(arguments);
+export async function string_replace_all(input, search) {
+  await arguments_assert(string_is, string_is)(arguments);
 }
