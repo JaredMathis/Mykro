d69318cc2d304828f41f6afc6f7531e54f9eccbd
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:17:17 -0400

    main file_js_argument_add fjsargadd string_join input string_is

--- src/string_join.mjs
+++ src/string_join.mjs
@@ -1,7 +1,5 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function string_join() {
-    await arguments_assert()(arguments);
-    // TODO
+import {string_is} from './string_is.mjs';
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function string_join(input) {
+  await arguments_assert(string_is)(arguments);
 }
