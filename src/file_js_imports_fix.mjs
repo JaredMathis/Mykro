68312930aaa2d073d062413c67b446d18bdbfd2c
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:50:41 -0400

    main noop progress

--- src/file_js_imports_fix.mjs
+++ src/file_js_imports_fix.mjs
@@ -1,7 +1,11 @@
 import {string_identifier_is} from './string_identifier_is.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
-import { file_js_all_match_exact } from './file_js_all_match_exact.mjs';
+import { file_js_transform } from './file_js_transform.mjs';
 export async function file_js_imports_fix(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
-  let match = await file_js_all_match_exact(function_name);
+  await file_js_transform(function_name, ast => {
+    await es_traverse(ast, async node => {
+
+    })
+  });
 }
