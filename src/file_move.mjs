c56b192c1d1354fc2f04585ba483dc7ec8644c50
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:06:36 -0400

    main file_js_argument_add fjargadd file_move file_path_to string_is

--- src/file_move.mjs
+++ src/file_move.mjs
@@ -1,5 +1,5 @@
-import {string_is} from './string_is.mjs';
+import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
-export async function file_move(file_path_from) {
-  await arguments_assert(string_is)(arguments);
+export async function file_move(file_path_from, file_path_to) {
+  await arguments_assert(string_is, string_is)(arguments);
 }
