b7647fbc6479dede2a81ccac42abfe4948cca60f
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:33:05 -0400

    main file_js_argument_add fjargadd file_directory_create_if_not_exists directory_path string_is

--- src/file_directory_create_if_not_exists.mjs
+++ src/file_directory_create_if_not_exists.mjs
@@ -1,7 +1,5 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function file_directory_create_if_not_exists() {
-    await arguments_assert()(arguments);
-    // TODO
+import {string_is} from './string_is.mjs';
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function file_directory_create_if_not_exists(directory_path) {
+  await arguments_assert(string_is)(arguments);
 }
