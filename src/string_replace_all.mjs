2d9ca4d929c89d4f10dbd72c1b6d1065e101cd23
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:16:05 -0400

    main noop added awaits

--- src/string_replace_all.mjs
+++ src/string_replace_all.mjs
@@ -1,5 +1,8 @@
 import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
+import { string_split } from "./string_split.mjs";
 export async function string_replace_all(input, search, replacement) {
   await arguments_assert(string_is, string_is, string_is)(arguments);
+  let split = await string_split(input, search);
+  
 }
