7a832f50c04a862e6c4399096622c5ee3da32b60
JaredMathis <jared.mathis@native.consulting>
2022-07-07 09:55:16 -0400

    main noop rename function

--- src/npm_install_latest_mykro.mjs
+++ src/npm_install_latest_mykro.mjs
@@ -2,7 +2,7 @@ import {npm_install_latest} from './npm_install_latest.mjs';
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
-export async function npm_install_mykro() {
+export async function npm_install_latest_mykro() {
     await arguments_assert()(arguments);
     await npm_install_latest(`mykro`)
 }
