7c593ad1168166c9b8704edfbd0416764c8624d5
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:48:40 -0400

    main noop refactor

--- src/file_js_name_to_path.mjs
+++ src/file_js_name_to_path.mjs
@@ -1,8 +1,7 @@
 import {arguments_assert} from "./arguments_assert.mjs";
 import {string_is} from "./string_is.mjs";
-import path from "path";
-import {mykro_config_src_path_get} from "./mykro_config_src_path_get.mjs";
 export async function file_js_name_to_path(function_name) {
   await arguments_assert(string_is)(arguments);
-  return path.join(await mykro_config_src_path_get(), `${function_name}.mjs`);
+  let match = await file_js_all_match_exact(function_name);
+  return match.file_path;
 }
