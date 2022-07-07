d46e183905972ae7d712c58866363f6d5b95b1cd
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:49:00 -0400

    main noop refactor

--- src/file_js_name_to_path.mjs
+++ src/file_js_name_to_path.mjs
@@ -1,5 +1,6 @@
 import {arguments_assert} from "./arguments_assert.mjs";
 import {string_is} from "./string_is.mjs";
+import {file_js_all_match_exact} from "./file_js_all_match_exact.mjs";
 export async function file_js_name_to_path(function_name) {
   await arguments_assert(string_is)(arguments);
   let match = await file_js_all_match_exact(function_name);
