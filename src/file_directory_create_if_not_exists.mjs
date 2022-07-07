e4eedcc0fff7e3d2d413f3f74fe2c326333deab6
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:32:39 -0400

    main file_js_new fjew file_directory_create_if_not_exists

---
+++ src/file_directory_create_if_not_exists.mjs
@@ -0,0 +1,7 @@
+
+import { arguments_assert } from "./arguments_assert.mjs";
+
+export async function file_directory_create_if_not_exists() {
+    await arguments_assert()(arguments);
+    // TODO
+}
