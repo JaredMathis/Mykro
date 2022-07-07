8e367520dd58247c48c286a4bf78f374b80eeb71
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:08:13 -0400

    main noop implmentation

--- src/file_move.mjs
+++ src/file_move.mjs
@@ -1,5 +1,7 @@
 import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
+import fs from 'fs';
 export async function file_move(file_path_from, file_path_to) {
   await arguments_assert(string_is, string_is)(arguments);
+  await fs.promises.rename(file_path_from, file_path_to);
 }
