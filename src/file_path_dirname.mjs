b72d96892ae0410f27577db8953b69978a8c141e
JaredMathis <jared.mathis@native.consulting>
2022-07-07 19:36:53 -0400

    main file_js_argument_add fjargadd file_path_dirname file_path string_is

--- src/file_path_dirname.mjs
+++ src/file_path_dirname.mjs
@@ -1,7 +1,5 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function file_path_dirname() {
-    await arguments_assert()(arguments);
-    // TODO
+import {string_is} from './string_is.mjs';
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function file_path_dirname(file_path) {
+  await arguments_assert(string_is)(arguments);
 }
