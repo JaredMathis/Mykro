5351a6bb26b2b4b0ba6b54ce88c04a76fb1fdfe8
JaredMathis <jared.mathis@native.consulting>
2022-07-07 09:50:37 -0400

    main noop turn off fast forward git merging

--- src/git_merge.mjs
+++ src/git_merge.mjs
@@ -3,5 +3,5 @@ import {command_line} from "./command_line.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function git_merge(merge_from) {
   await arguments_assert(string_is)(arguments);
-  return await command_line(`git merge "${merge_from}"`);
+  return await command_line(`git merge --no-ff "${merge_from}"`);
 }
