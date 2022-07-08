1efb9d7ab5571ee16c4de4ddad2b6932d6597d57
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:47:27 -0400

    main noop progress

--- src/file/js/export/declaration/default/to/named.mjs
+++ src/file/js/export/declaration/default/to/named.mjs
@@ -4,6 +4,8 @@ import {js_string_identifier_is} from "./../../../../../../js/string/identifier/
 import {js_arguments_assert} from "./../../../../../../js/arguments/assert.mjs";
 import {js_merge} from "./../../../../../../js/merge.mjs";
 import {js_for_each} from "./../../../../../../js/for/each.mjs";
+import { list_size } from "../../../../../../list/size.mjs";
+import { list_single } from "../../../../../../list/single.mjs";
 export async function file_js_export_declaration_default_to_named(function_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
   await file_js_transform_ast(function_name, async node => {
@@ -19,7 +21,11 @@ export async function file_js_export_declaration_default_to_named(function_name)
   let function_names = await file_js_all_function_names();
   await js_for_each(function_names, async function_name_other => {
     await file_js_transform_ast(function_name_other, async node => {
-
+      if (node.type === 'ImportDeclaration') {
+        if (await list_size(node.specifiers) === 1) {
+          let specifier = await list_single(node.specifiers);
+        }
+      }
     });
   });
 }
