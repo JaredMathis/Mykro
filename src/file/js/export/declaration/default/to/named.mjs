6a2e22c0cfd308e3fe96273bfebca7ade3bc70f2
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:45:43 -0400

    main noop unused import

--- src/file/js/export/declaration/default/to/named.mjs
+++ src/file/js/export/declaration/default/to/named.mjs
@@ -3,7 +3,6 @@ import {file_js_transform_ast} from "./../../../../transform/ast.mjs";
 import {js_string_identifier_is} from "./../../../../../../js/string/identifier/is.mjs";
 import {js_arguments_assert} from "./../../../../../../js/arguments/assert.mjs";
 import {js_merge} from "./../../../../../../js/merge.mjs";
-import {file_js_all} from "./../../../../all.mjs";
 import {js_for_each} from "./../../../../../../js/for/each.mjs";
 export async function file_js_export_declaration_default_to_named(function_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
