1b6ddca79079df2e3b70454f42c06ab5d615a9aa
JaredMathis <jared.mathis@native.consulting>
2022-07-08 00:10:40 -0400

    undid change doesn't work

--- src/es_parse.mjs
+++ src/es_parse.mjs
@@ -1,9 +1,9 @@
 import {arguments_assert} from "./arguments_assert.mjs";
 import {string_is} from "./string_is.mjs";
-import meriyah from "meriyah";
+import {parseScript} from "meriyah";
 export async function es_parse(unparsed) {
   await arguments_assert(string_is)(arguments);
-  return meriyah.parseScript(unparsed, {
+  return parseScript(unparsed, {
     module: true
   });
 }
