e5fda182a91841844af79f2aad98ecb258fbb896
JaredMathis <jared.mathis@native.consulting>
2022-07-08 14:03:14 -0400

    main file_js_argument_add file_js_argadd file_js_all_rename_if_starts_with prefix_new string_is

--- src/file/js/all/rename/if/starts/with.mjs
+++ src/file/js/all/rename/if/starts/with.mjs
@@ -1,4 +1,4 @@
 import {js_arguments_assert} from "./../../../../../../js/arguments/assert.mjs";
-export async function file_js_all_rename_if_starts_with(prefix_old) {
-  await js_arguments_assert(string_is)(arguments);
+export async function file_js_all_rename_if_starts_with(prefix_old, prefix_new) {
+  await js_arguments_assert(string_is, string_is)(arguments);
 }
