1a92a804ef45bdfeac063fb751c4d4be0b712635
JaredMathis <jared.mathis@native.consulting>
2022-07-07 23:44:49 -0400

    main noop progres

--- src/file_js_all_folderize.mjs
+++ src/file_js_all_folderize.mjs
@@ -1,7 +1,12 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
+import { file_js_all_for_each } from "./file_js_all_for_each.mjs";
+import { file_js_folderize } from "./file_js_folderize.mjs";
+import { file_js_imports_fix } from "./file_js_imports_fix.mjs";
 
 export async function file_js_all_folderize() {
     await arguments_assert()(arguments);
-    // TODO
+    
+    await file_js_all_for_each(file_js_folderize.name);
+    await file_js_all_for_each(file_js_imports_fix.name);
 }
