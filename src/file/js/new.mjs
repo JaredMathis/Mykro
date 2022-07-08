7c3456b6829d8cbffaeb8866c06dfd08541c9e6e
JaredMathis <jared.mathis@native.consulting>
2022-07-08 14:01:38 -0400

    main noop bugfix

--- src/file/js/new.mjs
+++ src/file/js/new.mjs
@@ -5,14 +5,15 @@ import {file_overwrite} from "./../overwrite.mjs";
 import {msft_vs_code_open} from "./../../msft/vs/code/open.mjs";
 import {file_js_path_get} from "./path/get.mjs";
 import {file_js_imports_resolve} from "./imports/resolve.mjs";
+import { js_arguments_assert } from "../../js/arguments/assert.mjs";
 export async function file_js_new(function_name) {
   let file_path = await file_js_path_get(function_name);
   await js_assert(js_not(file_exists))(file_path);
   await file_overwrite(file_path, `
-import { arguments_assert } from "./arguments_assert.mjs";
+import { ${js_arguments_assert.name} } from "./${js_arguments_assert.name}.mjs";
 
 export async function ${function_name}() {
-    await arguments_assert()(arguments);
+    await ${js_arguments_assert.name}()(arguments);
     // TODO
 }`);
   await file_js_imports_resolve(function_name);
