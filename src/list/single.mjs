a926e43f754d020c6e9f9ed0efe8ad88fda8c8cc
JaredMathis <jared.mathis@native.consulting>
2022-07-08 14:01:10 -0400

    troubleshoot data

--- src/list/single.mjs
+++ src/list/single.mjs
@@ -6,6 +6,6 @@ import {list_is} from "./is.mjs";
 import {list_size} from "./size.mjs";
 export async function list_single(list) {
   await js_arguments_assert(list_is)(arguments);
-  await js_assert(js_equals)(await list_size(list), 1);
+  await js_assert(js_equals, {list})(await list_size(list), 1);
   return await list_first(list);
 }
