06be59850aa0a67dfe5b9fc8d13857e90f7473d8
JaredMathis <jared.mathis@native.consulting>
2022-07-08 12:19:57 -0400

    main noop progress

--- src/file/js/new.mjs
+++ src/file/js/new.mjs
@@ -5,8 +5,10 @@ import {file_overwrite} from "./../overwrite.mjs";
 import {file_js_name_to_path} from "./name/to/path.mjs";
 import {msft_vs_code_open} from "./../../msft/vs/code/open.mjs";
 import {file_js_folderize} from "./folderize.mjs";
+import { file_js_path_get } from "./path/get.mjs";
+import { file_js_imports_fix } from "./imports/fix.mjs";
 export async function file_js_new(function_name) {
-  let file_path = await file_js_name_to_path(function_name);
+  let file_path = await file_js_path_get(function_name);
   await assert(not(file_exists))(file_path);
   await file_overwrite(file_path, `
 import { arguments_assert } from "./arguments_assert.mjs";
@@ -15,6 +17,6 @@ export async function ${function_name}() {
     await arguments_assert()(arguments);
     // TODO
 }`);
-  await file_js_folderize(function_name);
+  await file_js_imports_fix(function_name);
   await msft_vs_code_open(file_path);
 }
