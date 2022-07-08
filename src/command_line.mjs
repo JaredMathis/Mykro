517669095607d019f73670fdc1891d8e660f4b85
JaredMathis <jared.mathis@native.consulting>
2022-07-08 00:04:22 -0400

    fix command_line to make auto code easier

--- src/command_line.mjs
+++ src/command_line.mjs
@@ -1,7 +1,7 @@
 import util from "util";
 import child_process from "child_process";
-let exec = util.promisify(child_process.exec);
 export async function command_line() {
+  let exec = util.promisify(child_process.exec);
   let _arguments = Array.from(arguments);
   let command = _arguments.join(" ");
   return await exec(command);
