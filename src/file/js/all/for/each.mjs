b9db6ebdc421939c41d448383ef45381fd7a3194
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:31:21 -0400

    main file_js_auto file_js_all_for_each

--- src/file/js/all/for/each.mjs
+++ src/file/js/all/for/each.mjs
@@ -1,3 +1,4 @@
+import {file_js_all_function_names} from "./../function/names.mjs";
 import {js_error} from "./../../../../js/error.mjs";
 import {file_js_run} from "./../../run.mjs";
 import {file_js_all} from "./../../all.mjs";
@@ -11,8 +12,8 @@ import {list_add_all} from "./../../../../list/add/all.mjs";
 export async function file_js_all_for_each() {
   let function_name = arguments[0];
   let remaining = await list_starting_at(Array.from(arguments), 1);
-  let files = await file_js_all();
-  await js_for_each(files, async file => {
-    await file_js_run(function_name, await list_add_all([file.name], remaining), js_error, noop, js_log_error, js_error, noop);
+  let function_names = await file_js_all_function_names();
+  await js_for_each(function_names, async function_name_other => {
+    await file_js_run(function_name, await list_add_all([function_name_other], remaining), js_error, noop, js_log_error, js_error, noop);
   });
 }
