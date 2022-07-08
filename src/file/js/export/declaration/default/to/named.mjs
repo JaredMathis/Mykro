dc217a3dd9a4e01c6e50760092e8f1b5b1cfd88a
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:55:06 -0400

    main file_js_auto file_js_export_declaration_default_to_named

--- src/file/js/export/declaration/default/to/named.mjs
+++ src/file/js/export/declaration/default/to/named.mjs
@@ -1,13 +1,14 @@
+import {js_clone} from "./../../../../../../js/clone.mjs";
 import {file_js_all_function_names} from "./../../../../all/function/names.mjs";
 import {file_js_transform_ast} from "./../../../../transform/ast.mjs";
 import {js_string_identifier_is} from "./../../../../../../js/string/identifier/is.mjs";
 import {js_arguments_assert} from "./../../../../../../js/arguments/assert.mjs";
 import {js_merge} from "./../../../../../../js/merge.mjs";
 import {js_for_each} from "./../../../../../../js/for/each.mjs";
-import { list_size } from "../../../../../../list/size.mjs";
-import { list_single } from "../../../../../../list/single.mjs";
-import { js_assert } from "../../../../../../js/assert.mjs";
-import { js_equals } from "../../../../../../js/equals.mjs";
+import {list_size} from "./../../../../../../list/size.mjs";
+import {list_single} from "./../../../../../../list/single.mjs";
+import {js_assert} from "./../../../../../../js/assert.mjs";
+import {js_equals} from "./../../../../../../js/equals.mjs";
 export async function file_js_export_declaration_default_to_named(function_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
   await file_js_transform_ast(function_name, async node => {
@@ -23,18 +24,18 @@ export async function file_js_export_declaration_default_to_named(function_name)
   let function_names = await file_js_all_function_names();
   await js_for_each(function_names, async function_name_other => {
     await file_js_transform_ast(function_name_other, async node => {
-      if (node.type === 'ImportDeclaration') {
+      if (node.type === "ImportDeclaration") {
         if (await list_size(node.specifiers) === 1) {
           let specifier = await list_single(node.specifiers);
-          if (specifier.type === 'ImportDefaultSpecifier') {
+          if (specifier.type === "ImportDefaultSpecifier") {
             const local = specifier.local;
-            await js_assert(js_equals)(local.type, 'Identifier');
+            await js_assert(js_equals)(local.type, "Identifier");
             if (local.name === function_name) {
               let imported = await js_clone(local);
               await js_merge(specifier, {
-                type: 'ImportSpecifier',
-                imported,
-              })
+                type: "ImportSpecifier",
+                imported
+              });
             }
           }
         }
