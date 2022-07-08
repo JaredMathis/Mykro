b7499ac1edcc33d343673e6c0fe5c26909fd1876
JaredMathis <jared.mathis@native.consulting>
2022-07-08 18:44:52 -0400

    main file_js_argument_add file_js_argadd file_js_transform_ast ast_node_for_each js_function_is

--- src/file/js/transform/ast.mjs
+++ src/file/js/transform/ast.mjs
@@ -1,4 +1,4 @@
 import {js_arguments_assert} from "./../../../js/arguments/assert.mjs";
-export async function file_js_transform_ast(function_name) {
-  await js_arguments_assert(js_string_identifier_is)(arguments);
+export async function file_js_transform_ast(function_name, ast_node_for_each) {
+  await js_arguments_assert(js_string_identifier_is, js_function_is)(arguments);
 }
