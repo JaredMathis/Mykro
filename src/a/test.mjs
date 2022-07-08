9cbd7cfdb723db6d5362184a78d24d17e3f06faf
JaredMathis <jared.mathis@native.consulting>
2022-07-07 21:32:29 -0400

    main noop progress

--- src/a/test.mjs
+++ src/a/test.mjs
@@ -1,4 +1,8 @@
+import { a } from "../a.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function atest_rest() {
   await arguments_assert()(arguments);
+  a();
+  b();
+  c();
 }
