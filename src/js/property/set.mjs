b6c37ce21804d055083ea6bb334ecdd383ac0d6c
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:16:22 -0400

    main file_js_argument_add file_js_argadd js_property_set object defined_is

--- src/js/property/set.mjs
+++ src/js/property/set.mjs
@@ -1,4 +1,4 @@
 import {js_arguments_assert} from "./../arguments/assert.mjs";
-export async function js_property_set() {
-  await js_arguments_assert()(arguments);
+export async function js_property_set(object) {
+  await js_arguments_assert(defined_is)(arguments);
 }
