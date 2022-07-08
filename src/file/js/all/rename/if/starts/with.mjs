1b7a7a5225d01a441de05d85841dc0e736839a2b
JaredMathis <jared.mathis@native.consulting>
2022-07-08 14:01:51 -0400

    main file_js_new file_js_all_rename_if_starts_with

---
+++ src/file/js/all/rename/if/starts/with.mjs
@@ -0,0 +1,4 @@
+import {js_arguments_assert} from "./../../../../../../js/arguments/assert.mjs";
+export async function file_js_all_rename_if_starts_with() {
+  await js_arguments_assert()(arguments);
+}
