ed974df96a07c3329f057a30d41377276766cb8d
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:20:46 -0400

    main file_js_auto file_js_export_declaration_default_to_named

--- src/file/js/export/declaration/default/to/named.mjs
+++ src/file/js/export/declaration/default/to/named.mjs
@@ -1,7 +1,7 @@
 import {file_js_transform_ast} from "./../../../../transform/ast.mjs";
 import {js_string_identifier_is} from "./../../../../../../js/string/identifier/is.mjs";
 import {js_arguments_assert} from "./../../../../../../js/arguments/assert.mjs";
-import { js_merge } from "../../../../../../js/merge.mjs";
+import {js_merge} from "./../../../../../../js/merge.mjs";
 export async function file_js_export_declaration_default_to_named(file_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
   await file_js_transform_ast(file_name, async node => {
@@ -9,8 +9,8 @@ export async function file_js_export_declaration_default_to_named(file_name) {
       let merged = {
         type: "ExportNamedDeclaration",
         specifiers: [],
-        source: null,
-      }
+        source: null
+      };
       await js_merge(node, merged);
     }
   });
