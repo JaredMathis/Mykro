9759756f534745c76df9fe8f3fa6841c61bdfdde
JaredMathis <jared.mathis@native.consulting>
2022-07-08 14:11:42 -0400

    main file_js_auto string_prefix_replace

--- src/string/prefix/replace.mjs
+++ src/string/prefix/replace.mjs
@@ -1,3 +1,4 @@
+import {string_is} from "./../is.mjs";
 import {js_arguments_assert} from "./../../js/arguments/assert.mjs";
 export async function string_prefix_replace(prefix_old, prefix_new) {
   await js_arguments_assert(string_is, string_is)(arguments);
