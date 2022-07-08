451bb7b2f4dec79c54c7d56356410e86d36d36a3
JaredMathis <jared.mathis@native.consulting>
2022-07-07 21:30:58 -0400

    main noop progress

--- src/equals.mjs
+++ src/equals.mjs
@@ -1,3 +1,4 @@
+import {a} from './a.mjs';
 export function equals(a, b) {
   return a === b;
 }
--- src/run_line.mjs
+++ src/run_line.mjs
@@ -1,3 +1,4 @@
+import {a} from './a.mjs';
 import {string_starts_with_ensure} from "./string_starts_with_ensure.mjs";
 import {string_starts_with} from "./string_starts_with.mjs";
 import _ from "lodash";
