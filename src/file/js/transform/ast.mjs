568d590b03bb6c2ef528d0960d56ad666bd4fefd
JaredMathis <jared.mathis@native.consulting>
2022-07-08 18:45:58 -0400

    main noop progress

--- src/file/js/transform/ast.mjs
+++ src/file/js/transform/ast.mjs
@@ -3,4 +3,7 @@ import {js_string_identifier_is} from "./../../../js/string/identifier/is.mjs";
 import {js_arguments_assert} from "./../../../js/arguments/assert.mjs";
 export async function file_js_transform_ast(function_name, ast_node_for_each) {
   await js_arguments_assert(js_string_identifier_is, js_function_is)(arguments);
+  await file_js_transform(function_name, async ast => {
+    await es_traverse(ast, ast_node_for_each);
+  });
 }
