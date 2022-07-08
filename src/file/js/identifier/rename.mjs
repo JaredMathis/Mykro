62dceed62c0d2379afbbd06dc283f4ee86cd136d
JaredMathis <jared.mathis@native.consulting>
2022-07-08 18:47:59 -0400

    main noop refactor

--- src/file/js/identifier/rename.mjs
+++ src/file/js/identifier/rename.mjs
@@ -9,17 +9,16 @@ import {file_js_transform} from "./../transform.mjs";
 import {js_string_identifier_is} from "./../../../js/string/identifier/is.mjs";
 import {js_equals} from "./../../../js/equals.mjs";
 import {file_js_identifiers_get} from "./../identifiers/get.mjs";
+import { file_js_transform_ast } from "../transform/ast.mjs";
 export async function file_js_identifier_rename(function_name, name_from, name_to) {
   await js_arguments_assert(js_string_identifier_is, js_string_is, js_string_is)(arguments);
   let identifiers = await file_js_identifiers_get(function_name);
   await js_assert(js_not(list_contains))(identifiers, name_to);
-  await file_js_transform(function_name, async ast => {
-    await es_traverse(ast, async node => {
-      if (js_equals(node.type, "Identifier")) {
-        if (js_equals(node.name, name_from)) {
-          node.name = name_to;
-        }
+  await file_js_transform_ast(function_name, async node => {
+    if (js_equals(node.type, "Identifier")) {
+      if (js_equals(node.name, name_from)) {
+        node.name = name_to;
       }
-    });
+    }
   });
 }
