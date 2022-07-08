26174eabea7c398502686d5a0f5fc5d216dda27d
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:12:13 -0400

    main file_js_argument_add file_js_argadd js_merge into js_defined_is

--- src/js/merge.mjs
+++ src/js/merge.mjs
@@ -1,4 +1,4 @@
 import {js_arguments_assert} from "./arguments/assert.mjs";
-export async function js_merge() {
-  await js_arguments_assert()(arguments);
+export async function js_merge(into) {
+  await js_arguments_assert(js_defined_is)(arguments);
 }
