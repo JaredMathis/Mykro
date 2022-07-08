3b1f93d925a5e5e62a4b002daa1656009efde2db
JaredMathis <jared.mathis@native.consulting>
2022-07-08 12:47:21 -0400

    main file_js_rename false_is js_false_is

--- src/probability/is.mjs
+++ src/probability/is.mjs
@@ -4,7 +4,7 @@ import {defined_is} from "./../defined/is.mjs";
 import {number_at_most} from "./../number/at/most.mjs";
 import {number_negative_not_is} from "./../number/negative/not/is.mjs";
 import {list_where} from "./../list/where.mjs";
-import {false_is} from "./../false/is.mjs";
+import {js_false_is} from "./../js/false/is.mjs";
 import {true_is} from "./../true/is.mjs";
 export async function probability_is(p) {
   await arguments_assert(defined_is)(arguments);
