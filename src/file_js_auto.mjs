3554514effea2603298dd1f52293e974790d3fa8
JaredMathis <jared.mathis@native.consulting>
2022-07-08 12:03:18 -0400

    main file_js_argument_add fjargadd file_js_auto file_name string_identifier_is

--- src/file_js_auto.mjs
+++ src/file_js_auto.mjs
@@ -1,7 +1,4 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function file_js_auto() {
-    await arguments_assert()(arguments);
-    // TODO
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function file_js_auto(file_name) {
+  await arguments_assert(string_identifier_is)(arguments);
 }
