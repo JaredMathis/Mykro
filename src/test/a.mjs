e6116d40f271b87ccd03f52b24ad55afc2ecc300
JaredMathis <jared.mathis@native.consulting>
2022-07-08 12:33:21 -0400

    main noop test code

--- src/test/a.mjs
+++ src/test/a.mjs
@@ -1,4 +1,8 @@
 import {arguments_assert} from "./../arguments/assert.mjs";
+import { test_a_b } from "./a/b.mjs";
+import { test_a_b_c } from "./a/b/c.mjs";
 export async function test_a() {
   await arguments_assert()(arguments);
+  test_a_b();
+  test_a_b_c();
 }
--- src/test/a/b.mjs
+++ src/test/a/b.mjs
@@ -1,4 +1,6 @@
 import {arguments_assert} from "./../../arguments/assert.mjs";
+import { test_a_b_c } from "./b/c.mjs";
 export async function test_a_b() {
   await arguments_assert()(arguments);
+  test_a_b_c();
 }
