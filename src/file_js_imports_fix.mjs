dd45e41142ddaf28dbe1a9f956f0de3cbc27ba1a
JaredMathis <jared.mathis@native.consulting>
2022-07-07 21:59:06 -0400

    main file_js_imports_fix a_test

--- src/file_js_imports_fix.mjs
+++ src/file_js_imports_fix.mjs
@@ -3,10 +3,11 @@ import {es_traverse} from './es_traverse.mjs';
 import {string_identifier_is} from './string_identifier_is.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
 import { file_js_transform } from './file_js_transform.mjs';
-import { file_js_all } from './file_js_all.mjs';
+import { file_js_all_match_exact } from './file_js_all_match_exact.mjs';
 export async function file_js_imports_fix(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
-  let files = await file_js_all();
+  let match = await file_js_all_match_exact(function_name)
+  console.log({match})
   await file_js_transform(function_name, async ast => {
     await es_traverse(ast, async node => {
       if (node.type === 'ImportDeclaration') {
