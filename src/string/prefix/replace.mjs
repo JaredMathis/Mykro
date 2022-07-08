87a769f8a24e74e45a3ca8c2b07e3cac4dde9f33
JaredMathis <jared.mathis@native.consulting>
2022-07-08 14:11:08 -0400

    main file_js_argument_add file_js_argadd string_prefix_replace prefix_old string_is

--- src/string/prefix/replace.mjs
+++ src/string/prefix/replace.mjs
@@ -1,4 +1,4 @@
 import {js_arguments_assert} from "./../../js/arguments/assert.mjs";
-export async function string_prefix_replace() {
-  await js_arguments_assert()(arguments);
+export async function string_prefix_replace(prefix_old) {
+  await js_arguments_assert(string_is)(arguments);
 }
