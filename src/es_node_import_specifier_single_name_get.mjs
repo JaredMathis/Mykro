9715e2856c055c62c93e2321a5c345409a5e2aa0
JaredMathis <jared.mathis@native.consulting>
2022-07-07 21:52:06 -0400

    main noop refactor

--- src/es_node_import_specifier_single_name_get.mjs
+++ src/es_node_import_specifier_single_name_get.mjs
@@ -2,6 +2,7 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { assert } from "./assert.mjs";
 import { defined_is } from "./defined_is.mjs";
+import { equals_json } from "./equals_json.mjs";
 import { for_each } from "./for_each.mjs";
 import { list_single } from "./list_single.mjs";
 import { property_get } from "./property_get.mjs";
