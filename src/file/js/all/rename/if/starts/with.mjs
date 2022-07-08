1d2f9feb784aa083cf080651032cb472138ef1d6
JaredMathis <jared.mathis@native.consulting>
2022-07-08 14:06:36 -0400

    main file_js_whitespace file_js_all_rename_if_starts_with

--- src/file/js/all/rename/if/starts/with.mjs
+++ src/file/js/all/rename/if/starts/with.mjs
@@ -1,7 +1,9 @@
 import {file_js_all} from "./../../../../all.mjs";
 import {string_is} from "./../../../../../../string/is.mjs";
 import {js_arguments_assert} from "./../../../../../../js/arguments/assert.mjs";
+import {for_each} from "../../../../../../for/each.mjs";
 export async function file_js_all_rename_if_starts_with(prefix_old, prefix_new) {
   await js_arguments_assert(string_is, string_is)(arguments);
   let files = await file_js_all();
+  await for_each(files, file => {});
 }
