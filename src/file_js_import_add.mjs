14528dce22b738b21de12bfaaf26414e7d30344c
JaredMathis <jared.mathis@native.consulting>
2022-07-07 21:40:02 -0400

    main noop unused imports

--- src/file_js_import_add.mjs
+++ src/file_js_import_add.mjs
@@ -1,11 +1,11 @@
 import {arguments_assert} from "./arguments_assert.mjs";
-import {file_js_name_to_path} from "./file_js_name_to_path.mjs";
+import { file_js_all_match_exact } from "./file_js_all_match_exact.mjs";
 import {file_prepend} from "./file_prepend.mjs";
 import {msft_vs_code_open} from "./msft_vs_code_open.mjs";
 import {string_is} from "./string_is.mjs";
 export async function file_js_import_add(function_name, import_name) {
   await arguments_assert(string_is, string_is)(arguments);
-  let file_path = await file_js_name_to_path(function_name);
+  let {file_path} = await file_js_all_match_exact(function_name);
   await file_prepend(file_path, `import {${import_name}} from './${import_name}.mjs';
 `);
   await msft_vs_code_open(file_path);
--- src/file_js_name_to_path.mjs
+++ src/file_js_name_to_path.mjs
@@ -1,10 +1,10 @@
+import {error} from './error.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
 import {string_is} from "./string_is.mjs";
 import path from "path";
 import {mykro_config_src_path_get} from "./mykro_config_src_path_get.mjs";
 export async function file_js_name_to_path(function_name) {
   await arguments_assert(string_is)(arguments);
-  console.log({function_name})
   error()
   return path.join(await mykro_config_src_path_get(), `${function_name}.mjs`);
 }
