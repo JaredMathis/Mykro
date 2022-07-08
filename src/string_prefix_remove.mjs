69e6f73a2c02b05e1a6ba0c954ba860348284845
JaredMathis <jared.mathis@native.consulting>
2022-07-07 20:13:44 -0400

    main file_js_new fjew string_starts_withstring_prefix_remove asdf a

--- src/string_prefix_remove.mjs
+++ src/string_prefix_remove.mjs
@@ -1,8 +1,14 @@
+import {string_size} from './string_size.mjs';
+import {list_starting_at} from './list_starting_at.mjs';
 import {string_starts_with} from './string_starts_with.mjs';
 import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
 import { assert } from "./assert.mjs";
+import { string_to_list } from './string_to_list.mjs';
 export async function string_prefix_remove(input, prefix) {
   await arguments_assert(string_is, string_is)(arguments);
   await assert(string_starts_with)(input, prefix);
+  let input_as_list = await string_to_list(input)
+  let result = await list_starting_at(input_as_list, await string_size(prefix))
+  return result;
 }
