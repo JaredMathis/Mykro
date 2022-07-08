32cb1a9cb42d2698f4a480057903ad63164988fb
JaredMathis <jared.mathis@native.consulting>
2022-07-08 14:13:18 -0400

    main noop progress

--- src/string/prefix/replace.mjs
+++ src/string/prefix/replace.mjs
@@ -1,5 +1,8 @@
 import {string_is} from "./../is.mjs";
 import {js_arguments_assert} from "./../../js/arguments/assert.mjs";
-export async function string_prefix_replace(value, prefix_old, prefix_new) {
+import { string_prefix_remove } from "./remove.mjs";
+export async function string_prefix_replace(input, prefix_old, prefix_new) {
   await js_arguments_assert(string_is, string_is, string_is)(arguments);
+  let result = await string_prefix_remove(input, prefix_old)
+  return prefix_new + result;
 }
