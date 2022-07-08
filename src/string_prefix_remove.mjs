f7140967bcae94d67e3cb0df12720b27bec05da4
JaredMathis <jared.mathis@native.consulting>
2022-07-07 20:12:12 -0400

    main noop progress

--- src/string_prefix_remove.mjs
+++ src/string_prefix_remove.mjs
@@ -1,5 +1,8 @@
+import {string_starts_with} from './string_starts_with.mjs';
 import {string_is} from "./string_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
+import { assert } from "./assert.mjs";
 export async function string_prefix_remove(input, prefix) {
   await arguments_assert(string_is, string_is)(arguments);
+  await assert(string_starts_with)(input, prefix);
 }
