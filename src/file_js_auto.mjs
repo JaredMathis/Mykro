3fdca563576c0c1ddc8e655f5b3f72bff08e0b4a
JaredMathis <jared.mathis@native.consulting>
2022-07-08 12:04:48 -0400

    main file_js_imports_missing_add fjimpmisa file_js_auto

--- src/file_js_auto.mjs
+++ src/file_js_auto.mjs
@@ -1,3 +1,4 @@
+import {string_identifier_is} from './string_identifier_is.mjs';
 import {arguments_assert} from "./arguments_assert.mjs";
 export async function file_js_auto(file_name) {
   await arguments_assert(string_identifier_is)(arguments);
