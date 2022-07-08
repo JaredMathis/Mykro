38f2fe7bcba470a7d762c28d00eb1a77e29c993f
JaredMathis <jared.mathis@native.consulting>
2022-07-07 20:14:08 -0400

    main string_prefix_remove asdf a

--- src/string_starts_withstring_prefix_remove.mjs
+++
@@ -1,7 +0,0 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function string_starts_withstring_prefix_remove() {
-    await arguments_assert()(arguments);
-    // TODO
-}
