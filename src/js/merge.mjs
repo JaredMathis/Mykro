83df4ed5e7da65ae37ab348205fcb583408170de
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:16:03 -0400

    main file_js_add js_property_set

--- src/js/merge.mjs
+++ src/js/merge.mjs
@@ -1,5 +1,11 @@
 import {js_defined_is} from "./defined/is.mjs";
 import {js_arguments_assert} from "./arguments/assert.mjs";
+import { js_properties } from "./properties.mjs";
+import { js_for_each } from "./for/each.mjs";
 export async function js_merge(into, from) {
   await js_arguments_assert(js_defined_is, js_defined_is)(arguments);
+  let properties = await js_properties(from)
+  await js_for_each(properties, async property=> {
+    
+  })
 }
---
+++ src/js/property/set.mjs
@@ -0,0 +1,4 @@
+import {js_arguments_assert} from "./../arguments/assert.mjs";
+export async function js_property_set() {
+  await js_arguments_assert()(arguments);
+}
