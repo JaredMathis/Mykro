0be2da1d5040cd13319f262f5c51b14c41286605
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:25:54 -0400

    main file_js_auto file_js_all_function_names

--- src/file/js/all/function/names.mjs
+++ src/file/js/all/function/names.mjs
@@ -1,4 +1,6 @@
+import {file_js_all} from "./../../all.mjs";
 import {js_arguments_assert} from "./../../../../js/arguments/assert.mjs";
 export async function file_js_all_function_names() {
   await js_arguments_assert()(arguments);
+  let files = await file_js_all();
 }
