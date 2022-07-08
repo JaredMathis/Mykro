b7f31674b02cbcd06258fa6d49b668e91c1e3470
JaredMathis <jared.mathis@native.consulting>
2022-07-07 20:47:06 -0400

    main file_js_imports_fix atest_rest

--- src/atest/rest.mjs
+++ src/atest/rest.mjs
@@ -1,7 +1,4 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
+import {arguments_assert} from "./arguments_assert.mjs";
 export async function atest_rest() {
-    await arguments_assert()(arguments);
-    // TODO
+  await arguments_assert()(arguments);
 }
