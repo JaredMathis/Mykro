2c1cc0cef89238ef764f58a4f6e42a84ea1661fe
JaredMathis <jared.mathis@native.consulting>
2022-07-08 12:12:52 -0400

    main file_js_folderize file_js_auto

---
+++ src/file/js/auto.mjs
@@ -0,0 +1,5 @@
+import {string_identifier_is} from "./../../string/identifier/is.mjs";
+import {arguments_assert} from "./../../arguments/assert.mjs";
+export async function file_js_auto(file_name) {
+  await arguments_assert(string_identifier_is)(arguments);
+}
--- src/file_js_auto.mjs
+++
@@ -1,5 +0,0 @@
-import {string_identifier_is} from "./string/identifier/is.mjs";
-import {arguments_assert} from "./arguments/assert.mjs";
-export async function file_js_auto(file_name) {
-  await arguments_assert(string_identifier_is)(arguments);
-}
