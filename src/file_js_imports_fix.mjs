f40b1c7c67f6054da2711888e7cdf89004036cd9
JaredMathis <jared.mathis@native.consulting>
2022-07-07 23:12:19 -0400

    main file_js_imports_fix a_test

--- src/file_js_imports_fix.mjs
+++ src/file_js_imports_fix.mjs
@@ -1,3 +1,4 @@
+import {a} from './a.mjs';
 import {es_node_import_declaration_single_name_get} from './es_node_import_declaration_single_name_get.mjs';
 import {es_traverse} from './es_traverse.mjs';
 import {string_identifier_is} from './string_identifier_is.mjs';
@@ -9,20 +10,19 @@ import { property_get } from './property_get.mjs';
 import { list_size } from './list_size.mjs';
 import { list_where } from './list_where.mjs';
 import { string_to_list } from './string_to_list.mjs';
+import path from 'path'
 export async function file_js_imports_fix(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
   let files = await file_js_all();
   let match = await file_js_all_match_exact(function_name)
   let match_file_path = await property_get(match, 'file_path');
-  let match_file_path_as_list = await string_to_list(match_file_path)
-  let count = await list_size(await list_where(match_file_path_as_list, f => f === '\\'))
-  console.log(count)
   await file_js_transform(function_name, async ast => {
     await es_traverse(ast, async node => {
       if (node.type === 'ImportDeclaration') {
         let name = await es_node_import_declaration_single_name_get(node);
         let match2 = await file_js_all_match_exact(name)
-        console.log(match2)
+        console.log({a: path.relative(match_file_path, match2.file_path)});
+        console.log({b: path.relative(match2.file_path, match_file_path)});
       }
     })
   });
