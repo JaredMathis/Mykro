a9a43eecbdde11d5fbbc9a6dc0b8b196cc6f1ba1
JaredMathis <jared.mathis@native.consulting>
2022-07-08 12:25:32 -0400

    main file_js_imports_fix file_js_auto

--- src/file/js/auto.mjs
+++ src/file/js/auto.mjs
@@ -1,7 +1,7 @@
-import {file_js_imports_missing_add} from './file_js_imports_missing_add.mjs';
+import {file_js_imports_missing_add} from "./imports/missing/add.mjs";
 import {string_identifier_is} from "./../../string/identifier/is.mjs";
 import {arguments_assert} from "./../../arguments/assert.mjs";
-import { file_js_imports_fix } from "./imports/fix.mjs";
+import {file_js_imports_fix} from "./imports/fix.mjs";
 export async function file_js_auto(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
   await file_js_imports_missing_add(function_name);
