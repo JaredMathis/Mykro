605f6e4984fa28b9c479c74573d7f46726998dc6
JaredMathis <jared.mathis@native.consulting>
2022-07-07 20:28:13 -0400

    main file_js_all file_js_all

--- src/file_js_parse.mjs
+++ src/file_js_parse.mjs
@@ -1,11 +1,12 @@
 import {arguments_assert} from "./arguments_assert.mjs";
 import {string_is} from "./string_is.mjs";
 import {file_read} from "./file_read.mjs";
-import {file_js_name_to_path} from "./file_js_name_to_path.mjs";
+import {file_js_all_match_exact} from "./file_js_all_match_exact.mjs";
 import {es_parse} from "./es_parse.mjs";
 export async function file_js_parse(function_name) {
   await arguments_assert(string_is)(arguments);
-  let file_path = await file_js_name_to_path(function_name);
+  let match = await file_js_all_match_exact(function_name);
+  let {file_path} = match;
   let unparsed = await file_read(file_path);
   let ast = await es_parse(unparsed);
   return {
