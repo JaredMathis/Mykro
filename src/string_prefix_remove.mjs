aa652378e3ab716c80bbc76d1b235b32bba8740f
JaredMathis <jared.mathis@native.consulting>
2022-07-07 20:11:21 -0400

    main file_js_argument_add fjsargad string_prefix_remove input string_is

--- src/string_prefix_remove.mjs
+++ src/string_prefix_remove.mjs
@@ -1,7 +1,5 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function string_prefix_remove() {
-    await arguments_assert()(arguments);
-    // TODO
+import {string_is} from './string_is.mjs';
+import {arguments_assert} from "./arguments_assert.mjs";
+export async function string_prefix_remove(input) {
+  await arguments_assert(string_is)(arguments);
 }
