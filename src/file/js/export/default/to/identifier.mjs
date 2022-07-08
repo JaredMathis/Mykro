a3c4d39f36a0cb5f27c27a84e98f432ff17c0e86
JaredMathis <jared.mathis@native.consulting>
2022-07-08 18:38:12 -0400

    main file_js_argument_add file_js_argadd file_js_export_default_to_identifier file_name string_identifier_is

--- src/file/js/export/default/to/identifier.mjs
+++ src/file/js/export/default/to/identifier.mjs
@@ -1,4 +1,4 @@
 import {js_arguments_assert} from "./../../../../../js/arguments/assert.mjs";
-export async function file_js_export_default_to_identifier() {
-  await js_arguments_assert()(arguments);
+export async function file_js_export_default_to_identifier(file_name) {
+  await js_arguments_assert(string_identifier_is)(arguments);
 }
