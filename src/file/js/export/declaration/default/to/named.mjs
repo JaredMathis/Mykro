e5eaed390e9c3f896d873dba8c239883fed2f8fa
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:19:47 -0400

    main noop progress

--- src/file/js/export/declaration/default/to/named.mjs
+++ src/file/js/export/declaration/default/to/named.mjs
@@ -1,16 +1,17 @@
 import {file_js_transform_ast} from "./../../../../transform/ast.mjs";
 import {js_string_identifier_is} from "./../../../../../../js/string/identifier/is.mjs";
 import {js_arguments_assert} from "./../../../../../../js/arguments/assert.mjs";
+import { js_merge } from "../../../../../../js/merge.mjs";
 export async function file_js_export_declaration_default_to_named(file_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
   await file_js_transform_ast(file_name, async node => {
     if (node.type === "ExportDefaultDeclaration") {
-      return;
-      node.type = "ExportNamedDeclaration";
       let merged = {
+        type: "ExportNamedDeclaration",
         specifiers: [],
-        source: null
+        source: null,
       }
+      await js_merge(node, merged);
     }
   });
 }
