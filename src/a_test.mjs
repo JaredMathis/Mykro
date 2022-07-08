ed0ad5ed6ce8c478c849409a991f8575822ddbce
JaredMathis <jared.mathis@native.consulting>
2022-07-07 23:41:17 -0400

    main noop progress

--- src/a_test.mjs
+++
@@ -1,7 +0,0 @@
-
-import { arguments_assert } from "./arguments_assert.mjs";
-
-export async function a_test() {
-    await arguments_assert()(arguments);
-    // TODO
-}
--- src/file_js_folderize.mjs
+++ src/file_js_folderize.mjs
@@ -1,8 +1,8 @@
 import {file_js_all} from "./file_js_all.mjs";
 import {string_identifier_is} from "./string_identifier_is.mjs";
 import {arguments_assert} from "./arguments_assert.mjs";
+import { file_js_move } from "./file_js_move.mjs";
 export async function file_js_folderize(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
-  let files = await file_js_all();
-  return files;
+  await file_js_move(function_name, function_name)
 }
