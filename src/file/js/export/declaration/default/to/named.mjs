257438b79036555f1e71e78e48502ef5daf1c81b
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:23:33 -0400

    main noop progress

--- src/file/js/export/declaration/default/to/named.mjs
+++ src/file/js/export/declaration/default/to/named.mjs
@@ -2,6 +2,7 @@ import {file_js_transform_ast} from "./../../../../transform/ast.mjs";
 import {js_string_identifier_is} from "./../../../../../../js/string/identifier/is.mjs";
 import {js_arguments_assert} from "./../../../../../../js/arguments/assert.mjs";
 import {js_merge} from "./../../../../../../js/merge.mjs";
+import { file_js_all } from "../../../../all.mjs";
 export async function file_js_export_declaration_default_to_named(file_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
   await file_js_transform_ast(file_name, async node => {
@@ -14,4 +15,5 @@ export async function file_js_export_declaration_default_to_named(file_name) {
       await js_merge(node, merged);
     }
   });
+  await file_js_all()
 }
