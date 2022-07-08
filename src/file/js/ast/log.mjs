3877701c9ff8eaec41559920d2e7607e09939096
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:35:37 -0400

    main noop progress

--- src/file/js/ast/log.mjs
+++ src/file/js/ast/log.mjs
@@ -1,4 +1,5 @@
 import {js_arguments_assert} from "./../../../js/arguments/assert.mjs";
 export async function file_js_ast_log() {
   await js_arguments_assert()(arguments);
+  await file_js_transform_ast();
 }
