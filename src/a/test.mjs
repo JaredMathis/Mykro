4cdf13ec4c4ffd3dfc1a8c8cbcd61f84ffe19d68
JaredMathis <jared.mathis@native.consulting>
2022-07-07 23:28:33 -0400

    main file_js_imports_fix a_test

--- src/a/test.mjs
+++ src/a/test.mjs
@@ -1,7 +1,7 @@
-import {a_b_c} from "./a_b_c.mjs";
-import {a_b} from "./a_b.mjs";
-import {a} from "../a.mjs";
-import {arguments_assert} from "./arguments_assert.mjs";
+import {a_b_c} from ".\\b\\c.mjs";
+import {a_b} from ".\\b.mjs";
+import {a} from ".\\..\\a.mjs";
+import {arguments_assert} from ".\\..\\arguments_assert.mjs";
 export async function atest_rest() {
   await arguments_assert()(arguments);
   a();
