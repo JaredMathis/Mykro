8e2411a5b554051e870533e518d747bd71cfabef
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:17:19 -0400

    main file_js_auto js_property_set

--- src/js/property/set.mjs
+++ src/js/property/set.mjs
@@ -1,3 +1,5 @@
+import {js_string_is} from "./../string/is.mjs";
+import {js_defined_is} from "./../defined/is.mjs";
 import {js_arguments_assert} from "./../arguments/assert.mjs";
 export async function js_property_set(object, property_name, property_value) {
   await js_arguments_assert(js_defined_is, js_string_is, js_defined_is)(arguments);
