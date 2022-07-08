ec3d5393e886eec9a599c793ed668412aa3df259
JaredMathis <jared.mathis@native.consulting>
2022-07-08 12:23:23 -0400

    main file_js_imports_fix file_js_path_get

--- src/file/js/path/get.mjs
+++ src/file/js/path/get.mjs
@@ -1,8 +1,8 @@
-import {file_js_extension} from './file_js_extension.mjs';
-import {file_path_join} from './file_path_join.mjs';
-import {string_replace_all} from './string_replace_all.mjs';
-import {mykro_config_src_path_get} from './mykro_config_src_path_get.mjs';
-import { string_identifier_is } from "../../../string/identifier/is.mjs";
+import {file_js_extension} from "./../extension.mjs";
+import {file_path_join} from "./../../path/join.mjs";
+import {string_replace_all} from "./../../../string/replace/all.mjs";
+import {mykro_config_src_path_get} from "./../../../mykro/config/src/path/get.mjs";
+import {string_identifier_is} from "./../../../string/identifier/is.mjs";
 import {arguments_assert} from "./../../../arguments/assert.mjs";
 export async function file_js_path_get(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
