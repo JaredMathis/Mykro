11ec98250d1603b03f215bf418d3d28ec10d0af9
JaredMathis <jared.mathis@native.consulting>
2022-07-08 18:50:26 -0400

    main file_js_auto file_js_export_default_to_identifier

--- src/file/js/export/default/to/identifier.mjs
+++ src/file/js/export/default/to/identifier.mjs
@@ -1,6 +1,7 @@
+import {file_js_transform_ast} from "./../../../transform/ast.mjs";
 import {js_string_identifier_is} from "./../../../../../js/string/identifier/is.mjs";
 import {js_arguments_assert} from "./../../../../../js/arguments/assert.mjs";
 export async function file_js_export_default_to_identifier(file_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
-  
+  await file_js_transform_ast(file_name, async ast => {});
 }
