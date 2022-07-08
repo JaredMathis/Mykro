bdfcfbd09d86a96ded00c1991eaf876b67d9bca2
JaredMathis <jared.mathis@native.consulting>
2022-07-08 12:20:22 -0400

    main noop rename

--- src/file/js/path/get.mjs
+++ src/file/js/path/get.mjs
@@ -3,8 +3,8 @@ import {arguments_assert} from "./../../../arguments/assert.mjs";
 export async function file_js_path_get(function_name) {
   await arguments_assert(string_identifier_is)(arguments);
   let directory = await mykro_config_src_path_get();
-  let function_new_path = await string_replace_all(function_name, "_", "/");
-  function_new_path = await file_path_join([directory, function_new_path]);
-  function_new_path += await file_js_extension();
-  return function_new_path;
+  let function_path = await string_replace_all(function_name, "_", "/");
+  function_path = await file_path_join([directory, function_path]);
+  function_path += await file_js_extension();
+  return function_path;
 }
