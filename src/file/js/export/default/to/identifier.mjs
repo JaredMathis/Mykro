8661aae31004ce9a43da0fc056b5b266198122b7
JaredMathis <jared.mathis@native.consulting>
2022-07-08 18:43:07 -0400

    main noop test code

--- src/file/js/export/default/to/identifier.mjs
+++ src/file/js/export/default/to/identifier.mjs
@@ -2,4 +2,5 @@ import {js_string_identifier_is} from "./../../../../../js/string/identifier/is.
 import {js_arguments_assert} from "./../../../../../js/arguments/assert.mjs";
 export async function file_js_export_default_to_identifier(file_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
+  
 }
---
+++ src/test2.mjs
@@ -0,0 +1,5 @@
+import test from "./test.mjs";
+
+export default function test2() {
+    test();    
+}
