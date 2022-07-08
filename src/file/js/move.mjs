ce492118623d8e82607d4db8cd1a2b7ad9717151
JaredMathis <jared.mathis@native.consulting>
2022-07-08 12:36:55 -0400

    missing import

--- src/file/js/move.mjs
+++ src/file/js/move.mjs
@@ -6,6 +6,7 @@ import {file_move} from "./../move.mjs";
 import {arguments_assert} from "./../../arguments/assert.mjs";
 import {string_identifier_is} from "./../../string/identifier/is.mjs";
 import {string_is} from "./../../string/is.mjs";
+import { file_js_path_get } from "./path/get.mjs";
 export async function file_js_move(function_name, function_name_new) {
   await arguments_assert(string_identifier_is, string_is)(arguments);
   let match = await file_js_all_match_exact(function_name);
