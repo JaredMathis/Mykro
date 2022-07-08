970d8a3a01a83bebbcae03b55ef25de54598ed80
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:49:28 -0400

    main noop progress

--- src/file/js/export/declaration/default/to/named.mjs
+++ src/file/js/export/declaration/default/to/named.mjs
@@ -6,6 +6,8 @@ import {js_merge} from "./../../../../../../js/merge.mjs";
 import {js_for_each} from "./../../../../../../js/for/each.mjs";
 import { list_size } from "../../../../../../list/size.mjs";
 import { list_single } from "../../../../../../list/single.mjs";
+import { js_assert } from "../../../../../../js/assert.mjs";
+import { js_equals } from "../../../../../../js/equals.mjs";
 export async function file_js_export_declaration_default_to_named(function_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
   await file_js_transform_ast(function_name, async node => {
@@ -24,6 +26,9 @@ export async function file_js_export_declaration_default_to_named(function_name)
       if (node.type === 'ImportDeclaration') {
         if (await list_size(node.specifiers) === 1) {
           let specifier = await list_single(node.specifiers);
+          if (specifier.type === 'ImportDefaultSpecifier') {
+            await js_assert(js_equals)(specifier.local.type, 'Identifier');
+          }
         }
       }
     });
