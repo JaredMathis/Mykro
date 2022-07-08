f9455630f7ceaab00f61f3a5874a5322637f2f51
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:54:18 -0400

    main file_js_auto js_clone

--- src/js/clone.mjs
+++ src/js/clone.mjs
@@ -1,3 +1,4 @@
+import {js_defined_is} from "./defined/is.mjs";
 import {js_arguments_assert} from "./arguments/assert.mjs";
 export async function js_clone(object) {
   await js_arguments_assert(js_defined_is)(arguments);
