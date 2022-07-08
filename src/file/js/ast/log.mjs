66a9828b68b5f305db1c694d2430f77597610e9f
JaredMathis <jared.mathis@native.consulting>
2022-07-08 19:36:14 -0400

    main file_js_auto file_js_ast_log

--- src/file/js/ast/log.mjs
+++ src/file/js/ast/log.mjs
@@ -1,3 +1,5 @@
+import {file_js_transform_ast} from "./../transform/ast.mjs";
+import {js_string_identifier_is} from "./../../../js/string/identifier/is.mjs";
 import {js_arguments_assert} from "./../../../js/arguments/assert.mjs";
 export async function file_js_ast_log(function_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
