5c7996279dd4d7a01549fb85b2bfddd36edb4ba2
JaredMathis <jared.mathis@native.consulting>
2022-07-07 21:37:07 -0400

    main noop unused imports

--- src/file_js_name_to_path.mjs
+++ src/file_js_name_to_path.mjs
@@ -4,5 +4,6 @@ import path from "path";
 import {mykro_config_src_path_get} from "./mykro_config_src_path_get.mjs";
 export async function file_js_name_to_path(function_name) {
   await arguments_assert(string_is)(arguments);
+  console.log({function_name})
   return path.join(await mykro_config_src_path_get(), `${function_name}.mjs`);
 }
