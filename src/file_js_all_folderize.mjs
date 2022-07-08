4d9c41f66128d788b31fc363c368a2232c6b3ec3
JaredMathis <jared.mathis@native.consulting>
2022-07-07 23:42:57 -0400

    main file_js_new file_js_all_folderize

--- src/file_js_all_folderize.mjs
+++ src/file_js_all_folderize.mjs
@@ -1,7 +1,7 @@
 
 import { arguments_assert } from "./arguments_assert.mjs";
 
-export async function a_test() {
+export async function file_js_all_folderize() {
     await arguments_assert()(arguments);
     // TODO
 }
