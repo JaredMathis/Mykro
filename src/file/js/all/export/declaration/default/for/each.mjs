2c7361e23d11a3b80766344130bbc015a43dc27a
JaredMathis <jared.mathis@native.consulting>
2022-07-09 16:44:26 -0400

    main file_js_add file_js_all_export_declaration_default_for_each

---
+++ src/file/js/all/export/declaration/default/for/each.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../../../../m/js/arguments/assert.mjs";
+export async function file_js_all_export_declaration_default_for_each() {
+  await m_js_arguments_assert()(arguments);
+}
