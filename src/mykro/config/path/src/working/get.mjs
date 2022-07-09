ad4cb28e110559dd694be4690d0faa2efcf306e1
JaredMathis <jared.mathis@native.consulting>
2022-07-09 16:33:34 -0400

    main file_js_auto mykro_config_path_src_working_get

--- src/mykro/config/path/src/working/get.mjs
+++ src/mykro/config/path/src/working/get.mjs
@@ -1,4 +1,10 @@
+import {file_path_normalize} from "./../../../../../file/path/normalize.mjs";
+import {m_js_property_path_initialize_get} from "./../../../../../m/js/property/path/initialize/get.mjs";
+import {mykro_config_get} from "./../../../get.mjs";
 import {m_js_arguments_assert} from "./../../../../../m/js/arguments/assert.mjs";
 export async function mykro_config_path_src_working_get() {
   await m_js_arguments_assert()(arguments);
+  let config = await mykro_config_get();
+  let src_path = await m_js_property_path_initialize_get(config, ["path", "src", "working"], `./src`);
+  return await file_path_normalize(src_path);
 }
