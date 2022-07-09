f2f7dcd7c5cf21ab021ea4889e3065a0eced86f4
JaredMathis <jared.mathis@native.consulting>
2022-07-08 20:19:52 -0400

    main file_js_auto mykro_config_src_paths_get

--- src/mykro/config/src/paths/get.mjs
+++ src/mykro/config/src/paths/get.mjs
@@ -1,4 +1,6 @@
+import {mykro_config_src_path_get} from "./../path/get.mjs";
 import {js_arguments_assert} from "./../../../../js/arguments/assert.mjs";
 export async function mykro_config_src_paths_get() {
   await js_arguments_assert()(arguments);
+  let mykro_directory = await mykro_config_src_path_get();
 }
