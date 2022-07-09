ac4ac3738a91111836b07946d7fb947a614f7790
JaredMathis <jared.mathis@native.consulting>
2022-07-08 23:40:42 -0400

    main noop copied code

--- src/file/js/all/generic.mjs
+++ src/file/js/all/generic.mjs
@@ -1,4 +1,27 @@
 import {js_arguments_assert} from "./../../../js/arguments/assert.mjs";
-export async function file_js_all_generic() {
-  await js_arguments_assert()(arguments);
+export async function file_js_all_generic(directories) {
+  await js_arguments_assert(list_is)(arguments);
+  let directories_mapped = await list_map(directories, async directory => {
+    let directory_files = await file_list(directory);
+    let directory_files_mapped = await list_map(directory_files, async file_path => {
+      return {
+        without_directory: await js_string_prefix_remove(file_path, directory + "\\"),
+        file_path
+      }
+    })
+    return directory_files_mapped;
+  })
+  let files = await list_join(directories_mapped);
+  let mapped = await list_map(files, async f => {
+    let dir_name = await file_path_dirname(f.without_directory);
+    let normalized = await file_path_normalize(dir_name + "\\" + path_file_name(f.without_directory));
+    let name = await js_string_replace_all(normalized, "\\", "_");
+    return {
+      name,
+      file_path: f.file_path,
+      extension: await path_file_extension(f.file_path)
+    };
+  });
+  let filtered = await list_where(mapped, async m => m.extension === await file_js_extension());
+  return filtered;
 }
