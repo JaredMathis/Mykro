4a262c108b91939039fa0ca10149fed55099510d
JaredMathis <jared.mathis@native.consulting>
2022-07-08 23:45:23 -0400

    main file_js_argument_add file_js_argadd file_js_imports_mykroify file_name js_string_identifier_is

--- src/file/js/imports/mykroify.mjs
+++ src/file/js/imports/mykroify.mjs
@@ -1,4 +1,4 @@
 import {js_arguments_assert} from "./../../../js/arguments/assert.mjs";
-export async function file_js_imports_mykroify() {
-  await js_arguments_assert()(arguments);
+export async function file_js_imports_mykroify(file_name) {
+  await js_arguments_assert(js_string_identifier_is)(arguments);
 }
