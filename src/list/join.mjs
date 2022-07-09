4c50deda09df991ccb08a2089bcad4aa88b8f22b
JaredMathis <jared.mathis@native.consulting>
2022-07-08 20:26:45 -0400

    main file_js_auto list_join

--- src/list/join.mjs
+++ src/list/join.mjs
@@ -1,4 +1,12 @@
+import {list_add_all} from "./add/all.mjs";
+import {list_is} from "./is.mjs";
+import {js_for_each} from "./../js/for/each.mjs";
 import {js_arguments_assert} from "./../js/arguments/assert.mjs";
-export async function list_join(list, lists) {
-  await js_arguments_assert(list_is, list_is)(arguments);
+export async function list_join(lists) {
+  await js_arguments_assert(list_is)(arguments);
+  let result = [];
+  await js_for_each(lists, async list => {
+    await list_add_all(result, list);
+  });
+  return result;
 }
