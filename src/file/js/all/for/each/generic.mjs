4835257326970bf7f2d9c802c3f4709eeac97b02
JaredMathis <jared.mathis@native.consulting>
2022-07-09 16:40:27 -0400

    main file_js_add file_js_all_for_each_generic

---
+++ src/file/js/all/for/each/generic.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../../m/js/arguments/assert.mjs";
+export async function file_js_all_for_each_generic() {
+  await m_js_arguments_assert()(arguments);
+}
