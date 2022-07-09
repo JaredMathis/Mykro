0a80e6825c8e356dbb64bca45955f1aa9fe82b7c
JaredMathis <jared.mathis@native.consulting>
2022-07-08 23:46:27 -0400

    main noop progress

--- src/file/js/imports/mykroify.mjs
+++ src/file/js/imports/mykroify.mjs
@@ -1,5 +1,9 @@
 import {js_string_identifier_is} from "./../../../js/string/identifier/is.mjs";
 import {js_arguments_assert} from "./../../../js/arguments/assert.mjs";
-export async function file_js_imports_mykroify(file_name) {
+import { file_js_transform_ast } from "../transform/ast.mjs";
+export async function file_js_imports_mykroify(function_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
+  await file_js_transform_ast(function_name, node => {
+
+  })
 }
