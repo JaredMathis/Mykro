9e3452e911312b349cefb519ed007039dcd45850
JaredMathis <jared.mathis@native.consulting>
2022-07-08 23:04:14 -0400

    main file_js_auto file_js_index_generate

--- src/file/js/index/generate.mjs
+++ src/file/js/index/generate.mjs
@@ -1,23 +1,20 @@
-import { js_for_each } from "../../../js/for/each.mjs";
-import { js_string_replace_all } from "../../../js/string/replace/all.mjs";
-import { mykro_config_src_path_get } from "../../../mykro/config/src/path/get.mjs";
-import { file_overwrite } from "../../overwrite.mjs";
-import { file_js_all } from "../all.mjs";
-import { file_js_extension } from "../extension.mjs";
+import {file_path_join} from "./../../path/join.mjs";
+import {js_for_each} from "./../../../js/for/each.mjs";
+import {js_string_replace_all} from "./../../../js/string/replace/all.mjs";
+import {mykro_config_src_path_get} from "./../../../mykro/config/src/path/get.mjs";
+import {file_overwrite} from "./../../overwrite.mjs";
+import {file_js_all} from "./../all.mjs";
+import {file_js_extension} from "./../extension.mjs";
 import {js_arguments_assert} from "./../../../js/arguments/assert.mjs";
 export async function file_js_index_generate() {
   await js_arguments_assert()(arguments);
   let files = await file_js_all();
-
   let result = ``;
-
   await js_for_each(files, async file => {
     result += `
-import {${file.name}} from "./${await js_string_replace_all(file.file_path, '\\', '/')}";`
-  })
-
-  let output_file_path = await file_path_join(await mykro_config_src_path_get(), 'index' + await file_js_extension());
-  await file_overwrite(output_file_path  )
-
+import {${file.name}} from "./${await js_string_replace_all(file.file_path, "\\", "/")}";`;
+  });
+  let output_file_path = await file_path_join(await mykro_config_src_path_get(), "index" + await file_js_extension());
+  await file_overwrite(output_file_path);
   return result;
 }
