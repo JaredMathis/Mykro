dff016b31f8778b72b8d1c027a3d46875099ccdd
JaredMathis <jared.mathis@native.consulting>
2022-07-09 12:43:27 -0400

    main file_js_auto file_path_relative

--- src/file/path/relative.mjs
+++ src/file/path/relative.mjs
@@ -1,3 +1,4 @@
+import {js_string_is} from "./../../js/string/is.mjs";
 import {js_arguments_assert} from "./../../js/arguments/assert.mjs";
 export async function file_path_relative(file_path_a, file_path_b) {
   await js_arguments_assert(js_string_is, string_is)(arguments);
