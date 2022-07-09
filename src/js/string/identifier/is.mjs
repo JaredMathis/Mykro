68d432f81e3d2ca865b0c6f43298d047533e09ad
JaredMathis <jared.mathis@native.consulting>
2022-07-09 13:18:01 -0400

    main noop missing await

--- src/js/string/identifier/is.mjs
+++ src/js/string/identifier/is.mjs
@@ -7,7 +7,7 @@ import {list_index_of} from "./../../../list/index/of.mjs";
 import {js_string_to_list} from "./../to/list.mjs";
 export async function js_string_identifier_is(value) {
   await js_arguments_assert(js_defined_is)(arguments);
-  let identifer_characters = await constant_alphabet() + `_` + constant_numbers();
+  let identifer_characters = await constant_alphabet() + `_` + await constant_numbers();
   let identifer_characters_as_list = await js_string_to_list(identifer_characters);
   let value_as_list = await js_string_to_list(value);
   let result = true;
@@ -15,7 +15,6 @@ export async function js_string_identifier_is(value) {
     let index = await list_index_of(identifer_characters_as_list, v);
     if (!index.success) {
       result = false;
-      console.log({v, identifer_characters_as_list})
       return true;
     }
   });
