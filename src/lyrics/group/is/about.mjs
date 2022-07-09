49a41e599e826c8392a7407b1cfaed2d961e62a7
JaredMathis <jared.mathis@native.consulting>
2022-07-09 17:59:49 -0400

    main noop remove lyrics

--- src/lyrics/group/is/about.mjs
+++
@@ -1,9 +0,0 @@
-import _ from "lodash";
-import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
-import {m_js_string_is} from "./../../../m/js/string/is.mjs";
-export async function lyrics_group_is_about(object_clause) {
-  await m_js_arguments_assert(m_js_string_is)(arguments);
-  return g => {
-    return _.isUndefined(g.parent) && g.nouns.includes(object_clause);
-  };
-}
