ba068088ca17e9581f10eb90e11db5ebaffc291c
JaredMathis <jared.mathis@native.consulting>
2022-07-08 23:46:47 -0400

    main file_js_imports_mykroify file_js_imports_mykroify

--- src/file/js/imports/mykroify.mjs
+++ src/file/js/imports/mykroify.mjs
@@ -1,9 +1,7 @@
 import {js_string_identifier_is} from "./../../../js/string/identifier/is.mjs";
 import {js_arguments_assert} from "./../../../js/arguments/assert.mjs";
-import { file_js_transform_ast } from "../transform/ast.mjs";
+import {file_js_transform_ast} from "../transform/ast.mjs";
 export async function file_js_imports_mykroify(function_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
-  await file_js_transform_ast(function_name, node => {
-
-  })
+  await file_js_transform_ast(function_name, node => {});
 }
