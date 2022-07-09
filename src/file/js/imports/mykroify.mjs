65978bb191195ac408148b07c6b1c2f4adcf69c7
JaredMathis <jared.mathis@native.consulting>
2022-07-09 12:58:15 -0400

    main noop move code

--- src/file/js/imports/mykroify.mjs
+++ src/file/js/imports/mykroify.mjs
@@ -13,6 +13,8 @@ import { js_equals } from "../../../js/equals.mjs";
 import { file_path_relative } from "../../path/relative.mjs";
 export async function file_js_imports_mykroify(function_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
+  let match = await file_js_all_match_exact(function_name);
+  let match_file_path = await property_get(match, "file_path");
   let files_mykro = await file_js_all_mykro();
   await file_js_transform_ast(function_name, async node => {
     if (node.type === "ImportDeclaration") {
@@ -24,8 +26,9 @@ export async function file_js_imports_mykroify(function_name) {
             await js_assert(js_number_at_most)(await list_size(imported_matches), 1);
             if (equals(await list_size(imported_matches), 1)) {
               await js_assert(js_equals)(node.source.type, 'Literal')
-              let match = await list_single(match);
-              node.source.value = await file_path_relative()
+              let imported_match = await list_single(imported_match);
+              console.log({imported_match, match_file_path})        
+      
             }
           }
         }
--- src/file/js/imports/resolve.mjs
+++ src/file/js/imports/resolve.mjs
@@ -14,7 +14,6 @@ import {file_js_transform_ast} from "./../transform/ast.mjs";
 export async function file_js_imports_resolve(function_name) {
   await js_arguments_assert(js_string_identifier_is)(arguments);
   let match = await file_js_all_match_exact(function_name);
-  let match_file_path = await property_get(match, "file_path");
   await file_js_transform_ast(function_name, async node => {
     if (node.type === "ImportDeclaration") {
       if (!await js_string_starts_with(node.source.value, ".")) {
@@ -23,6 +22,7 @@ export async function file_js_imports_resolve(function_name) {
       let name;
       name = await es_node_import_declaration_single_name_get(node);
       let match_import = await file_js_all_match_exact(name);
+      let match_file_path = await property_get(match, "file_path");
       let import_path = await file_path_relative(await file_path_dirname(match_file_path), match_import.file_path);
       import_path = ".\\" + import_path;
       import_path = await js_string_replace_all(import_path, "\\", "/");
