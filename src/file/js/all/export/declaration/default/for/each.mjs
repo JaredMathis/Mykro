787bb7cf7a9589e8df87d9ada575b9e2ff3b273f
JaredMathis <jared.mathis@native.consulting>
2022-07-09 16:49:33 -0400

    main file_js_all_export_declaration_default_for_each noop

--- src/file/js/all/export/declaration/default/for/each.mjs
+++ src/file/js/all/export/declaration/default/for/each.mjs
@@ -5,11 +5,12 @@ import {file_js_all_for_each_generic} from "./../../../../for/each/generic.mjs";
 import {file_js_all_function_names} from "./../../../../function/names.mjs";
 import {m_js_arguments_assert} from "./../../../../../../../m/js/arguments/assert.mjs";
 import {list_where} from "./../../../../../../../list/where.mjs";
+import { m_js_string_starts_with } from "../../../../../../../m/js/string/starts/with.mjs";
 export async function file_js_all_export_declaration_default_for_each() {
-  await m_js_arguments_assert()(arguments);
   let path_working = await mykro_config_path_src_working_get();
   let files = await file_js_all();
-  let filtered = await await list_where(files, file => file.file_path);
-  let function_names = await list_map(files, file => file.name);
+  let filtered = await await list_where(files, async file => await m_js_string_starts_with(file.file_path, path_working));
+  let function_names = await list_map(filtered, file => file.name);
+  console.log({function_names})
   await file_js_all_for_each_generic(function_names, arguments);
 }
