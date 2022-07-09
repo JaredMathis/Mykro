f5e045fe05807bb616bba78175ec8a5450ded44d
JaredMathis <jared.mathis@native.consulting>
2022-07-09 16:41:17 -0400

    main noop refactor

--- src/file/js/all/for/each.mjs
+++ src/file/js/all/for/each.mjs
@@ -8,9 +8,13 @@ import {list_starting_at} from "./../../../../list/starting/at.mjs";
 import {list_add_all} from "./../../../../list/add/all.mjs";
 export async function file_js_all_for_each() {
   let function_names = await file_js_all_function_names();
-  let function_name = arguments[0];
-  let remaining = await list_starting_at(Array.from(arguments), 1);
-  await m_js_for_each(function_names, async function_name_other => {
+  await file_js_all_for_each_generic(function_names, arguments);
+}
+async function file_js_all_for_each_generic(function_names, _arguments) {
+  let function_name = _arguments[0];
+  let remaining = await list_starting_at(Array.from(_arguments), 1);
+  await m_js_for_each(function_names, async (function_name_other) => {
     await file_js_run(function_name, await list_add_all([function_name_other], remaining), m_js_error, noop, m_js_log_error, m_js_error, noop);
   });
 }
+
