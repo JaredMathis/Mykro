a770460b0755506ca0c48a2097b558488883a4db
JaredMathis <jared.mathis@native.consulting>
2022-07-08 20:21:08 -0400

    main file_js_auto mykro_config_src_paths_get

--- src/mykro/config/src/paths/get.mjs
+++ src/mykro/config/src/paths/get.mjs
@@ -1,3 +1,4 @@
+import {list_add} from "./../../../../list/add.mjs";
 import {mykro_config_src_other_get} from "./../other/get.mjs";
 import {mykro_config_src_path_get} from "./../path/get.mjs";
 import {js_arguments_assert} from "./../../../../js/arguments/assert.mjs";
@@ -6,5 +7,5 @@ export async function mykro_config_src_paths_get() {
   let mykro_directory = await mykro_config_src_path_get();
   let other_direcories = await mykro_config_src_other_get();
   let result = [];
-  await list_add(result,mykro_directory )
+  await list_add(result, mykro_directory);
 }
