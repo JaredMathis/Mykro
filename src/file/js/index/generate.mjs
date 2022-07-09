62185f1c093e2214cb42899711f9a7a254f63f92
JaredMathis <jared.mathis@native.consulting>
2022-07-08 23:06:22 -0400

    main file_js_index_generate file_js_index_generate

--- src/file/js/index/generate.mjs
+++ src/file/js/index/generate.mjs
@@ -14,6 +14,12 @@ export async function file_js_index_generate() {
     result += `
 import {${file.name}} from "./${await js_string_replace_all(file.file_path, "\\", "/")}";`;
   });
+
+  await js_for_each(files, async file => {
+    result += `
+export {${file.name}};`;
+  });
+
   let output_file_path = await file_path_join([await mykro_config_src_path_get(), "index" + await file_js_extension()]);
   await file_overwrite(output_file_path, result);
   return result;
