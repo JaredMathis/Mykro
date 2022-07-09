ac139e7d5838b99b1fa70e362c32cfddf9e43660
JaredMathis <jared.mathis@native.consulting>
2022-07-09 16:42:08 -0400

    main file_js_auto file_js_all_for_each_generic

--- src/file/js/all/for/each/generic.mjs
+++ src/file/js/all/for/each/generic.mjs
@@ -1,4 +1,6 @@
+import {list_is} from "./../../../../../list/is.mjs";
+import {m_js_defined_is} from "./../../../../../m/js/defined/is.mjs";
 import {m_js_arguments_assert} from "./../../../../../m/js/arguments/assert.mjs";
-export async function file_js_all_for_each_generic() {
-  await m_js_arguments_assert()(arguments);
+export async function file_js_all_for_each_generic(function_names, _arguments) {
+  await m_js_arguments_assert(list_is, m_js_defined_is)(arguments);
 }
