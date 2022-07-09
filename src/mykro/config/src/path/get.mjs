b5b984977dcb1d9d6e98d5517997abbafca33015
JaredMathis <jared.mathis@native.consulting>
2022-07-09 16:32:27 -0400

    main noop refactor

--- src/mykro/config/src/path/get.mjs
+++ src/mykro/config/src/path/get.mjs
@@ -2,10 +2,10 @@ import {m_js_property_path_initialize_get} from "./../../../../m/js/property/pat
 import {constant_mykro} from "./../../../../constant/mykro.mjs";
 import {mykro_config_get} from "./../../get.mjs";
 import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
-import path from "path";
+import { file_path_normalize } from "../../../../file/path/normalize.mjs";
 export async function mykro_config_src_path_get() {
   await m_js_arguments_assert()(arguments);
   let config = await mykro_config_get();
   let src_path = await m_js_property_path_initialize_get(config, ["path", "src", await constant_mykro()], `./node_modules/${await constant_mykro()}/src`);
-  return path.normalize(src_path);
+  return await file_path_normalize(src_path);
 }
