5f5ec1f153631ed50163abaed9464065df70889c
JaredMathis <jared.mathis@native.consulting>
2022-07-08 23:43:34 -0400

    main noop implementation

--- src/file/js/all/mykro.mjs
+++ src/file/js/all/mykro.mjs
@@ -1,4 +1,8 @@
+import { mykro_config_src_path_get } from "../../../mykro/config/src/path/get.mjs";
 import {js_arguments_assert} from "./../../../js/arguments/assert.mjs";
+import { file_js_all_generic } from "./generic.mjs";
 export async function file_js_all_mykro() {
   await js_arguments_assert()(arguments);
+  let mykro_directory = await mykro_config_src_path_get();
+  return await file_js_all_generic([mykro_directory]);
 }
