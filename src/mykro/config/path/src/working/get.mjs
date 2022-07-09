38acfe97054c6b3dc7b8fbc0e6903483eaceb74f
JaredMathis <jared.mathis@native.consulting>
2022-07-09 16:31:38 -0400

    main file_js_add mykro_config_path_src_working_get

---
+++ src/mykro/config/path/src/working/get.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../../m/js/arguments/assert.mjs";
+export async function mykro_config_path_src_working_get() {
+  await m_js_arguments_assert()(arguments);
+}
