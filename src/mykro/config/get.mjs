17f24f6be9d63c0f00baad6f253936c7a2ae2930
JaredMathis <jared.mathis@native.consulting>
2022-07-08 20:16:27 -0400

    main file_js_auto mykro_config_src_path_get

--- src/mykro/config/get.mjs
+++ src/mykro/config/get.mjs
@@ -2,21 +2,12 @@ import {mykro_config_path} from "./path.mjs";
 import {file_json_read} from "./../../file/json/read.mjs";
 import {file_overwrite} from "./../../file/overwrite.mjs";
 import {file_exists} from "./../../file/exists.mjs";
-import {constant_mykro} from "./../../constant/mykro.mjs";
 import {js_arguments_assert} from "./../../js/arguments/assert.mjs";
 export async function mykro_config_get() {
   await js_arguments_assert()(arguments);
   const config_path = await mykro_config_path();
   if (!await file_exists(config_path)) {
-    await file_overwrite(config_path, `
-  {
-      "path": {
-          "src": {
-              "${await constant_mykro()}": "./node_modules/${await constant_mykro()}/src"
-          }
-      }
-  }
-        `);
+    await file_overwrite(config_path, `{}`);
   }
   let config = await file_json_read(config_path);
   return config;
--- src/mykro/config/src/path/get.mjs
+++ src/mykro/config/src/path/get.mjs
@@ -1,3 +1,4 @@
+import {js_property_path_initialize_get} from "./../../../../js/property/path/initialize/get.mjs";
 import {constant_mykro} from "./../../../../constant/mykro.mjs";
 import {mykro_config_get} from "./../../get.mjs";
 import {js_arguments_assert} from "./../../../../js/arguments/assert.mjs";
@@ -6,5 +7,8 @@ export async function mykro_config_src_path_get() {
   await js_arguments_assert()(arguments);
   let config = await mykro_config_get();
   let src_path = config.path.src[await constant_mykro()];
+  if (false) {
+    let src_path = await js_property_path_initialize_get(config, ["path", "src", await constant_mykro()], `./node_modules/${await constant_mykro()}/src`);
+  }
   return path.normalize(src_path);
 }
