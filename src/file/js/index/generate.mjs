a52b68a92c1490883488280aefd872c993cbcc2a
JaredMathis <jared.mathis@native.consulting>
2022-07-08 21:50:01 -0400

    main file_js_index_generate file_js_index_generate

--- src/file/js/index/generate.mjs
+++ src/file/js/index/generate.mjs
@@ -1,4 +1,7 @@
+import { file_js_all } from "../all.mjs";
 import {js_arguments_assert} from "./../../../js/arguments/assert.mjs";
 export async function file_js_index_generate() {
   await js_arguments_assert()(arguments);
+  let files = await file_js_all();
+  return files;
 }
