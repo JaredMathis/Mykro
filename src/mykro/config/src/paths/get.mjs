7f75d22339763a05a1023156bec1b9ab9e1335ca
JaredMathis <jared.mathis@native.consulting>
2022-07-08 20:20:15 -0400

    main noop progress

--- src/mykro/config/src/paths/get.mjs
+++ src/mykro/config/src/paths/get.mjs
@@ -3,4 +3,5 @@ import {js_arguments_assert} from "./../../../../js/arguments/assert.mjs";
 export async function mykro_config_src_paths_get() {
   await js_arguments_assert()(arguments);
   let mykro_directory = await mykro_config_src_path_get();
+  let other_direcories = await mykro_config_src_other_get();
 }
