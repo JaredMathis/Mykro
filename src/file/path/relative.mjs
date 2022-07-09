8cb03a97225cff1c98d1a5937ebdfe1183b36796
JaredMathis <jared.mathis@native.consulting>
2022-07-09 12:42:37 -0400

    main file_js_argument_add file_js_argadd file_path_relative file_path_a string_is

--- src/file/path/relative.mjs
+++ src/file/path/relative.mjs
@@ -1,4 +1,4 @@
 import {js_arguments_assert} from "./../../js/arguments/assert.mjs";
-export async function file_path_relative() {
-  await js_arguments_assert()(arguments);
+export async function file_path_relative(file_path_a) {
+  await js_arguments_assert(string_is)(arguments);
 }
