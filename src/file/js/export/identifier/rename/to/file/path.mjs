3fcfe071d02ddfaec702b090a1c96df3284a68bd
JaredMathis <jared.mathis@native.consulting>
2022-07-09 20:31:39 -0400

    main noop progress

--- src/file/js/export/identifier/rename/to/file/path.mjs
+++ src/file/js/export/identifier/rename/to/file/path.mjs
@@ -3,11 +3,20 @@ import {file_js_transform} from "./../../../../../transform.mjs";
 import {m_js_string_identifier_is} from "./../../../../../../../m/js/string/identifier/is.mjs";
 import {file_js_transform_ast} from "./../../../../../transform/ast.mjs";
 import {m_js_arguments_assert} from "./../../../../../../../m/js/arguments/assert.mjs";
+import { list_add } from "../../../../../../../list/add.mjs";
+import { m_js_equals } from "../../../../../../../m/js/equals.mjs";
+import { list_size } from "../../../../../../../list/size.mjs";
 export async function file_js_export_identifier_rename_to_file_path(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
   await file_js_transform(function_name, async ast => {
+    let result = [];
     await es_traverse(ast, async node => {
-      if (node.type === "ExportNamedDeclaration") {}
+      if (node.type === "ExportNamedDeclaration") {
+        await list_add(result, node);
+      }
     });
+    if (m_js_equals(await list_size(result), 1)) {
+      
+    }
   });
 }
