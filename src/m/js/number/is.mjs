ef9e665690df44fcf799b21df2766573aa4b82ad
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:51:04 -0400

    main noop lodash refactor away

--- src/m/js/number/is.mjs
+++ src/m/js/number/is.mjs
@@ -1,7 +1,6 @@
-import _ from "lodash";
 import {m_js_arguments_assert} from "./../arguments/assert.mjs";
 import {m_js_defined_is} from "./../defined/is.mjs";
 export async function m_js_number_is(value) {
   await m_js_arguments_assert(m_js_defined_is)(arguments);
-  return _.isNumber(value);
+  return typeof value === 'number';
 }
