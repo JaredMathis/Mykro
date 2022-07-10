22fc410268947329848ac9240e381baf77efcbd5
JaredMathis <jared.mathis@native.consulting>
2022-07-10 17:56:12 -0400

    main file_js_auto m_ui_element

--- src/file/js/export/identifier/to/file/name.mjs
+++ src/file/js/export/identifier/to/file/name.mjs
@@ -2,5 +2,4 @@ import {m_js_string_identifier_is} from "./../../../../../../m/js/string/identif
 import {m_js_arguments_assert} from "./../../../../../../m/js/arguments/assert.mjs";
 export async function file_js_export_identifier_to_file_name(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
-  
 }
--- src/m/element.mjs
+++
@@ -1,4 +0,0 @@
-import {m_js_arguments_assert} from "./js/arguments/assert.mjs";
-export async function m_element() {
-  await m_js_arguments_assert()(arguments);
-}
