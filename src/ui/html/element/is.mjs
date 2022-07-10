f6fdfcdade2709cdf61f91a51cb28ba6eb6da9ef
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:57:37 -0400

    main noop typo

--- src/ui/html/element/is.mjs
+++ src/ui/html/element/is.mjs
@@ -2,5 +2,5 @@ import {m_js_defined_is} from "./../../../m/js/defined/is.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
 export async function ui_html_element_is(input) {
   await m_js_arguments_assert(m_js_defined_is)(arguments);
-  return obj instanceof HTMLElement;
+  return input instanceof HTMLElement;
 }
