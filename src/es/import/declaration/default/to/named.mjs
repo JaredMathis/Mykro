2a37feea426a3986eb1eba452a5845694ad7cb75
JaredMathis <jared.mathis@native.consulting>
2022-07-10 12:27:32 -0400

    main noop fixed argument

--- src/es/import/declaration/default/to/named.mjs
+++ src/es/import/declaration/default/to/named.mjs
@@ -6,9 +6,9 @@ import {list_single} from "./../../../../../list/single.mjs";
 import {list_size} from "./../../../../../list/size.mjs";
 import {m_js_arguments_assert} from "./../../../../../m/js/arguments/assert.mjs";
 import { m_js_defined_is } from "../../../../../m/js/defined/is.mjs";
-import { m_js_string_identifier_is } from "../../../../../m/js/string/identifier/is.mjs";
+import { m_js_function_is } from "../../../../../m/js/function/is.mjs";
 export async function es_import_declaration_default_to_named(node, predicate) {
-  await m_js_arguments_assert(m_js_defined_is, m_js_string_identifier_is)(arguments);
+  await m_js_arguments_assert(m_js_defined_is, m_js_function_is)(arguments);
   if (node.type === "ImportDeclaration") {
     if (await list_size(node.specifiers) === 1) {
       let specifier = await list_single(node.specifiers);
