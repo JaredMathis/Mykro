ad56df5eb35408263bf004c982d252e78ec69378
JaredMathis <jared.mathis@native.consulting>
2022-07-09 20:52:38 -0400

    main file_js_argument_add file_js_argadd file_js_await_missing_add function_name m_js_string_identifier_is

--- src/file/js/await/missing/add.mjs
+++ src/file/js/await/missing/add.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
-export async function file_js_await_missing_add() {
-  await m_js_arguments_assert()(arguments);
+export async function file_js_await_missing_add(function_name) {
+  await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
 }
