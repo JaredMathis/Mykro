36a88459d59d77d87a136f51c4a79127c7d2f67c
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:13:51 -0400

    main file_js_argument_add file_js_arg_add ui_html_index_generate main_function_name m_js_string_identifier_is

--- src/ui/html/index/generate.mjs
+++ src/ui/html/index/generate.mjs
@@ -1,4 +1,16 @@
+import {file_js_all_match_exact} from "../../../file/js/all/match/exact.mjs";
+import {file_overwrite} from "../../../file/overwrite.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
-export async function ui_html_index_generate() {
-  await m_js_arguments_assert()(arguments);
+export async function ui_html_index_generate(main_function_name) {
+  await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
+  let match = file_js_all_match_exact(main_function_name);
+  let result = `
+<!DOCTYPE html>
+<html>
+    <body>
+        <script src="${match.file_path}"></script>
+    </body>
+</html>
+`;
+  await file_overwrite("index.html", result);
 }
