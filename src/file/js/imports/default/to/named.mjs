9672b1baf01bc06b5c3b33990b547c41578164d6
JaredMathis <jared.mathis@native.consulting>
2022-07-10 12:05:02 -0400

    main file_js_auto file_js_imports_default_to_named

--- src/file/js/imports/default/to/named.mjs
+++ src/file/js/imports/default/to/named.mjs
@@ -1,3 +1,4 @@
+import {m_js_string_identifier_is} from "./../../../../../m/js/string/identifier/is.mjs";
 import {m_js_arguments_assert} from "./../../../../../m/js/arguments/assert.mjs";
 export async function file_js_imports_default_to_named(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
