4122b1a0722adf81f5d8255d762e57eba62d5a60
JaredMathis <jared.mathis@native.consulting>
2022-07-10 13:02:00 -0400

    main noop refactor

--- src/file/js/export/identifier/rename/to/file/path.mjs
+++ src/file/js/export/identifier/rename/to/file/path.mjs
@@ -9,24 +9,5 @@ import {list_size} from "./../../../../../../../list/size.mjs";
 import {list_single} from "./../../../../../../../list/single.mjs";
 export async function file_js_export_identifier_rename_to_file_path(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
-  await file_js_transform(function_name, async ast => {
-    let result = [];
-    await es_traverse(ast, async node => {
-      if (node.type === "ExportNamedDeclaration") {
-        await list_add(result, node);
-      }
-    });
-    if (m_js_equals(await list_size(result), 1)) {
-      let export_declaration = await list_single(result);
-      let declaration = export_declaration.declaration;
-      if (declaration.type === "FunctionDeclaration") {
-        if (declaration.id.type === "Identifier") {
-          let function_name_existing = declaration.id.name;
-          if (!m_js_equals(function_name_existing, function_name)) {
-            await file_js_all_identifier_rename(function_name_existing, function_name);
-          }
-        }
-      }
-    }
-  });
+  await file_js_export_identifier_rename_to_file_path(function_name, false);
 }
--- src/file/js/export/identifier/rename/to/file/path/quick.mjs
+++ src/file/js/export/identifier/rename/to/file/path/quick.mjs
@@ -1,4 +1,5 @@
 import {m_js_arguments_assert} from "./../../../../../../../../m/js/arguments/assert.mjs";
-export async function file_js_export_identifier_rename_to_file_path_quick() {
-  await m_js_arguments_assert()(arguments);
+export async function file_js_export_identifier_rename_to_file_path_quick(function_name) {
+  await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
+  await file_js_export_identifier_rename_to_file_path(function_name, true);
 }
