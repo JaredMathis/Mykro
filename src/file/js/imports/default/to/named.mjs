c7b83557e5a4b9ca0a66c2975082642f12d6b111
JaredMathis <jared.mathis@native.consulting>
2022-07-10 12:20:16 -0400

    main noop missing async

--- src/file/js/imports/default/to/named.mjs
+++ src/file/js/imports/default/to/named.mjs
@@ -5,7 +5,7 @@ import { es_import_declaration_default_to_named } from "../../../../../es/import
 import { m_js_tautology } from "../../../../../m/js/tautology.mjs";
 export async function file_js_imports_default_to_named(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
-  await file_js_transform_ast(function_name, node => {
+  await file_js_transform_ast(function_name, async node => {
     await es_import_declaration_default_to_named(node, m_js_tautology);
   });
 }
