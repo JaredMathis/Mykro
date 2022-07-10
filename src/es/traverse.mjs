7a4b8e48c1e2b98d5bcbc0c547f6acb6cd9d1240
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:30:16 -0400

    main noop refactor lodash away

--- src/es/traverse.mjs
+++ src/es/traverse.mjs
@@ -1,4 +1,3 @@
-import _ from "lodash";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {m_js_defined_is} from "./../m/js/defined/is.mjs";
 import {m_js_function_is} from "./../m/js/function/is.mjs";
@@ -8,11 +7,12 @@ import {list_map} from "./../list/map.mjs";
 import {list_where} from "./../list/where.mjs";
 import {m_js_property_get} from "./../m/js/property/get.mjs";
 import {tree_traverse} from "./../tree/traverse.mjs";
+import { list_is } from "../list/is.mjs";
 export async function es_traverse(ast, ast_node_for_each) {
   await m_js_arguments_assert(m_js_defined_is, m_js_function_is)(arguments);
   await tree_traverse(ast, async node => {
     let values = await js_values(node);
-    return await list_where(values, async v => v !== null && (await m_js_property_has(v, "type") || _.isArray(v)));
+    return await list_where(values, async v => v !== null && (await m_js_property_has(v, "type") || await list_is(v)));
   }, ast_node_for_each);
 }
 async function js_values(node) {
