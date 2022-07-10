44f908182337fa518aedfcc7f3aa9de4d26405f4
JaredMathis <jared.mathis@native.consulting>
2022-07-10 19:02:11 -0400

    main file_js_argument_add argadd ui_element_html_inner_set element ui_html_element_is

--- src/ui/element/html/inner/set.mjs
+++ src/ui/element/html/inner/set.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
-export async function ui_element_html_inner_set() {
-  await m_js_arguments_assert()(arguments);
+export async function ui_element_html_inner_set(element) {
+  await m_js_arguments_assert(ui_html_element_is)(arguments);
 }
