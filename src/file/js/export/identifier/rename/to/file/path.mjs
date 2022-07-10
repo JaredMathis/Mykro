d334e5d6c8d3a6aa0c310fed90a3ec3159a32d5d
JaredMathis <jared.mathis@native.consulting>
2022-07-10 13:05:17 -0400

    main noop unused imports

--- src/file/js/export/identifier/rename/to/file/path.mjs
+++ src/file/js/export/identifier/rename/to/file/path.mjs
@@ -1,13 +1,6 @@
 import {file_js_export_identifier_rename_to_file_path_generic} from "./path/generic.mjs";
-import {file_js_all_identifier_rename} from "./../../../../../all/identifier/rename.mjs";
-import {es_traverse} from "./../../../../../../../es/traverse.mjs";
-import {file_js_transform} from "./../../../../../transform.mjs";
 import {m_js_string_identifier_is} from "./../../../../../../../m/js/string/identifier/is.mjs";
 import {m_js_arguments_assert} from "./../../../../../../../m/js/arguments/assert.mjs";
-import {list_add} from "./../../../../../../../list/add.mjs";
-import {m_js_equals} from "./../../../../../../../m/js/equals.mjs";
-import {list_size} from "./../../../../../../../list/size.mjs";
-import {list_single} from "./../../../../../../../list/single.mjs";
 export async function file_js_export_identifier_rename_to_file_path(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
   await file_js_export_identifier_rename_to_file_path_generic(function_name, false);
--- src/file/js/export/identifier/rename/to/file/path/quick.mjs
+++ src/file/js/export/identifier/rename/to/file/path/quick.mjs
@@ -1,5 +1,4 @@
 import {file_js_export_identifier_rename_to_file_path_generic} from "./generic.mjs";
-import {file_js_export_identifier_rename_to_file_path} from "./../path.mjs";
 import {m_js_string_identifier_is} from "./../../../../../../../../m/js/string/identifier/is.mjs";
 import {m_js_arguments_assert} from "./../../../../../../../../m/js/arguments/assert.mjs";
 export async function file_js_export_identifier_rename_to_file_path_quick(function_name) {
