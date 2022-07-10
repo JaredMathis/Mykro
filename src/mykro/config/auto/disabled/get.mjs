293a9e8432e4fcaa457ac5387f5f60105b14913c
JaredMathis <jared.mathis@native.consulting>
2022-07-10 17:47:25 -0400

    main noop default value update

--- src/mykro/config/auto/disabled/get.mjs
+++ src/mykro/config/auto/disabled/get.mjs
@@ -4,5 +4,5 @@ import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
 export async function mykro_config_auto_disabled_get() {
   await m_js_arguments_assert()(arguments);
   let config = await mykro_config_get();
-  return await m_js_property_path_initialize_get(config, ["auto", "disabled"], false);
+  return await m_js_property_path_initialize_get(config, ["auto", "disabled"], true);
 }
