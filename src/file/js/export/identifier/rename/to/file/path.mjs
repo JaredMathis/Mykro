b64a242b5b53090e9450ccfbda81d37d6232ee60
JaredMathis <jared.mathis@native.consulting>
2022-07-09 20:25:00 -0400

    main noop progress

--- src/file/js/export/identifier/rename/to/file/path.mjs
+++ src/file/js/export/identifier/rename/to/file/path.mjs
@@ -1,4 +1,9 @@
 import {m_js_arguments_assert} from "./../../../../../../../m/js/arguments/assert.mjs";
 export async function file_js_export_identifier_rename_to_file_path() {
   await m_js_arguments_assert()(arguments);
+  await file_js_transform_ast(function_name, async node => {
+    if (node.type === "ExportNamedDeclaration") {
+
+    }
+  });
 }
