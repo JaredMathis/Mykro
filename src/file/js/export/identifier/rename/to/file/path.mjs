9d1ddc99d114d7bd7a5bf18d3a6bfd230d6c838c
JaredMathis <jared.mathis@native.consulting>
2022-07-09 20:45:10 -0400

    main file_js_export_identifier_rename_to_file_path test_a

--- src/file/js/export/identifier/rename/to/file/path.mjs
+++ src/file/js/export/identifier/rename/to/file/path.mjs
@@ -21,7 +21,11 @@ export async function file_js_export_identifier_rename_to_file_path(function_nam
       let declaration = export_declaration.declaration;
       if (declaration.type === "FunctionDeclaration") {
         if (declaration.id.type === "Identifier") {
-          await file_js_all_identifier_rename(function_name, function_name);
+          let function_name_existing = declaration.id.name;
+          if (!m_js_equals(function_name_existing, function_name)) {
+            console.log('here');
+            // await file_js_all_identifier_rename(function_name, function_name);
+          }
         }
       }
     }
