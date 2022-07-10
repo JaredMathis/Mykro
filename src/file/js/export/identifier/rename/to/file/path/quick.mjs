2ba86c0b6955db681ee49d86da836f3e6b2eebdd
JaredMathis <jared.mathis@native.consulting>
2022-07-10 12:59:31 -0400

    main file_js_add file_js_export_identifier_rename_to_file_path_quick

---
+++ src/file/js/export/identifier/rename/to/file/path/quick.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../../../../../m/js/arguments/assert.mjs";
+export async function file_js_export_identifier_rename_to_file_path_quick() {
+  await m_js_arguments_assert()(arguments);
+}
