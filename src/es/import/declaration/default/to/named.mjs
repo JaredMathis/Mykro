1efae92c595610183176450cd26cbe6dd7d2c0ac
JaredMathis <jared.mathis@native.consulting>
2022-07-10 12:14:09 -0400

    main noop typo fix

--- src/es/import/declaration/default/to/named.mjs
+++ src/es/import/declaration/default/to/named.mjs
@@ -7,7 +7,7 @@ import {list_size} from "./../../../../../list/size.mjs";
 import {m_js_arguments_assert} from "./../../../../../m/js/arguments/assert.mjs";
 import { m_js_defined_is } from "../../../../../m/js/defined/is.mjs";
 import { m_js_string_identifier_is } from "../../../../../m/js/string/identifier/is.mjs";
-export async function es_import_declaration_default_to_named(node, function_name) {
+export async function es_import_declaration_default_to_named(node, predicate) {
   await m_js_arguments_assert(m_js_defined_is, m_js_string_identifier_is)(arguments);
   if (node.type === "ImportDeclaration") {
     if (await list_size(node.specifiers) === 1) {
--- src/file/js/imports/default/to/named.mjs
+++ src/file/js/imports/default/to/named.mjs
@@ -3,5 +3,7 @@ import {m_js_arguments_assert} from "./../../../../../m/js/arguments/assert.mjs"
 import {file_js_transform_ast} from "./../../../transform/ast.mjs";
 export async function file_js_imports_default_to_named(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
-  await file_js_transform_ast(function_name, node => {});
+  await file_js_transform_ast(function_name, node => {
+    
+  });
 }
