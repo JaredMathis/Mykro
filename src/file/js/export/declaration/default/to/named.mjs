8d1704576f41c99df1b42ab684caf7e58ffe276f
JaredMathis <jared.mathis@native.consulting>
2022-07-10 12:57:07 -0400

    main noop unused imports

--- src/file/js/export/declaration/default/to/named.mjs
+++ src/file/js/export/declaration/default/to/named.mjs
@@ -1,14 +1,9 @@
-import {m_js_clone} from "./../../../../../../m/js/clone.mjs";
 import {file_js_all_function_names} from "./../../../../all/function/names.mjs";
 import {file_js_transform_ast} from "./../../../../transform/ast.mjs";
 import {m_js_string_identifier_is} from "./../../../../../../m/js/string/identifier/is.mjs";
 import {m_js_arguments_assert} from "./../../../../../../m/js/arguments/assert.mjs";
 import {m_js_merge} from "./../../../../../../m/js/merge.mjs";
 import {m_js_for_each} from "./../../../../../../m/js/for/each.mjs";
-import {list_size} from "./../../../../../../list/size.mjs";
-import {list_single} from "./../../../../../../list/single.mjs";
-import {m_js_assert} from "./../../../../../../m/js/assert.mjs";
-import {m_js_equals} from "./../../../../../../m/js/equals.mjs";
 export async function file_js_export_declaration_default_to_named(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
   await file_js_transform_ast(function_name, async node => {
