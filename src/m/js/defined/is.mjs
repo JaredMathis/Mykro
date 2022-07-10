405ded3f3a52c98c7110615a535f58e139e14ac0
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:55:10 -0400

    main noop refactor external depedency away

--- src/m/js/defined/is.mjs
+++ src/m/js/defined/is.mjs
@@ -1,7 +1,6 @@
-import _ from "lodash";
 import {m_js_arguments_assert} from "./../arguments/assert.mjs";
 import {m_js_tautology} from "./../tautology.mjs";
 export async function m_js_defined_is(value) {
   await m_js_arguments_assert(m_js_tautology)(arguments);
-  return !_.isUndefined(value);
+  return value !== undefined
 }
