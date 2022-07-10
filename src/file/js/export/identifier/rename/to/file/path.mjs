31a7c2c668c95067daa4c41234db868ec4ebe529
JaredMathis <jared.mathis@native.consulting>
2022-07-10 13:04:52 -0400

    main file_js_auto file_js_export_identifier_rename_to_file_path

--- src/file/js/export/identifier/rename/to/file/path.mjs
+++ src/file/js/export/identifier/rename/to/file/path.mjs
@@ -1,3 +1,4 @@
+import {file_js_export_identifier_rename_to_file_path_generic} from "./path/generic.mjs";
 import {file_js_all_identifier_rename} from "./../../../../../all/identifier/rename.mjs";
 import {es_traverse} from "./../../../../../../../es/traverse.mjs";
 import {file_js_transform} from "./../../../../../transform.mjs";
@@ -9,5 +10,5 @@ import {list_size} from "./../../../../../../../list/size.mjs";
 import {list_single} from "./../../../../../../../list/single.mjs";
 export async function file_js_export_identifier_rename_to_file_path(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
-  await file_js_export_identifier_rename_to_file_path(function_name, false);
+  await file_js_export_identifier_rename_to_file_path_generic(function_name, false);
 }
--- src/file/js/export/identifier/rename/to/file/path/quick.mjs
+++ src/file/js/export/identifier/rename/to/file/path/quick.mjs
@@ -3,5 +3,5 @@ import {m_js_string_identifier_is} from "./../../../../../../../../m/js/string/i
 import {m_js_arguments_assert} from "./../../../../../../../../m/js/arguments/assert.mjs";
 export async function file_js_export_identifier_rename_to_file_path_quick(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
-  await file_js_export_identifier_rename_to_file_path(function_name, true);
+  await file_js_export_identifier_rename_to_file_path_generic(function_name, true);
 }
