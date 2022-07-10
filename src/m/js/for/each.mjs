b82c8dcafc5ce45c48d07f45710811dd9dae46f5
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:50:00 -0400

    main noop lodash refactor away

--- src/m/js/for/each.mjs
+++ src/m/js/for/each.mjs
@@ -1,9 +1,10 @@
-import _ from "lodash";
+import { list_is } from "../../../list/is.mjs";
+import { m_js_function_is } from "../function/is.mjs";
 import {m_js_arguments_assert} from "./../arguments/assert.mjs";
 import {m_js_defined_is} from "./../defined/is.mjs";
 import {m_js_true_is} from "./../true/is.mjs";
 export async function m_js_for_each(list, for_each_item) {
-  await m_js_arguments_assert(_.isArray, _.isFunction)(arguments);
+  await m_js_arguments_assert(list_is, m_js_function_is)(arguments);
   let index = 0;
   for (let item of list) {
     let result = await for_each_item(item, index);
