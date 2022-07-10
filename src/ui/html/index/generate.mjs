52b606d6482fc869d2e59d6284ca0ed1a82df9e5
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:14:25 -0400

    main noop missing import

--- src/ui/html/index/generate.mjs
+++ src/ui/html/index/generate.mjs
@@ -1,5 +1,6 @@
 import {file_js_all_match_exact} from "../../../file/js/all/match/exact.mjs";
 import {file_overwrite} from "../../../file/overwrite.mjs";
+import { m_js_string_identifier_is } from "../../../m/js/string/identifier/is.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
 export async function ui_html_index_generate(main_function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
