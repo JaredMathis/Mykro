6ec6ac4087d338644fab6cf363515ec32ce2ef10
JaredMathis <jared.mathis@native.consulting>
2022-07-10 19:03:22 -0400

    main file_js_auto ui_element_html_inner_set

--- src/ui/element/html/inner/set.mjs
+++ src/ui/element/html/inner/set.mjs
@@ -1,3 +1,4 @@
+import {m_js_string_is} from "./../../../../m/js/string/is.mjs";
 import {ui_html_element_is} from "./../../../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
 export async function ui_element_html_inner_set(element, text) {
