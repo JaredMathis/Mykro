8d7eb2d9221cda43ca27f1256f87355a6bbdeedd
JaredMathis <jared.mathis@native.consulting>
2022-07-09 20:26:48 -0400

    main file_js_auto file_js_export_identifier_rename_to_file_path

--- src/file/js/export/identifier/rename/to/file/path.mjs
+++ src/file/js/export/identifier/rename/to/file/path.mjs
@@ -1,7 +1,8 @@
+import {m_js_string_identifier_is} from "./../../../../../../../m/js/string/identifier/is.mjs";
 import {file_js_transform_ast} from "./../../../../../transform/ast.mjs";
 import {m_js_arguments_assert} from "./../../../../../../../m/js/arguments/assert.mjs";
 export async function file_js_export_identifier_rename_to_file_path(function_name) {
-  await m_js_arguments_assert(m_js_string_identififer_is)(arguments);
+  await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
   await file_js_transform_ast(function_name, async node => {
     if (node.type === "ExportNamedDeclaration") {
       console.log({
