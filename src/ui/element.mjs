86293927edc7379a9698da7c4c3e17998f11cf2c
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:59:29 -0400

    main noop typo

--- src/ui/element.mjs
+++ src/ui/element.mjs
@@ -3,7 +3,7 @@ import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {ui_html_element_is} from "./html/element/is.mjs";
 export async function ui_element(parent, tag_name) {
   await m_js_arguments_assert(ui_html_element_is, m_js_string_is)(arguments);
-  let result = parent.createElement(tag_name);
+  let result = document.createElement(tag_name);
   parent.appendChild(result);
   return result;
 }
