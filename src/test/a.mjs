cb8771192f890cbf59800f20f5d914f94978fffa
JaredMathis <jared.mathis@native.consulting>
2022-07-09 20:27:59 -0400

    main file_js_export_identifier_rename_to_file_path test_a

--- src/test/a.mjs
+++ src/test/a.mjs
@@ -1,7 +1,7 @@
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {test_a_b} from "./a/b.mjs";
 import {test_a_b_d} from "./a/b/d.mjs";
-export async function test_a() {
+export async function test_aa() {
   await m_js_arguments_assert()(arguments);
   test_a_b();
   test_a_b_d();
