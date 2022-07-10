98f3baa35885757d9c778b7885325af4cb11a96b
JaredMathis <jared.mathis@native.consulting>
2022-07-09 20:18:10 -0400

    main file_js_all_rename_if_starts_with probability_ random_probability_

---
+++ src/random/probability/is.mjs
@@ -0,0 +1,13 @@
+import _ from "lodash";
+import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
+import {m_js_defined_is} from "./../../m/js/defined/is.mjs";
+import {m_js_number_at_most} from "./../../m/js/number/at/most.mjs";
+import {m_js_number_negative_not_is} from "./../../m/js/number/negative/not/is.mjs";
+import {list_where} from "./../../list/where.mjs";
+import {m_js_false_is} from "./../../m/js/false/is.mjs";
+import {m_js_true_is} from "./../../m/js/true/is.mjs";
+export async function random_probability_is(p) {
+  await m_js_arguments_assert(m_js_defined_is)(arguments);
+  let requirements = [_.isNumber(p), await m_js_number_negative_not_is(p), await m_js_number_at_most(p, 1)];
+  return _.every(await list_where(requirements, m_js_true_is));
+}
--- src/random/with/probability.mjs
+++ src/random/with/probability.mjs
@@ -1,6 +1,6 @@
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
-import {probability_is} from "./../../probability/is.mjs";
+import {random_probability_is} from "./../probability/is.mjs";
 export async function random_with_probability(probability) {
-  await m_js_arguments_assert(probability_is)(arguments);
+  await m_js_arguments_assert(random_probability_is)(arguments);
   return Math.random() <= probability;
 }
