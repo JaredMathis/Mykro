65d46bdbf3605889ae9ca438190339f83bee57d6
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:03:45 -0400

    main file_js_argument_add file_jsargadd ui_html_element_is input m_js_defined_is

--- src/ui/html/element/is.mjs
+++ src/ui/html/element/is.mjs
@@ -1,4 +1,5 @@
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
-export async function ui_html_element_is() {
-  await m_js_arguments_assert()(arguments);
+export async function ui_html_element_is(input) {
+  await m_js_arguments_assert(m_js_defined_is)(arguments);
+  return obj instanceof HTMLElement;
 }
