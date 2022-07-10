41c5aa48c44697af123f64741b29bd89ba83f516
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:06:30 -0400

    main noop unused import

--- src/ui/element.mjs
+++ src/ui/element.mjs
@@ -1,7 +1,6 @@
 import {m_js_string_is} from "./../m/js/string/is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {ui_html_element_is} from "./html/element/is.mjs";
-import { result } from "lodash";
 export async function ui_element(parent, tag_name) {
   await m_js_arguments_assert(ui_html_element_is, m_js_string_is)(arguments);
   let result = parent.createElement(tag_name);
