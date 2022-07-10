c509455aaba47a8856e6bfb8b409b84e6f09500a
JaredMathis <jared.mathis@native.consulting>
2022-07-09 20:42:33 -0400

    main file_js_auto file_js_export_identifier_rename_to_file_path

--- src/file/js/export/identifier/rename/to/file/path.mjs
+++ src/file/js/export/identifier/rename/to/file/path.mjs
@@ -1,11 +1,12 @@
+import {file_js_all_identifier_rename} from "./../../../../../all/identifier/rename.mjs";
 import {es_traverse} from "./../../../../../../../es/traverse.mjs";
 import {file_js_transform} from "./../../../../../transform.mjs";
 import {m_js_string_identifier_is} from "./../../../../../../../m/js/string/identifier/is.mjs";
 import {m_js_arguments_assert} from "./../../../../../../../m/js/arguments/assert.mjs";
-import { list_add } from "../../../../../../../list/add.mjs";
-import { m_js_equals } from "../../../../../../../m/js/equals.mjs";
-import { list_size } from "../../../../../../../list/size.mjs";
-import { list_single } from "../../../../../../../list/single.mjs";
+import {list_add} from "./../../../../../../../list/add.mjs";
+import {m_js_equals} from "./../../../../../../../m/js/equals.mjs";
+import {list_size} from "./../../../../../../../list/size.mjs";
+import {list_single} from "./../../../../../../../list/single.mjs";
 export async function file_js_export_identifier_rename_to_file_path(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
   await file_js_transform(function_name, async ast => {
@@ -18,8 +19,8 @@ export async function file_js_export_identifier_rename_to_file_path(function_nam
     if (m_js_equals(await list_size(result), 1)) {
       let export_declaration = await list_single(result);
       let declaration = export_declaration.declaration;
-      if (declaration.type === 'FunctionDeclaration') {
-        if (declaration.id.type === 'Identifier') {
+      if (declaration.type === "FunctionDeclaration") {
+        if (declaration.id.type === "Identifier") {
           await file_js_all_identifier_rename(function_name, function_name);
         }
       }
