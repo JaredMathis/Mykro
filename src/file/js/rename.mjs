44214fc9cf45a6a623a3bb12f6b8db49034a2817
JaredMathis <jared.mathis@native.consulting>
2022-07-09 20:43:53 -0400

    main noop undid removal of code

--- src/file/js/rename.mjs
+++ src/file/js/rename.mjs
@@ -7,6 +7,7 @@ import {file_js_all_identifier_rename} from "./all/identifier/rename.mjs";
 import {file_js_move} from "./move.mjs";
 export async function file_js_rename(function_name, function_name_new) {
   await m_js_arguments_assert(m_js_string_identifier_is, m_js_string_identifier_is)(arguments);
+  await m_js_assert(m_js_not(m_js_equals))(function_name, function_name_new);
   await file_js_all_identifier_rename(function_name, function_name_new);
   await file_js_move(function_name, function_name_new);
 }
