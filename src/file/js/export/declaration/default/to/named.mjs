274125024b395d87d2a919b3f48dc64694228943
JaredMathis <jared.mathis@native.consulting>
2022-07-10 12:07:46 -0400

    main noop refactor

--- src/file/js/export/declaration/default/to/named.mjs
+++ src/file/js/export/declaration/default/to/named.mjs
@@ -24,22 +24,26 @@ export async function file_js_export_declaration_default_to_named(function_name)
   let function_names = await file_js_all_function_names();
   await m_js_for_each(function_names, async function_name_other => {
     await file_js_transform_ast(function_name_other, async node => {
-      if (node.type === "ImportDeclaration") {
-        if (await list_size(node.specifiers) === 1) {
-          let specifier = await list_single(node.specifiers);
-          if (specifier.type === "ImportDefaultSpecifier") {
-            const local = specifier.local;
-            await m_js_assert(m_js_equals)(local.type, "Identifier");
-            if (local.name === function_name) {
-              let imported = await m_js_clone(local);
-              await m_js_merge(specifier, {
-                type: "ImportSpecifier",
-                imported
-              });
-            }
-          }
-        }
-      }
+      await es_import_declaration_default_to_named(node, function_name);
     });
   });
 }
+async function es_import_declaration_default_to_named(node, function_name) {
+  if (node.type === "ImportDeclaration") {
+    if (await list_size(node.specifiers) === 1) {
+      let specifier = await list_single(node.specifiers);
+      if (specifier.type === "ImportDefaultSpecifier") {
+        const local = specifier.local;
+        await m_js_assert(m_js_equals)(local.type, "Identifier");
+        if (local.name === function_name) {
+          let imported = await m_js_clone(local);
+          await m_js_merge(specifier, {
+            type: "ImportSpecifier",
+            imported
+          });
+        }
+      }
+    }
+  }
+}
+
