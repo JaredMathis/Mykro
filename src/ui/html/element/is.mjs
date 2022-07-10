1e0f206b955451f0c938ee70b3a8b5395ebfdde1
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:04:02 -0400

    main file_js_auto ui_html_element_is

--- src/ui/html/element/is.mjs
+++ src/ui/html/element/is.mjs
@@ -1,3 +1,4 @@
+import {m_js_defined_is} from "./../../../m/js/defined/is.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
 export async function ui_html_element_is(input) {
   await m_js_arguments_assert(m_js_defined_is)(arguments);
