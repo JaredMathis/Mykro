dda33c3a5e87099f580ad73d031cce7d7625e43a
JaredMathis <jared.mathis@native.consulting>
2022-07-10 13:08:15 -0400

    main noop logic fix

--- src/file/js/export/identifier/rename/to/file/path/generic.mjs
+++ src/file/js/export/identifier/rename/to/file/path/generic.mjs
@@ -8,6 +8,7 @@ import {file_js_transform} from "./../../../../../../transform.mjs";
 import {m_js_string_identifier_is} from "./../../../../../../../../m/js/string/identifier/is.mjs";
 import {m_js_arguments_assert} from "./../../../../../../../../m/js/arguments/assert.mjs";
 import { m_js_boolean_is } from "../../../../../../../../m/js/boolean/is.mjs";
+import { file_js_identifier_rename } from "../../../../../../identifier/rename.mjs";
 export async function file_js_export_identifier_rename_to_file_path_generic(function_name, quick) {
   await m_js_arguments_assert(m_js_string_identifier_is, m_js_boolean_is)(arguments);
   await file_js_transform(function_name, async ast => {
@@ -24,7 +25,9 @@ export async function file_js_export_identifier_rename_to_file_path_generic(func
         if (declaration.id.type === "Identifier") {
           let function_name_existing = declaration.id.name;
           if (!m_js_equals(function_name_existing, function_name)) {
-            if (!quick) {
+            if (quick) {
+              await file_js_identifier_rename(function_name, function_name_existing, function_name)
+            } else {
               await file_js_all_identifier_rename(function_name_existing, function_name);
             }
           }
