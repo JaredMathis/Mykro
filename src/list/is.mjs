53c5991631dba7265b4427a6814926e2da4b0675
JaredMathis <jared.mathis@native.consulting>
2022-07-10 18:48:37 -0400

    main noop lodash refactor away

--- src/list/is.mjs
+++ src/list/is.mjs
@@ -1,7 +1,6 @@
-import _ from "lodash";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {m_js_defined_is} from "./../m/js/defined/is.mjs";
 export async function list_is(value) {
   await m_js_arguments_assert(m_js_defined_is)(arguments);
-  return _.isArray(value);
+  return Array.isArray(value);
 }
