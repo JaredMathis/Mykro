60f31303cdea4ef562bb50aa6bc279cbc74f7257
JaredMathis <jared.mathis@native.consulting>
2022-07-10 12:03:08 -0400

    main file_js_add file_js_imports_default_to_named

---
+++ src/file/js/imports/default/to/named.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../../m/js/arguments/assert.mjs";
+export async function file_js_imports_default_to_named() {
+  await m_js_arguments_assert()(arguments);
+}
