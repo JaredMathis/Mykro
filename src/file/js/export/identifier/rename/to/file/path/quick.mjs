16b3af2ff8138e710f67eb15bd1be92b4207f5ca
JaredMathis <jared.mathis@native.consulting>
2022-07-10 13:03:21 -0400

    main file_js_auto file_js_export_identifier_rename_to_file_path_quick

--- src/file/js/export/identifier/rename/to/file/path/quick.mjs
+++ src/file/js/export/identifier/rename/to/file/path/quick.mjs
@@ -1,3 +1,5 @@
+import {file_js_export_identifier_rename_to_file_path} from "./../path.mjs";
+import {m_js_string_identifier_is} from "./../../../../../../../../m/js/string/identifier/is.mjs";
 import {m_js_arguments_assert} from "./../../../../../../../../m/js/arguments/assert.mjs";
 export async function file_js_export_identifier_rename_to_file_path_quick(function_name) {
   await m_js_arguments_assert(m_js_string_identifier_is)(arguments);
