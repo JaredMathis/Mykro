da4bb787b11118c6ce39670c02e5f623c867c693
JaredMathis <jared.mathis@native.consulting>
2022-07-10 22:02:49 -0400

    main file_js_auto list_all

--- src/list/all.mjs
+++ src/list/all.mjs
@@ -1,4 +1,15 @@
+import {m_js_true_is} from "./../m/js/true/is.mjs";
+import {list_is} from "./is.mjs";
+import {m_js_for_each} from "./../m/js/for/each.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 export async function list_all(list) {
   await m_js_arguments_assert(list_is)(arguments);
+  let result = true;
+  await m_js_for_each(list, async item => {
+    if (!await m_js_true_is(item)) {
+      result = false;
+      return true;
+    }
+  });
+  return result;
 }
