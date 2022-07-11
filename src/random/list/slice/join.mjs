bcde55519bd5de6ed886111ea2570a4d0cc4a698
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:55:21 -0400

    main file_js_auto random_list_slice_join

--- src/random/list/slice/join.mjs
+++ src/random/list/slice/join.mjs
@@ -1,4 +1,11 @@
+import {m_js_string_join} from "./../../../m/js/string/join.mjs";
+import {random_list_slice} from "./../slice.mjs";
+import {list_is} from "./../../../list/is.mjs";
+import {m_js_number_is} from "./../../../m/js/number/is.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
-export async function random_list_slice_join() {
-  await m_js_arguments_assert()(arguments);
+export async function random_list_slice_join(list, count) {
+  await m_js_arguments_assert(list_is, m_js_number_is)(arguments);
+  let slice = await random_list_slice(list, count);
+  let joined = await m_js_string_join(slice, " ");
+  return joined;
 }
