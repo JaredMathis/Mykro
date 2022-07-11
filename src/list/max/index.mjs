aadb6e0fa44bc271d7afa72bc1ffa2ed4bdacf00
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:46:42 -0400

    main file_js_auto list_max_index

--- src/list/max/index.mjs
+++ src/list/max/index.mjs
@@ -1,4 +1,7 @@
+import {list_size} from "./../size.mjs";
+import {list_is} from "./../is.mjs";
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
 export async function list_max_index(list) {
   await m_js_arguments_assert(list_is)(arguments);
+  return await list_size(list) - 1;
 }
