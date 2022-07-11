47960bd96f5da249c6a94a5190717852af9593f2
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:18:26 -0400

    main file_js_auto ui_element_style_button_primary

--- src/ui/element/style/button/primary.mjs
+++ src/ui/element/style/button/primary.mjs
@@ -1,15 +1,13 @@
-import {ui_element_style_background_color} from "./../background/color.mjs";
 import {ui_color_blue} from "./../../../color/blue.mjs";
-import {ui_element_style_border} from "./../border.mjs";
 import {ui_html_element_is} from "./../../../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
+import {ui_element_style_background_color_border} from "./../background/color/border.mjs";
 export async function ui_element_style_button_primary(element) {
   await m_js_arguments_assert(ui_html_element_is)(arguments);
   element.style.cursor = "pointer";
   element.style.listStyleType = "none";
   const color = await ui_color_blue();
-  await ui_element_style_border(element, `rgba(${color}, 1)`);
-  await ui_element_style_background_color(element, color, 0.3);
-  element.style.width = '100%';
-  element.style.boxSizing = 'border-box';
+  await ui_element_style_background_color_border(element, color, 1, 0.3);
+  element.style.width = "100%";
+  element.style.boxSizing = "border-box";
 }
