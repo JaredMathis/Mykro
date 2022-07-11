a3897cfb65efed8e7360c2a802479dfb10b7904b
JaredMathis <jared.mathis@native.consulting>
2022-07-10 22:40:33 -0400

    main noop put view back

--- src/ui/view.mjs
+++ src/ui/view.mjs
@@ -1,16 +1,17 @@
 import {ui_element_remove} from "./element/remove.mjs";
+import {ui_html_element_is} from "./html/element/is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {m_js_defined_is} from "./../m/js/defined/is.mjs";
 import { m_js_property_get } from "../m/js/property/get.mjs";
-export async function ui_view() {
-  await m_js_arguments_assert()(arguments);
+export async function ui_view(parent) {
+  await m_js_arguments_assert(ui_html_element_is)(arguments);
   let current;
   return {
     set: async constructor => {
       if (m_js_defined_is(current)) {
         await ui_element_remove(current);
       }
-      let component = await constructor();
+      let component = await constructor(parent);
       current = await m_js_property_get(component, 'container');
     }
   };
