107fd382e5628b7d468ee22947efb28effd7cda5
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:42:43 -0400

    main file_js_argument_add argadd random_list_shuffle list list_is

--- src/random/list/shuffle.mjs
+++ src/random/list/shuffle.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
-export async function random_list_shuffle() {
-  await m_js_arguments_assert()(arguments);
+export async function random_list_shuffle(list) {
+  await m_js_arguments_assert(list_is)(arguments);
 }
