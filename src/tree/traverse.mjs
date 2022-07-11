2309b6b534aa1cf7fd2593c8d57dbeed806cd050
JaredMathis <jared.mathis@native.consulting>
2022-07-11 16:53:07 -0400

    main noop refactor

--- src/tree/traverse.mjs
+++ src/tree/traverse.mjs
@@ -1,15 +1,13 @@
-import _ from "lodash";
+import { list_is } from "../list/is.mjs";
+import { m_js_defined_is } from "../m/js/defined/is.mjs";
+import { m_js_function_is } from "../m/js/function/is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {m_js_assert} from "./../m/js/assert.mjs";
-import {m_js_not} from "./../m/js/not.mjs";
 export async function tree_traverse(node, children_get, for_each_child) {
-  await m_js_arguments_assert(m_js_not(_.isUndefined), _.isFunction, _.isFunction)(arguments);
+  await m_js_arguments_assert(m_js_defined_is, m_js_function_is, m_js_function_is)(arguments);
   await for_each_child(node);
   let children = await children_get(node);
-  if (false) {
-    m_js_undefined_is;
-  }
-  m_js_assert(_.isArray)(children);
+  m_js_assert(list_is)(children);
   for (let child of children) {
     await tree_traverse(child, children_get, for_each_child);
   }
