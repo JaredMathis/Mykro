a48d177b85eb60ba48b5aed9aff79e4efb710719
JaredMathis <jared.mathis@native.consulting>
2022-07-11 07:36:52 -0400

    main file_js_auto file_js_transform

--- src/file/js/transform.mjs
+++ src/file/js/transform.mjs
@@ -1,4 +1,3 @@
-import {file_overwrite} from "./../overwrite.mjs";
 import {es_unparse} from "./../../es/unparse.mjs";
 import {file_js_parse} from "./parse.mjs";
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
@@ -6,6 +5,7 @@ import {m_js_function_is} from "./../../m/js/function/is.mjs";
 import {m_js_string_identifier_is} from "./../../m/js/string/identifier/is.mjs";
 import {m_js_equals} from "./../../m/js/equals.mjs";
 import {m_js_comment} from "./../../m/js/comment.mjs";
+import {file_overwrite_if_changed} from "./../overwrite/if/changed.mjs";
 export async function file_js_transform(function_name, transformer) {
   await m_js_arguments_assert(m_js_string_identifier_is, m_js_function_is)(arguments);
   let {ast, unparsed, file_path} = await file_js_parse(function_name);
@@ -15,5 +15,5 @@ export async function file_js_transform(function_name, transformer) {
     await m_js_comment(`There were no changes to the AST, so we're returning rather than saving`);
     return;
   }
-  await file_overwrite(file_path, text);
+  await file_overwrite_if_changed(file_path, text);
 }
