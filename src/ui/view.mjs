a4327ceaf6890f04775c0f152115985ef224c3d5
JaredMathis <jared.mathis@native.consulting>
2022-07-10 22:32:28 -0400

    main file_js_add ui_element_remove

--- src/ui/view.mjs
+++ src/ui/view.mjs
@@ -1,10 +1,14 @@
 import {ui_html_element_is} from "./html/element/is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
+import { m_js_defined_is } from "../m/js/defined/is.mjs";
 export async function ui_view(parent) {
   await m_js_arguments_assert(ui_html_element_is)(arguments);
+  let current;
   return {
-    set: async () => {
-
+    set: async (constructor) => {
+      if (m_js_defined_is(current)) {
+        
+      }
     }
   }
 }
