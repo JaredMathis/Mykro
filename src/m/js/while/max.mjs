ea9ae52aa492f2c331f5a95fc20ab7c24ebeb68a
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:35:19 -0400

    main file_js_argument_add argadd m_js_while_max for_each_item m_js_function_is

--- src/m/js/while/max.mjs
+++ src/m/js/while/max.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../arguments/assert.mjs";
-export async function m_js_while_max(max_iterations) {
-  await m_js_arguments_assert(m_js_number_is)(arguments);
+export async function m_js_while_max(max_iterations, for_each_item) {
+  await m_js_arguments_assert(m_js_number_is, m_js_function_is)(arguments);
 }
