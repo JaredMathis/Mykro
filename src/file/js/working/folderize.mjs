651a19044610e82308c70a3aa9bc71672726e534
JaredMathis <jared.mathis@native.consulting>
2022-07-11 07:39:46 -0400

    main noop refactor

--- src/file/js/working/folderize.mjs
+++ src/file/js/working/folderize.mjs
@@ -1,11 +1,11 @@
 import {file_js_imports_resolve} from "./../imports/resolve.mjs";
 import {file_js_folderize} from "./../folderize.mjs";
 import {m_js_for_each} from "./../../../m/js/for/each.mjs";
-import {file_js_all} from "./../all.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
+import { file_js_working } from "../working.mjs";
 export async function file_js_working_folderize() {
   await m_js_arguments_assert()(arguments);
-  let files = await file_js_all();
+  let files = await file_js_working();
   await m_js_for_each(files, async file => {
     await file_js_folderize(file.name);
   });
