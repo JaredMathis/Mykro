f58856ebf215f635dd32eb3e35e007457f60d936
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:53:59 -0400

    main noop missing await

--- src/ui/view.mjs
+++ src/ui/view.mjs
@@ -5,7 +5,7 @@ import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {m_js_defined_is} from "./../m/js/defined/is.mjs";
 import {m_js_property_get} from "./../m/js/property/get.mjs";
 export async function ui_view(parent) {
-  let container = ui_element(parent, "div");
+  let container = await ui_element(parent, "div");
   await m_js_arguments_assert(ui_html_element_is)(arguments);
   let current;
   return {
