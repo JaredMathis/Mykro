5d75b6173f7300b84cb671eb360176339e5e9273
JaredMathis <jared.mathis@native.consulting>
2022-07-11 19:55:59 -0400

    main file_js_auto random_list_slice_join

--- src/random/list/slice/join.mjs
+++ src/random/list/slice/join.mjs
@@ -1,11 +1,12 @@
+import {m_js_string_is} from "./../../../m/js/string/is.mjs";
 import {m_js_string_join} from "./../../../m/js/string/join.mjs";
 import {random_list_slice} from "./../slice.mjs";
 import {list_is} from "./../../../list/is.mjs";
 import {m_js_number_is} from "./../../../m/js/number/is.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
-export async function random_list_slice_join(list, slice_size) {
-  await m_js_arguments_assert(list_is, m_js_number_is)(arguments);
+export async function random_list_slice_join(list, slice_size, join_by) {
+  await m_js_arguments_assert(list_is, m_js_number_is, m_js_string_is)(arguments);
   let slice = await random_list_slice(list, slice_size);
-  let joined = await m_js_string_join(slice, " ");
+  let joined = await m_js_string_join(slice, join_by);
   return joined;
 }
