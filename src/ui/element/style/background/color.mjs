28df44b2a0b58c116f703f7bdae7be47178c8164
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:49:48 -0400

    main file_js_argument_add file_js_argadd ui_element_style_background_color element ui_html_element_is

--- src/ui/element/style/background/color.mjs
+++ src/ui/element/style/background/color.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
-export async function ui_element_style_background_color() {
-  await m_js_arguments_assert()(arguments);
+export async function ui_element_style_background_color(element) {
+  await m_js_arguments_assert(ui_html_element_is)(arguments);
 }
