e0e50daa1af9310b165c8050642eeee7a0e580f2
JaredMathis <jared.mathis@native.consulting>
2022-07-11 07:37:55 -0400

    main noop refactor

--- src/file/js/working.mjs
+++ src/file/js/working.mjs
@@ -1,4 +1,9 @@
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
 export async function file_js_working() {
   await m_js_arguments_assert()(arguments);
+  let path_working = await mykro_config_path_src_working_get();
+  let files = await file_js_all();
+  let filtered = await await list_where(files, async (file) => await m_js_string_starts_with(file.file_path, path_working));
+  let function_names = await list_map(filtered, file => file.name);
+  return function_names;
 }
--- src/file/js/working/for/each.mjs
+++ src/file/js/working/for/each.mjs
@@ -1,13 +1,7 @@
-import {mykro_config_path_src_working_get} from "./../../../../mykro/config/path/src/working/get.mjs";
-import {list_map} from "./../../../../list/map.mjs";
-import {file_js_all} from "./../../all.mjs";
 import {file_js_all_for_each_generic} from "./../../all/for/each/generic.mjs";
-import {list_where} from "./../../../../list/where.mjs";
-import {m_js_string_starts_with} from "./../../../../m/js/string/starts/with.mjs";
+import { file_js_working } from "../../working.mjs";
 export async function file_js_working_for_each() {
-  let path_working = await mykro_config_path_src_working_get();
-  let files = await file_js_all();
-  let filtered = await await list_where(files, async file => await m_js_string_starts_with(file.file_path, path_working));
-  let function_names = await list_map(filtered, file => file.name);
+  let function_names = await file_js_working();
   await file_js_all_for_each_generic(function_names, arguments);
 }
+
