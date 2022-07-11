62928df18944ac7c96992f58628b179462f9800c
JaredMathis <jared.mathis@native.consulting>
2022-07-11 07:47:03 -0400

    main file_js_auto file_js_working_folderize

--- src/file/js/working/folderize.mjs
+++ src/file/js/working/folderize.mjs
@@ -1,6 +1,5 @@
 import {file_js_move_without_resolve} from "./../move/without/resolve.mjs";
 import {file_js_imports_resolve} from "./../imports/resolve.mjs";
-import {file_js_folderize} from "./../folderize.mjs";
 import {m_js_for_each} from "./../../../m/js/for/each.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
 import {file_js_working} from "./../working.mjs";
