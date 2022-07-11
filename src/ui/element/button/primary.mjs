efe127f15ceeb0ce25e095205df5c2db2ea0c249
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:14:35 -0400

    main file_js_auto ui_element_button_primary

--- src/ui/element/button/primary.mjs
+++ src/ui/element/button/primary.mjs
@@ -1,10 +1,14 @@
+import {m_js_function_is} from "./../../../m/js/function/is.mjs";
+import {m_js_string_is} from "./../../../m/js/string/is.mjs";
+import {ui_html_element_is} from "./../../html/element/is.mjs";
 import {ui_element_on} from "./../on.mjs";
 import {ui_element_style_button_primary} from "./../style/button/primary.mjs";
 import {ui_element_text} from "./../text.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
 export async function ui_element_button_primary(container, button_text, on_click) {
   await m_js_arguments_assert(ui_html_element_is, m_js_string_is, m_js_function_is)(arguments);
-  let next = await ui_element_text(container, "button", button_text);
-  await ui_element_style_button_primary(next);
-  await ui_element_on(next, "click", on_next);
+  let button = await ui_element_text(container, "button", button_text);
+  await ui_element_style_button_primary(button);
+  await ui_element_on(button, "click", on_click);
+  return button;
 }
