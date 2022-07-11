c7341abee1fc05033cfd9856f3ab3cbf5147a9ff
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:47:33 -0400

    main file_js_add list_swap

--- src/random/list/shuffle.mjs
+++ src/random/list/shuffle.mjs
@@ -3,9 +3,11 @@ import {list_is} from "./../../list/is.mjs";
 import {list_size} from "./../../list/size.mjs";
 import {m_js_for_each} from "./../../m/js/for/each.mjs";
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
+import { random_number } from "../number.mjs";
 export async function random_list_shuffle(list) {
   await m_js_arguments_assert(list_is)(arguments);
   await m_js_for_each(await list_range(await list_size(list)), async index => {
-    
+    let index_new = await random_number(index, await list_max_index(list));
+    await list_swap(list, index, index_new);
   });
 }
