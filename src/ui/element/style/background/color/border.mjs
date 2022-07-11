622f9ab5a4f06194e57145dcc816436539a85ab0
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:16:34 -0400

    main file_js_add ui_element_style_background_color_border

---
+++ src/ui/element/style/background/color/border.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../../m/js/arguments/assert.mjs";
+export async function ui_element_style_background_color_border() {
+  await m_js_arguments_assert()(arguments);
+}
