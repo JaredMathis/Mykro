2515591ffea17155b19da0401764bc8cee1461ae
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:00:12 -0400

    main noop progress

--- src/list/take.mjs
+++ src/list/take.mjs
@@ -3,4 +3,5 @@ import {list_is} from "./is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 export async function list_take(list, count) {
   await m_js_arguments_assert(list_is, m_js_number_is)(arguments);
+  return list.slice(0, count);
 }
