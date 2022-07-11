26b580094be860690af5a9d244a2b7690208a1fa
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:20:27 -0400

    main noop refactor

--- src/ui/element/style/button/generic.mjs
+++ src/ui/element/style/button/generic.mjs
@@ -2,12 +2,11 @@ import {ui_color_blue} from "./../../../color/blue.mjs";
 import {ui_html_element_is} from "./../../../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
 import {ui_element_style_background_color_border} from "./../background/color/border.mjs";
-export async function ui_element_style_button_generic(element) {
-  await m_js_arguments_assert(ui_html_element_is)(arguments);
+export async function ui_element_style_button_generic(element, color, border_opacity, background_opacity) {
+  await m_js_arguments_assert(ui_html_element_is, m_js_string_is, random_probability_is, random_probability_is)(arguments);
   element.style.cursor = "pointer";
   element.style.listStyleType = "none";
-  const color = await ui_color_blue();
-  await ui_element_style_background_color_border(element, color, 1, 0.3);
+  await ui_element_style_background_color_border(element, color, border_opacity, background_opacity);
   element.style.width = "100%";
   element.style.boxSizing = "border-box";
 }
