113d657746960deed8303745db0535b15fc72be6
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:48:57 -0400

    main file_js_argument_add argadd list_swap index_b m_js_number_is

--- src/list/swap.mjs
+++ src/list/swap.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
-export async function list_swap(list, index_a) {
-  await m_js_arguments_assert(list_is, m_js_number_is)(arguments);
+export async function list_swap(list, index_a, index_b) {
+  await m_js_arguments_assert(list_is, m_js_number_is, m_js_number_is)(arguments);
 }
