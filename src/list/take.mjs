c321f0d5d3a98111c3a136d177ae961e2e1b1bb8
JaredMathis <jared.mathis@native.consulting>
2022-07-11 16:59:30 -0400

    main file_js_auto list_take

--- src/list/take.mjs
+++ src/list/take.mjs
@@ -1,3 +1,5 @@
+import {m_js_number_is} from "./../m/js/number/is.mjs";
+import {list_is} from "./is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 export async function list_take(list, count) {
   await m_js_arguments_assert(list_is, m_js_number_is)(arguments);
