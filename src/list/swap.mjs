addcae4a51dd8d4d29b5434ce11615afdf13c9fe
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:47:38 -0400

    main file_js_argument_add argadd list_swap list list_is

--- src/list/swap.mjs
+++ src/list/swap.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
-export async function list_swap() {
-  await m_js_arguments_assert()(arguments);
+export async function list_swap(list) {
+  await m_js_arguments_assert(list_is)(arguments);
 }
