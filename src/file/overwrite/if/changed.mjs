5ee4471de260d7066bb14083ed0848b37a0e53c3
JaredMathis <jared.mathis@native.consulting>
2022-07-11 07:35:53 -0400

    main file_js_auto file_overwrite_if_changed

--- src/file/overwrite/if/changed.mjs
+++ src/file/overwrite/if/changed.mjs
@@ -1,5 +1,13 @@
 import {m_js_string_is} from "./../../../m/js/string/is.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
+import {file_read} from "./../../read.mjs";
+import {m_js_equals} from "./../../../m/js/equals.mjs";
+import {file_overwrite} from "./../../overwrite.mjs";
 export async function file_overwrite_if_changed(file_path, text) {
   await m_js_arguments_assert(m_js_string_is, m_js_string_is)(arguments);
+  let existing = await file_read(file_path);
+  if (m_js_equals(existing, text)) {
+    return;
+  }
+  await file_overwrite(file_path, text);
 }
