492e8992fa78c913cedf2e5830ef7484519d2e17
JaredMathis <jared.mathis@native.consulting>
2022-07-10 22:33:36 -0400

    main file_js_auto ui_element_remove

--- src/ui/element/remove.mjs
+++ src/ui/element/remove.mjs
@@ -1,3 +1,4 @@
+import {ui_html_element_is} from "./../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
 export async function ui_element_remove(element) {
   await m_js_arguments_assert(ui_html_element_is)(arguments);
