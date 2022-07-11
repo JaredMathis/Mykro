407bfae860265808b9f5e7f1560e104090cef4b2
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:50:13 -0400

    main file_js_auto ui_view

--- src/ui/view.mjs
+++ src/ui/view.mjs
@@ -1,20 +1,11 @@
+import {ui_element} from "./element.mjs";
 import {ui_element_remove} from "./element/remove.mjs";
 import {ui_html_element_is} from "./html/element/is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {m_js_defined_is} from "./../m/js/defined/is.mjs";
-import { m_js_property_get } from "../m/js/property/get.mjs";
+import {m_js_property_get} from "./../m/js/property/get.mjs";
 export async function ui_view(parent) {
-  let container = ui_element(parent, 'div');
+  let container = ui_element(parent, "div");
   await m_js_arguments_assert(ui_html_element_is)(arguments);
   let current;
-  return {
-    container: container,
-    view_set: async constructor => {
-      if (await m_js_defined_is(current)) {
-        await ui_element_remove(current);
-      }
-      let component = await constructor(container);
-      current = await m_js_property_get(component, 'container');
-    }
-  };
 }
