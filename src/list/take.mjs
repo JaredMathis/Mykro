9fa2d8d0b754d0088d81e30581cd6b0d7ced5e85
JaredMathis <jared.mathis@native.consulting>
2022-07-11 16:59:22 -0400

    main file_js_argument_add argadd list_take count m_js_number_is

--- src/list/take.mjs
+++ src/list/take.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
-export async function list_take(list) {
-  await m_js_arguments_assert(list_is)(arguments);
+export async function list_take(list, count) {
+  await m_js_arguments_assert(list_is, m_js_number_is)(arguments);
 }
