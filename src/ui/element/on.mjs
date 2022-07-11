00db06cfd44d9c7b4d442c0c466259e9754803f8
JaredMathis <jared.mathis@native.consulting>
2022-07-11 16:04:43 -0400

    main file_js_argument_add file_js_argumentadd ui_element_on element ui_html_element_is

--- src/ui/element/on.mjs
+++ src/ui/element/on.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
-export async function ui_element_on() {
-  await m_js_arguments_assert()(arguments);
+export async function ui_element_on(element) {
+  await m_js_arguments_assert(ui_html_element_is)(arguments);
 }
