98faa43cb64cccde6631bb26421a44d2351961c7
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:48:24 -0400

    main noop progress

--- src/ui/view.mjs
+++ src/ui/view.mjs
@@ -4,14 +4,16 @@ import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {m_js_defined_is} from "./../m/js/defined/is.mjs";
 import { m_js_property_get } from "../m/js/property/get.mjs";
 export async function ui_view(parent) {
+  let container = ui_element(parent, 'div');
   await m_js_arguments_assert(ui_html_element_is)(arguments);
   let current;
   return {
-    set: async constructor => {
+    container: container,
+    view_set: async constructor => {
       if (await m_js_defined_is(current)) {
         await ui_element_remove(current);
       }
-      let component = await constructor(parent);
+      let component = await constructor(container);
       current = await m_js_property_get(component, 'container');
     }
   };
