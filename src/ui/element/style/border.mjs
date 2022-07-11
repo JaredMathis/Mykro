dc4c1da358f11fa2b699afb3cfa8ef0e8c1cd116
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:30:44 -0400

    main file_js_argument_add file_js_argadd ui_element_style_border element ui_html_element_is

--- src/ui/element/style/border.mjs
+++ src/ui/element/style/border.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
-export async function ui_element_style_border() {
-  await m_js_arguments_assert()(arguments);
+export async function ui_element_style_border(element) {
+  await m_js_arguments_assert(ui_html_element_is)(arguments);
 }
