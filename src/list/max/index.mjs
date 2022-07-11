78c58ec8b5e7c5f84f4de4d2e4511ee2296b3df7
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:46:21 -0400

    main file_js_argument_add argadd list_max_index list list_is

--- src/list/max/index.mjs
+++ src/list/max/index.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
-export async function list_max_index() {
-  await m_js_arguments_assert()(arguments);
+export async function list_max_index(list) {
+  await m_js_arguments_assert(list_is)(arguments);
 }
