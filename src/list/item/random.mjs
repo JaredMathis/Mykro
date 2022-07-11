2a1e27fa707441e387c5c7c1108e8d8a8e6d2d87
JaredMathis <jared.mathis@native.consulting>
2022-07-11 16:48:51 -0400

    main noop refactor external dependency away

--- src/list/item/random.mjs
+++ src/list/item/random.mjs
@@ -1,8 +1,9 @@
-import _ from "lodash";
+import { list_size } from "../size.mjs";
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
 import {list_is_not_empty} from "./../is/not/empty.mjs";
 export async function list_item_random(list) {
   await m_js_arguments_assert(list_is_not_empty)(arguments);
-  let index = _.random(0, list.length - 1);
+  const size = await list_size(list);
+  let index = Math.floor(Math.random() * size);
   return list[index];
 }
