84ac6aed672917672acf70d48df70a3f354e2b07
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:22:57 -0400

    main file_js_auto ui_element_style_button_generic

--- src/ui/element/style/button/generic.mjs
+++ src/ui/element/style/button/generic.mjs
@@ -1,3 +1,5 @@
+import {random_probability_is} from "./../../../../random/probability/is.mjs";
+import {m_js_string_is} from "./../../../../m/js/string/is.mjs";
 import {ui_html_element_is} from "./../../../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
 import {ui_element_style_background_color_border} from "./../background/color/border.mjs";
