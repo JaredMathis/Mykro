4f28acfdb8586e8bcf36285ffc0bc10b0b59d3b8
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:52:00 -0400

    main noop progress

--- src/ui/element/style/background/color.mjs
+++ src/ui/element/style/background/color.mjs
@@ -4,4 +4,5 @@ import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
 import {random_probability_is} from "./../../../../random/probability/is.mjs";
 export async function ui_element_style_background_color(element, color, alpha) {
   await m_js_arguments_assert(ui_html_element_is, m_js_string_is, random_probability_is)(arguments);
+  element.style.backgroundColor = `rgba(${color}, ${alpha})`;
 }
