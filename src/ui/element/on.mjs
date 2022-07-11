f6be06a37f94d7fa9bb4ee4696ecc80148dbe12d
JaredMathis <jared.mathis@native.consulting>
2022-07-11 16:05:58 -0400

    main file_js_auto ui_element_on

--- src/ui/element/on.mjs
+++ src/ui/element/on.mjs
@@ -1,3 +1,6 @@
+import {m_js_function_is} from "./../../m/js/function/is.mjs";
+import {m_js_string_is} from "./../../m/js/string/is.mjs";
+import {ui_html_element_is} from "./../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
 export async function ui_element_on(element, event_name, action) {
   await m_js_arguments_assert(ui_html_element_is, m_js_string_is, m_js_function_is)(arguments);
