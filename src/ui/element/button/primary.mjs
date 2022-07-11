bd6329b2d11ff28710ab30be580a705ce015269e
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:12:32 -0400

    main file_js_argument_add argadd ui_element_button_primary button_text m_js_string_is

--- src/ui/element/button/primary.mjs
+++ src/ui/element/button/primary.mjs
@@ -2,8 +2,8 @@ import {ui_element_on} from "./../on.mjs";
 import {ui_element_style_button_primary} from "./../style/button/primary.mjs";
 import {ui_element_text} from "./../text.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
-export async function ui_element_button_primary(container) {
-  await m_js_arguments_assert(ui_html_element_is)(arguments);
+export async function ui_element_button_primary(container, button_text) {
+  await m_js_arguments_assert(ui_html_element_is, m_js_string_is)(arguments);
   let next = await ui_element_text(container, "button", "Another Example");
   await ui_element_style_button_primary(next);
   await ui_element_on(next, "click", on_next);
