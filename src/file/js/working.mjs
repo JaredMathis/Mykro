e67eb404e13f1d4c045467492619c7238fdd2851
JaredMathis <jared.mathis@native.consulting>
2022-07-11 07:43:36 -0400

    main noop refactor

--- src/file/js/working.mjs
+++ src/file/js/working.mjs
@@ -9,6 +9,5 @@ export async function file_js_working() {
   let path_working = await mykro_config_path_src_working_get();
   let files = await file_js_all();
   let filtered = await await list_where(files, async file => await m_js_string_starts_with(file.file_path, path_working));
-  let function_names = await list_map(filtered, file => file.name);
-  return function_names;
+  return filtered;
 }
--- src/file/js/working/for/each.mjs
+++ src/file/js/working/for/each.mjs
@@ -1,7 +1,8 @@
 import {file_js_all_for_each_generic} from "./../../all/for/each/generic.mjs";
 import { file_js_working } from "../../working.mjs";
 export async function file_js_working_for_each() {
-  let function_names = await file_js_working();
+  let working = await file_js_working();
+  let function_names = await list_map(working, file => file.name);
   await file_js_all_for_each_generic(function_names, arguments);
 }
 
