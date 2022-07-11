92d3df290f53cba05eb5b732c0054f5ae8a45991
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:45:27 -0400

    main command_line cmli npm run pub

--- src/list/map.mjs
+++ src/list/map.mjs
@@ -1,8 +1,9 @@
-import _ from "lodash";
+import { m_js_function_is } from "../m/js/function/is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {m_js_for_each} from "./../m/js/for/each.mjs";
+import { list_is } from "./is.mjs";
 export async function list_map(list, mapper) {
-  await m_js_arguments_assert(_.isArray, _.isFunction)(arguments);
+  await m_js_arguments_assert(list_is, m_js_function_is)(arguments);
   let result = [];
   await m_js_for_each(list, async item => {
     let mapped = await mapper(item);
