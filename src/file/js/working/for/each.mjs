71d6ba44bcd9bcf1e13cca5e0bb81813cb734798
JaredMathis <jared.mathis@native.consulting>
2022-07-11 08:57:38 -0400

    main file_js_auto file_js_working_for_each

--- src/file/js/working/for/each.mjs
+++ src/file/js/working/for/each.mjs
@@ -1,8 +1,8 @@
+import {list_map} from "./../../../../list/map.mjs";
 import {file_js_all_for_each_generic} from "./../../all/for/each/generic.mjs";
-import { file_js_working } from "../../working.mjs";
+import {file_js_working} from "./../../working.mjs";
 export async function file_js_working_for_each() {
   let working = await file_js_working();
   let function_names = await list_map(working, file => file.name);
   await file_js_all_for_each_generic(function_names, arguments);
 }
-
