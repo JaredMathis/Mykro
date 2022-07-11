05c3da1ae00fa8d2cd7783ef346a52ae7a3ed033
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:17:48 -0400

    main noop new function

--- src/ui/element/style/background/color/border.mjs
+++ src/ui/element/style/background/color/border.mjs
@@ -1,4 +1,9 @@
+import { m_js_string_is } from "../../../../../m/js/string/is.mjs";
+import { random_probability_is } from "../../../../../random/probability/is.mjs";
+import { ui_html_element_is } from "../../../../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../../../m/js/arguments/assert.mjs";
-export async function ui_element_style_background_color_border() {
-  await m_js_arguments_assert()(arguments);
+export async function ui_element_style_background_color_border(element, color, border_opacity, background_opacity) {
+  await m_js_arguments_assert(ui_html_element_is, m_js_string_is, random_probability_is, random_probability_is)(arguments);
+  await ui_element_style_border(element, `rgba(${color}, ${border_opacity})`);
+  await ui_element_style_background_color(element, color, background_opacity);
 }
