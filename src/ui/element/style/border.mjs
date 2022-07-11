31920dcac04d2dd52e01f20abe0df694e9e4d7a1
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:35:31 -0400

    main noop progress

--- src/ui/element/style/border.mjs
+++ src/ui/element/style/border.mjs
@@ -2,4 +2,10 @@ import {ui_html_element_is} from "./../../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
 export async function ui_element_style_border(element) {
   await m_js_arguments_assert(ui_html_element_is)(arguments);
+  element.style = {
+    border: '0.3vh solid black',
+    borderRadius: '2vh',
+    padding: '1.3vh'
+  }
+  return element;
 }
