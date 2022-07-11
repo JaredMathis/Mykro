0afede5ecb62ff52344c4132fd602854d2f90f1b
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:21:52 -0400

    main noop progress

---
+++ src/ui/element/style/button/secondary.mjs
@@ -0,0 +1,7 @@
+import {ui_element_style_button_generic} from "./generic.mjs";
+import {ui_html_element_is} from "../../../html/element/is.mjs";
+import {m_js_arguments_assert} from "../../../../m/js/arguments/assert.mjs";
+export async function ui_element_style_button_secondary(element) {
+  await m_js_arguments_assert(ui_html_element_is)(arguments);
+  return await ui_element_style_button_generic(element, `1,1,1`, 0.2, 0.12);
+}
