934850eac14ac1575b0a7dcccbdcd78e239ad5df
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:25:13 -0400

    main noop refactor external dependency away

--- src/m/js/number/at/least.mjs
+++ src/m/js/number/at/least.mjs
@@ -1,6 +1,6 @@
-import _ from "lodash";
+import { m_js_number_is } from "../is.mjs";
 import {m_js_arguments_assert} from "./../../arguments/assert.mjs";
 export async function m_js_number_at_least(value, minimum) {
-  await m_js_arguments_assert(_.isNumber, _.isNumber)(arguments);
+  await m_js_arguments_assert(m_js_number_is, m_js_number_is)(arguments);
   return value >= minimum;
 }
