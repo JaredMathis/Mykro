b3e47220a698164827d22023bb014ace8ae008e1
JaredMathis <jared.mathis@native.consulting>
2022-07-11 16:06:20 -0400

    main noop progress

--- src/ui/element/on.mjs
+++ src/ui/element/on.mjs
@@ -4,4 +4,5 @@ import {ui_html_element_is} from "./../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
 export async function ui_element_on(element, event_name, action) {
   await m_js_arguments_assert(ui_html_element_is, m_js_string_is, m_js_function_is)(arguments);
+  element.addEventListener(event_name, action);
 }
