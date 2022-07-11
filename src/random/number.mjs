46a47c57432d0c5cfd6d3fea66984e0b51f80a75
JaredMathis <jared.mathis@native.consulting>
2022-07-11 16:51:04 -0400

    main noop refactor external dependency away

--- src/random/number.mjs
+++ src/random/number.mjs
@@ -1,7 +1,6 @@
-import _ from "lodash";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 import {m_js_number_is} from "./../m/js/number/is.mjs";
 export async function random_number(min, max) {
   await m_js_arguments_assert(m_js_number_is, m_js_number_is)(arguments);
-  return _.random(min, max);
+  return min + Math.floor(Math.random() * (max - min + 1));
 }
