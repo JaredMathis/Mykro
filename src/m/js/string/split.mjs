89f851f361ebf062fd9550a2b9730873cce2008a
JaredMathis <jared.mathis@native.consulting>
2022-07-11 16:51:30 -0400

    main noop refactor external dependency away

--- src/m/js/string/split.mjs
+++ src/m/js/string/split.mjs
@@ -1,6 +1,6 @@
-import _ from "lodash";
 import {m_js_arguments_assert} from "./../arguments/assert.mjs";
+import { m_js_string_is } from "./is.mjs";
 export async function m_js_string_split(s, split_by) {
-  await m_js_arguments_assert(_.isString, _.isString)(arguments);
+  await m_js_arguments_assert(m_js_string_is, m_js_string_is)(arguments);
   return s.split(split_by);
 }
