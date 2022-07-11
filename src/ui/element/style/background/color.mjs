f3294c707be0ef9eb8a19aa09ab3d19a86a159f8
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:51:22 -0400

    main file_js_auto ui_element_style_background_color

--- src/ui/element/style/background/color.mjs
+++ src/ui/element/style/background/color.mjs
@@ -1,6 +1,7 @@
 import {m_js_string_is} from "./../../../../m/js/string/is.mjs";
 import {ui_html_element_is} from "./../../../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
+import {random_probability_is} from "./../../../../random/probability/is.mjs";
 export async function ui_element_style_background_color(element, color, alpha) {
-  await m_js_arguments_assert(ui_html_element_is, m_js_string_is, probability_is)(arguments);
+  await m_js_arguments_assert(ui_html_element_is, m_js_string_is, random_probability_is)(arguments);
 }
