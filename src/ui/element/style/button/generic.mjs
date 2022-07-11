96b24d9d23514bf360569b5f00138bc98b65d533
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:19:59 -0400

    main noop copy paste

--- src/ui/element/style/button/generic.mjs
+++ src/ui/element/style/button/generic.mjs
@@ -1,4 +1,13 @@
+import {ui_color_blue} from "./../../../color/blue.mjs";
+import {ui_html_element_is} from "./../../../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
-export async function ui_element_style_button_generic() {
-  await m_js_arguments_assert()(arguments);
+import {ui_element_style_background_color_border} from "./../background/color/border.mjs";
+export async function ui_element_style_button_generic(element) {
+  await m_js_arguments_assert(ui_html_element_is)(arguments);
+  element.style.cursor = "pointer";
+  element.style.listStyleType = "none";
+  const color = await ui_color_blue();
+  await ui_element_style_background_color_border(element, color, 1, 0.3);
+  element.style.width = "100%";
+  element.style.boxSizing = "border-box";
 }
