54f1ce96d49948aa9c519228393f44fe5ef29de2
JaredMathis <jared.mathis@native.consulting>
2022-07-10 22:33:25 -0400

    main noop progress

--- src/ui/element/remove.mjs
+++ src/ui/element/remove.mjs
@@ -1,4 +1,5 @@
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
 export async function ui_element_remove(element) {
   await m_js_arguments_assert(ui_html_element_is)(arguments);
+  element.remove();
 }
