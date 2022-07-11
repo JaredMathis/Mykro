1c9a4bddde43f6081f57d33276066c98e227a4d0
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:11:39 -0400

    main file_js_auto ui_element_button_primary

--- src/ui/element/button/primary.mjs
+++ src/ui/element/button/primary.mjs
@@ -1,4 +1,10 @@
+import {ui_element_on} from "./../on.mjs";
+import {ui_element_style_button_primary} from "./../style/button/primary.mjs";
+import {ui_element_text} from "./../text.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
 export async function ui_element_button_primary() {
   await m_js_arguments_assert()(arguments);
+  let next = await ui_element_text(container, "button", "Another Example");
+  await ui_element_style_button_primary(next);
+  await ui_element_on(next, "click", on_next);
 }
