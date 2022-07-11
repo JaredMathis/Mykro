888501eaf17e539d6b94f758fee981cdc6aca103
JaredMathis <jared.mathis@native.consulting>
2022-07-10 23:19:49 -0400

    main file_js_add ui_element_style_button_generic

---
+++ src/ui/element/style/button/generic.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
+export async function ui_element_style_button_generic() {
+  await m_js_arguments_assert()(arguments);
+}
