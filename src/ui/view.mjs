53b9964c0f929857cb07418d7f91ebacb24b0bcc
JaredMathis <jared.mathis@native.consulting>
2022-07-10 22:34:23 -0400

    main file_js_auto ui_view

--- src/ui/view.mjs
+++ src/ui/view.mjs
@@ -1,14 +1,15 @@
+import {ui_element_remove} from "./element/remove.mjs";
 import {ui_html_element_is} from "./html/element/is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
-import { m_js_defined_is } from "../m/js/defined/is.mjs";
+import {m_js_defined_is} from "./../m/js/defined/is.mjs";
 export async function ui_view(parent) {
   await m_js_arguments_assert(ui_html_element_is)(arguments);
   let current;
   return {
-    set: async (constructor) => {
+    set: async constructor => {
       if (m_js_defined_is(current)) {
-        
+        await ui_element_remove(current);
       }
     }
-  }
+  };
 }
