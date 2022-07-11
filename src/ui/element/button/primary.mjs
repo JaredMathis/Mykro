24a84c1c53bd1eaf88783e9b65a2b1fcb31ec103
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:13:52 -0400

    main noop refactor

--- src/ui/element/button/primary.mjs
+++ src/ui/element/button/primary.mjs
@@ -4,7 +4,7 @@ import {ui_element_text} from "./../text.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
 export async function ui_element_button_primary(container, button_text) {
   await m_js_arguments_assert(ui_html_element_is, m_js_string_is)(arguments);
-  let next = await ui_element_text(container, "button", "Another Example");
+  let next = await ui_element_text(container, "button", button_text);
   await ui_element_style_button_primary(next);
   await ui_element_on(next, "click", on_next);
 }
