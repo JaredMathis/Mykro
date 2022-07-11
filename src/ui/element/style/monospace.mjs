919f999e0f994b36166fafa9cf79dce5096cb9cd
JaredMathis <jared.mathis@native.consulting>
2022-07-11 00:03:33 -0400

    main command_line cmli npm run pub

--- src/ui/element/style/monospace.mjs
+++ src/ui/element/style/monospace.mjs
@@ -2,5 +2,5 @@ import {ui_html_element_is} from "./../../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
 export async function ui_element_style_monospace(element) {
   await m_js_arguments_assert(ui_html_element_is)(arguments);
-  element.style.fontFamily = monospace;
+  element.style.fontFamily = 'monospace';
 }
