e304db8e1ac9022c39cf29e10a95ecae2ebb977f
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:50:29 -0400

    main file_js_auto list_swap

--- src/list/swap.mjs
+++ src/list/swap.mjs
@@ -3,4 +3,7 @@ import {list_is} from "./is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 export async function list_swap(list, index_a, index_b) {
   await m_js_arguments_assert(list_is, m_js_number_is, m_js_number_is)(arguments);
+  let temp = list[index_a];
+  list[index_a] = list[index_b];
+  list[index_b] = temp;
 }
