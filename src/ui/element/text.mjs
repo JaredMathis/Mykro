f87671895454760d8639cfeed376d86c016c8f9e
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:14:35 -0400

    main file_js_auto ui_element_text

--- src/ui/element/text.mjs
+++ src/ui/element/text.mjs
@@ -1,3 +1,5 @@
+import {m_js_string_is} from "./../../m/js/string/is.mjs";
+import {ui_html_element_is} from "./../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
 export async function ui_element_text(parent, text) {
   await m_js_arguments_assert(ui_html_element_is, m_js_string_is)(arguments);
