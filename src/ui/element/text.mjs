47367ec2e086664b2e649fdd9fe4e480cfdc1cf1
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:14:22 -0400

    main file_js_argument_add file_js_argadd ui_element_text text m_js_string_is

--- src/ui/element/text.mjs
+++ src/ui/element/text.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
-export async function ui_element_text(parent) {
-  await m_js_arguments_assert(ui_html_element_is)(arguments);
+export async function ui_element_text(parent, text) {
+  await m_js_arguments_assert(ui_html_element_is, m_js_string_is)(arguments);
 }
