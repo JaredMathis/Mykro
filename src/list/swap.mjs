603908950068231441990b464a5769aadec418a5
JaredMathis <jared.mathis@native.consulting>
2022-07-11 17:49:06 -0400

    main file_js_auto list_swap

--- src/list/swap.mjs
+++ src/list/swap.mjs
@@ -1,3 +1,5 @@
+import {m_js_number_is} from "./../m/js/number/is.mjs";
+import {list_is} from "./is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 export async function list_swap(list, index_a, index_b) {
   await m_js_arguments_assert(list_is, m_js_number_is, m_js_number_is)(arguments);
