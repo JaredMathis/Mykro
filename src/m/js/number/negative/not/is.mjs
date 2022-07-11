87475f3799bc7bb74b78bbaaf0ca8db4464a7d2c
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:23:22 -0400

    main noop refactor external dependency away

--- src/m/js/number/negative/not/is.mjs
+++ src/m/js/number/negative/not/is.mjs
@@ -1,7 +1,7 @@
-import _ from "lodash";
+import { m_js_number_is } from "../../is.mjs";
 import {m_js_arguments_assert} from "./../../../arguments/assert.mjs";
 import {m_js_number_at_least} from "./../../at/least.mjs";
 export async function m_js_number_negative_not_is(value) {
-  await m_js_arguments_assert(_.isNumber)(arguments);
+  await m_js_arguments_assert(m_js_number_is)(arguments);
   return m_js_number_at_least(value, 0);
 }
