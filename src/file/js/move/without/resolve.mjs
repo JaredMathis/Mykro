9765b6397a18ff3c93e91bc56e347869ade29ca7
JaredMathis <jared.mathis@native.consulting>
2022-07-11 07:12:37 -0400

    main file_js_add file_js_move_without_resolve

---
+++ src/file/js/move/without/resolve.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
+export async function file_js_move_without_resolve() {
+  await m_js_arguments_assert()(arguments);
+}
