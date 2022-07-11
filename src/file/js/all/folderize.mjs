74eae4e8f67fe82ee6a00e01d68a1c9d5b1d2f46
JaredMathis <jared.mathis@native.consulting>
2022-07-11 07:16:13 -0400

    main file_js_auto file_js_all_folderize

--- src/file/js/all/folderize.mjs
+++ src/file/js/all/folderize.mjs
@@ -1,7 +1,15 @@
-import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
-import {file_js_all_for_each} from "./for/each.mjs";
+import {file_js_imports_resolve} from "./../imports/resolve.mjs";
 import {file_js_folderize} from "./../folderize.mjs";
+import {m_js_for_each} from "./../../../m/js/for/each.mjs";
+import {file_js_all} from "./../all.mjs";
+import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
 export async function file_js_all_folderize() {
   await m_js_arguments_assert()(arguments);
-  await file_js_all_for_each(file_js_folderize.name);
+  let files = await file_js_all();
+  await m_js_for_each(files, async file => {
+    await file_js_folderize(file.name);
+  });
+  await m_js_for_each(files, async file => {
+    await file_js_imports_resolve(file.name);
+  });
 }
