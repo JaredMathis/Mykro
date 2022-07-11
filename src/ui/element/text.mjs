20042675e458d0b69bfc0e443b005f081a8cd3a4
JaredMathis <jared.mathis@native.consulting>
2022-07-10 21:18:57 -0400

    main noop typo

--- src/ui/element/text.mjs
+++ src/ui/element/text.mjs
@@ -3,9 +3,9 @@ import {ui_html_element_is} from "./../html/element/is.mjs";
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
 import {ui_element} from "./../element.mjs";
 import {ui_element_html_inner_set} from "./html/inner/set.mjs";
-export async function ui_element_text(parent, text) {
-  await m_js_arguments_assert(ui_html_element_is, m_js_string_is)(arguments);
-  let result = await ui_element(parent);
+export async function ui_element_text(parent, tag_name, text) {
+  await m_js_arguments_assert(ui_html_element_is, m_js_string_is, m_js_string_is)(arguments);
+  let result = await ui_element(parent, tag_name);
   await ui_element_html_inner_set(result, text);
   return result;
 }
