2ee6f060ddbe482a3fab77b2f0d18214f50b82b8
JaredMathis <jared.mathis@native.consulting>
2022-07-12 09:03:59 -0400

    main noop unused imports

--- src/file/js/all/identifier/rename.mjs
+++ src/file/js/all/identifier/rename.mjs
@@ -1,13 +1,9 @@
-import {es_traverse} from "./../../../../es/traverse.mjs";
 import {m_js_not} from "./../../../../m/js/not.mjs";
 import {m_js_string_is} from "./../../../../m/js/string/is.mjs";
 import {m_js_arguments_assert} from "./../../../../m/js/arguments/assert.mjs";
 import {file_js_all_identifiers_get} from "./../identifiers/get.mjs";
 import {m_js_assert} from "./../../../../m/js/assert.mjs";
 import {list_contains} from "./../../../../list/contains.mjs";
-import {file_js_transform} from "./../../transform.mjs";
-import {m_js_string_identifier_is} from "./../../../../m/js/string/identifier/is.mjs";
-import {m_js_equals} from "./../../../../m/js/equals.mjs";
 import {file_js_all_for_each} from "./../for/each.mjs";
 import {file_js_identifier_rename} from "./../../identifier/rename.mjs";
 export async function file_js_all_identifier_rename(name_from, name_to) {
