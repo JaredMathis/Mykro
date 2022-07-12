b2fb8d3e9a42db3d74b2c297598cd71b4f38d244
JaredMathis <jared.mathis@native.consulting>
2022-07-11 21:40:38 -0400

    main file_js_auto ui_element_button_secondary

--- src/ui/element/button/secondary.mjs
+++ src/ui/element/button/secondary.mjs
@@ -1,4 +1,15 @@
+import {ui_element_style_button_secondary} from "./../style/button/secondary.mjs";
+import {m_js_function_is} from "./../../../m/js/function/is.mjs";
+import {m_js_string_is} from "./../../../m/js/string/is.mjs";
+import {ui_html_element_is} from "./../../html/element/is.mjs";
+import {ui_element_on} from "./../on.mjs";
+import {ui_element_style_button_primary} from "./../style/button/primary.mjs";
+import {ui_element_text} from "./../text.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
-export async function ui_element_button_secondary() {
-  await m_js_arguments_assert()(arguments);
+export async function ui_element_button_secondary(container, button_text, on_click) {
+  await m_js_arguments_assert(ui_html_element_is, m_js_string_is, m_js_function_is)(arguments);
+  let button = await ui_element_text(container, "button", button_text);
+  await ui_element_style_button_secondary(button);
+  await ui_element_on(button, "click", on_click);
+  return button;
 }
