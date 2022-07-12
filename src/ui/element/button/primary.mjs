8d10f60abe4855eed3bdd0da77da570e6f228260
JaredMathis <jared.mathis@native.consulting>
2022-07-11 21:42:13 -0400

    main file_js_auto ui_element_button_primary

--- src/ui/element/button/primary.mjs
+++ src/ui/element/button/primary.mjs
@@ -1,14 +1,10 @@
+import {ui_element_button_generic} from "./generic.mjs";
 import {m_js_function_is} from "./../../../m/js/function/is.mjs";
 import {m_js_string_is} from "./../../../m/js/string/is.mjs";
 import {ui_html_element_is} from "./../../html/element/is.mjs";
-import {ui_element_on} from "./../on.mjs";
 import {ui_element_style_button_primary} from "./../style/button/primary.mjs";
-import {ui_element_text} from "./../text.mjs";
 import {m_js_arguments_assert} from "./../../../m/js/arguments/assert.mjs";
 export async function ui_element_button_primary(container, button_text, on_click) {
   await m_js_arguments_assert(ui_html_element_is, m_js_string_is, m_js_function_is)(arguments);
-  let button = await ui_element_text(container, "button", button_text);
-  await ui_element_style_button_primary(button);
-  await ui_element_on(button, "click", on_click);
-  return button;
+  return ui_element_button_generic(container, button_text, on_click, ui_element_style_button_primary);
 }
