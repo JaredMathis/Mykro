c9026c419d60c32212848ee50a2810400a75f2dc
JaredMathis <jared.mathis@native.consulting>
2022-07-15 16:14:53 -0400

    main noop progress

--- src/m/js/number/smaller.mjs
+++ src/m/js/number/smaller.mjs
@@ -1,4 +1,9 @@
 import {m_js_arguments_assert} from "./../arguments/assert.mjs";
-export async function m_js_number_smaller() {
-  await m_js_arguments_assert()(arguments);
+import { m_js_number_is } from "./is.mjs";
+export async function m_js_number_smaller(a, b) {
+  await m_js_arguments_assert(m_js_number_is, m_js_number_is)(arguments);
+  if (a < b) {
+    return a;
+  }
+  return b;
 }
