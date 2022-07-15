10bd6b32d476f3491ff64a7f213b26c1f55dacc7
JaredMathis <jared.mathis@native.consulting>
2022-07-14 21:39:00 -0400

    main noop progress

--- src/m/js/undefined/is.mjs
+++ src/m/js/undefined/is.mjs
@@ -1,4 +1,7 @@
+import { m_js_defined_is } from "../defined/is.mjs";
+import { m_js_tautology } from "../tautology.mjs";
 import {m_js_arguments_assert} from "./../arguments/assert.mjs";
-export async function m_js_undefined_is() {
-  await m_js_arguments_assert()(arguments);
+export async function m_js_undefined_is(value) {
+  await m_js_arguments_assert(m_js_tautology)(arguments);
+  return !await m_js_defined_is(value);
 }
