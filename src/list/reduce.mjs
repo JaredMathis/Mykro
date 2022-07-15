8118d5f7cea4e0499f016da23715d0a3d1752469
JaredMathis <jared.mathis@native.consulting>
2022-07-15 16:13:13 -0400

    main noop progress

--- src/list/reduce.mjs
+++ src/list/reduce.mjs
@@ -1,4 +1,7 @@
+import { m_js_function_is } from "../m/js/function/is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
-export async function list_reduce() {
-  await m_js_arguments_assert()(arguments);
+import { list_is } from "./is.mjs";
+export async function list_reduce(list, reducer) {
+  await m_js_arguments_assert(list_is, m_js_function_is)(arguments);
+  return list.reduce(reducer);
 }
