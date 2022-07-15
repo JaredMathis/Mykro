c65c0c910450c04524655d2b87ed24d4e8490be1
JaredMathis <jared.mathis@native.consulting>
2022-07-15 16:11:29 -0400

    main file_js_auto list_smallest

--- src/list/smallest.mjs
+++ src/list/smallest.mjs
@@ -1,4 +1,5 @@
+import {list_is} from "./is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
-export async function list_smallest() {
-  await m_js_arguments_assert()(arguments);
+export async function list_smallest(list) {
+  await m_js_arguments_assert(list_is)(arguments);
 }
