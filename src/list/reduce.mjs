d1acc67ca5a491febdf60231eba38fcceb64458d
JaredMathis <jared.mathis@native.consulting>
2022-07-15 17:07:25 -0400

    main file_js_auto list_reduce

--- src/list/reduce.mjs
+++ src/list/reduce.mjs
@@ -1,7 +1,13 @@
-import { m_js_function_is } from "../m/js/function/is.mjs";
+import {m_js_for_each} from "./../m/js/for/each.mjs";
+import {list_first} from "./first.mjs";
+import {m_js_function_is} from "./../m/js/function/is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
-import { list_is } from "./is.mjs";
+import {list_is} from "./is.mjs";
 export async function list_reduce(list, reducer) {
   await m_js_arguments_assert(list_is, m_js_function_is)(arguments);
-  return list.reduce(reducer);
+  let result = await list_first(list);
+  await m_js_for_each(list, async item => {
+    result = await reducer(result, item);
+  });
+  return result;
 }
