043f586a4a0e4e543fe11b5bb813fe5d46e80d85
JaredMathis <jared.mathis@native.consulting>
2022-07-15 16:06:24 -0400

    main file_js_auto list_without

--- src/list/without.mjs
+++ src/list/without.mjs
@@ -1,3 +1,4 @@
+import {list_is} from "./is.mjs";
 import {m_js_arguments_assert} from "./../m/js/arguments/assert.mjs";
 export async function list_without(list, values) {
   await m_js_arguments_assert(list_is, list_is)(arguments);
