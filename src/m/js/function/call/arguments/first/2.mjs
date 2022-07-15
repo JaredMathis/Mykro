04c3693921b36e76f4f7829b8e0effffce4055be
JaredMathis <jared.mathis@native.consulting>
2022-07-15 16:24:19 -0400

    main file_js_auto m_js_function_call_arguments_first_2

--- src/m/js/function/call/arguments/first/2.mjs
+++ src/m/js/function/call/arguments/first/2.mjs
@@ -1,4 +1,8 @@
+import {m_js_function_is} from "./../../../is.mjs";
 import {m_js_arguments_assert} from "./../../../../arguments/assert.mjs";
-export async function m_js_function_call_arguments_first_2() {
-  await m_js_arguments_assert()(arguments);
+export async function m_js_function_call_arguments_first_2(_function) {
+  await m_js_arguments_assert(m_js_function_is)(arguments);
+  return function wrapped(a, b) {
+    return _function(a, b);
+  };
 }
