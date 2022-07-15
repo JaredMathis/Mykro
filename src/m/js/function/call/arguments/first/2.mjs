775265e874149135860e79e10e8b5d12042c7951
JaredMathis <jared.mathis@native.consulting>
2022-07-15 16:23:04 -0400

    main file_js_add m_js_function_call_arguments_first_2

---
+++ src/m/js/function/call/arguments/first/2.mjs
@@ -0,0 +1,4 @@
+import {m_js_arguments_assert} from "./../../../../arguments/assert.mjs";
+export async function m_js_function_call_arguments_first_2() {
+  await m_js_arguments_assert()(arguments);
+}
