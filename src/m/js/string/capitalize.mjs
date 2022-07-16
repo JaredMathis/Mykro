1c384f5ec5f99b40b3eabd659d9c0db6d3d3b07a
JaredMathis <jared.mathis@native.consulting>
2022-07-16 19:10:30 -0400

    main file_js_argument_add argadd m_js_string_capitalize input m_js_string_is

--- src/m/js/string/capitalize.mjs
+++ src/m/js/string/capitalize.mjs
@@ -1,4 +1,5 @@
 import {m_js_arguments_assert} from "./../arguments/assert.mjs";
-export async function m_js_string_capitalize() {
-  await m_js_arguments_assert()(arguments);
+export async function m_js_string_capitalize(input) {
+  await m_js_arguments_assert(m_js_string_is)(arguments);
+  let words = m_js_string_split(input, " ");
 }
