03ce10d764dd06c4b6f6ff0862d773b7f2841596
JaredMathis <jared.mathis@native.consulting>
2022-07-16 19:43:12 -0400

    main file_js_auto m_js_string_listify

--- src/m/js/string/listify.mjs
+++ src/m/js/string/listify.mjs
@@ -1,5 +1,12 @@
+import {m_js_string_from_list} from "./from/list.mjs";
+import {m_js_string_to_list} from "./to/list.mjs";
 import {m_js_string_is} from "./is.mjs";
 import {m_js_arguments_assert} from "./../arguments/assert.mjs";
-export async function m_js_string_listify(s) {
-  await m_js_arguments_assert(m_js_string_is)(arguments);
+import {m_js_function_is} from "./../function/is.mjs";
+export async function m_js_string_listify(s, mapping) {
+  await m_js_arguments_assert(m_js_string_is, m_js_function_is)(arguments);
+  let listified = await m_js_string_to_list(s);
+  let result = await mapping(listified);
+  let stringified = await m_js_string_from_list(result);
+  return stringified;
 }
