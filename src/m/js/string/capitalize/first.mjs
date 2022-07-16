80b77658cf217598cdfcc6e121ecbdcadb9869d3
JaredMathis <jared.mathis@native.consulting>
2022-07-16 19:43:55 -0400

    main file_js_auto m_js_string_capitalize_first

--- src/m/js/string/capitalize/first.mjs
+++ src/m/js/string/capitalize/first.mjs
@@ -1,10 +1,11 @@
+import {m_js_string_size} from "./../size.mjs";
 import {m_js_string_listify} from "./../listify.mjs";
 import {list_starting_at} from "./../../../../list/starting/at.mjs";
 import {m_js_string_is} from "./../is.mjs";
 import {m_js_arguments_assert} from "./../../arguments/assert.mjs";
 export async function m_js_string_capitalize_first(s) {
   await m_js_arguments_assert(m_js_string_is)(arguments);
-  if (await string_size(s) === 0) {
+  if (await m_js_string_size(s) === 0) {
     return s;
   }
   return s[0] + await m_js_string_listify(async l => await list_starting_at(l, 1));
