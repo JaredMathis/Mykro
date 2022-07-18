85c2d93baa7563c3dc1632c71ff7a267106ce401
JaredMathis <jared.mathis@native.consulting>
2022-07-17 20:11:59 -0400

    main file_js_auto m_js_for_range

--- src/m/js/for/range.mjs
+++ src/m/js/for/range.mjs
@@ -1,5 +1,8 @@
+import {list_range} from "./../../../list/range.mjs";
 import {m_js_number_is} from "./../number/is.mjs";
 import {m_js_arguments_assert} from "./../arguments/assert.mjs";
+import {m_js_for_each} from "./each.mjs";
 export async function m_js_for_range(count) {
   await m_js_arguments_assert(m_js_number_is)(arguments);
+  m_js_for_each(await list_range());
 }
