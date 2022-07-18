a946516735b14ba33f5522b650b78a0ba00ccf35
JaredMathis <jared.mathis@native.consulting>
2022-07-18 07:41:06 -0400

    main file_js_auto constant_alphabet_sorted

--- src/constant/alphabet/sorted.mjs
+++ src/constant/alphabet/sorted.mjs
@@ -1,4 +1,10 @@
+import {m_js_string_to_list} from "./../../m/js/string/to/list.mjs";
+import {constant_alphabet} from "./../alphabet.mjs";
 import {m_js_arguments_assert} from "./../../m/js/arguments/assert.mjs";
 export async function constant_alphabet_sorted() {
   await m_js_arguments_assert()(arguments);
+  let letters = await constant_alphabet();
+  let letters_as_list = await m_js_string_to_list(letters);
+  letters_as_list.sort();
+  return letters_as_list;
 }
